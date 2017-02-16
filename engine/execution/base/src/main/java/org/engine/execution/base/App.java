612c463066388398e8fa978e31c3f6e51c96b658
sishu@dtstack.com <sishu@dtstack.com>
2017-02-16 11:28:11 +0800

    ysq

---
+++ engine/execution/base/src/main/java/org/engine/execution/base/App.java
@@ -0,0 +1,13 @@
+package org.engine.execution.base;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
