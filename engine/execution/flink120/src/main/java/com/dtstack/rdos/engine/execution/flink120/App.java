2e04e6e785349ff504aa6c3e60f0e87efed9d460
sishu@dtstack.com <sishu@dtstack.com>
2017-02-16 13:26:11 +0800

    ysq

---
+++ engine/execution/flink120/src/main/java/com/dtstack/rdos/engine/execution/flink120/App.java
@@ -0,0 +1,13 @@
+package com.dtstack.rdos.engine.execution.flink120;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
