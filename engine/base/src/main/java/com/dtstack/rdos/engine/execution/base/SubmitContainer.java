3256e3a17c64dde56ee6e13c9c1bda9501d2b730
sishu@dtstack.com <sishu@dtstack.com>
2017-03-03 18:51:12 +0800

    ysq

--- engine/base/src/main/java/com/dtstack/rdos/engine/execution/base/SubmitContainer.java
+++ engine/base/src/main/java/com/dtstack/rdos/engine/execution/base/SubmitContainer.java
@@ -19,38 +19,36 @@ public class SubmitContainer {
 
     private ClientType clientType;
 
-    /**设置上传jar文件临时目录,如果未设置默认是/tmp/flinkjar*/
-    private String jarFileTmpPath;
+//    /**设置上传jar文件临时目录,如果未设置默认是/tmp/flinkjar*/
+//    private String jarFileTmpPath;
 
     //计算资源
     private int slots;
 
-    private String zkNamespace;
-
-    private String host;
-
-    private String jobManagerPort;
+//    private String zkNamespace;
+//
+//    private String host;
+//
+//    private String jobManagerPort;
 
     private static SubmitContainer submitContainer;
     
-    public static SubmitContainer createSubmitContainer(ClientType clientType,String zkNamespace,String executionEngineUrl,String jarFileTmpPath,int slots){
+    private Properties properties = new Properties();
+    
+    public static SubmitContainer createSubmitContainer(ClientType clientType,int slots,Properties properties){
     	if(submitContainer!=null){
     		synchronized(SubmitContainer.class){
     			if(submitContainer!=null){
-    				String[] uports = executionEngineUrl.split(":");
-    				submitContainer = new SubmitContainer(clientType,zkNamespace,uports[0],uports[1],jarFileTmpPath,slots);
+    				submitContainer = new SubmitContainer(clientType,slots,properties);
     			}
     		}
     	}
     	return SubmitContainer.submitContainer;
     }
     
-    private SubmitContainer(ClientType clientType,String zkNamespace,String host,String jobManagerPort,String jarFileTmpPath,int slots){
+    private SubmitContainer(ClientType clientType,int slots,Properties  properties){
     	this.clientType = clientType;
-    	this.zkNamespace = zkNamespace;
-    	this.host= host;
-    	this.jobManagerPort = jobManagerPort;
-    	this.jarFileTmpPath = jarFileTmpPath;
+    	this.properties.putAll(properties);
     	this.slots = slots;
     	start();
     }
@@ -62,17 +60,11 @@ public class SubmitContainer {
     
 
     public void start(){
-
-        Properties properties = new Properties();
-        JobSubmitExecutor.getInstance().init(clientType, jarFileTmpPath, slots, properties);
+        JobSubmitExecutor.getInstance().init(clientType,slots,properties);
         JobSubmitExecutor.getInstance().start();
 
         logger.info("------start job container----");
         logger.info("client_type:{}", clientType);
-        logger.info("zkNamespace:{}", zkNamespace);
-        logger.info("jobMgrHost:{}", host);
-        logger.info("jobManagerPort:{}", jobManagerPort);
-        logger.info("jarFileTmpPath:{}", jarFileTmpPath);
         logger.info("slots:{}", slots);
         logger.info("------------------------------");
     }
@@ -86,44 +78,16 @@ public class SubmitContainer {
     public void setClientType(ClientType clientType) {
         this.clientType = clientType;
     }
-
-    public String getZkNamespace() {
-        return zkNamespace;
-    }
-
-    public void setZkNamespace(String zkNamespace) {
-        this.zkNamespace = zkNamespace;
-    }
-
-    public String getHost() {
-        return host;
-    }
-
-    public void setHost(String host) {
-        this.host = host;
-    }
-
-    public String getJobManagerPort() {
-        return jobManagerPort;
-    }
-
-    public void setJobManagerPort(String jobManagerPort) {
-        this.jobManagerPort = jobManagerPort;
-    }
-
-    public String getJarFileTmpPath() {
-        return jarFileTmpPath;
-    }
-
-    public void setJarFileTmpPath(String jarFileTmpPath) {
-        this.jarFileTmpPath = jarFileTmpPath;
-    }
-
-	public int getSlots() {
-		return slots;
-	}
-
+    
 	public void setSlots(int slots) {
 		this.slots = slots;
+	}
+
+	public Properties getProperties() {
+		return properties;
+	}
+
+	public void setProperties(Properties properties) {
+		this.properties = properties;
 	}
 }
