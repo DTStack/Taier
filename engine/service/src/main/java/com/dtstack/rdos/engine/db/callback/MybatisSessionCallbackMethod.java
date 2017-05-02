df4741d11cfccdc8fe0db6f82e55d12f8b46a34b
sishu@dtstack.com <sishu@dtstack.com>
2017-05-02 15:01:49 +0800

    ysq

---
+++ engine/service/src/main/java/com/dtstack/rdos/engine/db/callback/MybatisSessionCallbackMethod.java
@@ -0,0 +1,34 @@
+package com.dtstack.rdos.engine.db.callback;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.dtstack.rdos.commom.exception.ExceptionUtil;
+
+/**
+ * 
+ * Reason: TODO ADD REASON(可选)
+ * Date: 2017年03月03日 下午1:25:18
+ * Company: www.dtstack.com
+ * @author sishu.yss
+ *
+ */
+public class MybatisSessionCallbackMethod {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MybatisSessionCallbackMethod.class);
+
+	private static SqlSessionFactory sqlSessionFactory = MyBatisConnectionFactory.getSqlSessionFactory();
+	
+	public static Object doCallback(MybatisSessionCallback mybatisSessionCallback){
+		SqlSession sqlSession = sqlSessionFactory.openSession(true);
+		try{
+			return mybatisSessionCallback.execute(sqlSession);
+		}catch(Exception e){
+			logger.error("MybatisSessionCallbackMethod error:{}",ExceptionUtil.getErrorMessage(e));
+		}finally{
+			if(sqlSession!=null)sqlSession.close();
+		}
+		return null;
+	}
+}
---
+++ engine/service/src/main/java/com/dtstack/rdos/engine/send/HttpSendClient.java
@@ -0,0 +1,33 @@
+package com.dtstack.rdos.engine.send;
+
+import com.dtstack.rdos.common.http.PoolHttpClient;
+import com.dtstack.rdos.common.util.PublicUtil;
+import com.dtstack.rdos.common.util.UrlUtil;
+import com.dtstack.rdos.engine.entrance.service.paramObject.ParamAction;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.dtstack.rdos.engine.entrance.zk.ZkDistributed;
+
+/**
+ * Created by sishu.yss on 2017/3/14.
+ */
+public class HttpSendClient {
+
+    private static ZkDistributed zkDistributed = ZkDistributed.getZkDistributed();
+
+    public static void actionStart(String address,ParamAction paramAction) throws Exception{
+        PoolHttpClient.post(UrlUtil.getHttpUrl(address, Urls.START), PublicUtil.ObjectToMap(paramAction));
+    }
+
+    @SuppressWarnings("serial")
+	public static void migration(final String node)throws Exception{
+        Map<String,Object> params = new HashMap<String,Object>(){
+            {
+                put("node",node);
+            }
+        };
+        PoolHttpClient.post(UrlUtil.getHttpUrl(zkDistributed.isHaveMaster(), Urls.MIGRATE),params);
+    }
+}
