74e035c521afef77e12799cd8e7a84eb9b12a2a5
zoudaokoulife <zoudaokoulife@gmail.com>
2017-05-10 16:33:48 +0800

    防止重复提交判断

--- engine/service/src/main/java/com/dtstack/rdos/engine/db/mapper/RdosStreamActionLogMapper.java
+++ engine/service/src/main/java/com/dtstack/rdos/engine/db/mapper/RdosStreamActionLogMapper.java
@@ -1,9 +1,10 @@
 package com.dtstack.rdos.engine.db.mapper;
 
+import com.dtstack.rdos.engine.db.dataobject.RdosStreamActionLog;
 import org.apache.ibatis.annotations.Param;
 
 /**
- * 
+ *
  * Reason: TODO ADD REASON(可选)
  * Date: 2017年03月03日 下午1:25:18
  * Company: www.dtstack.com
@@ -11,7 +12,10 @@ import org.apache.ibatis.annotations.Param;
  *
  */
 public interface RdosStreamActionLogMapper {
-	
-  public void updateActionStatus(@Param("actionLogId")Long actionLogId, @Param("status") int status);
+
+    void updateActionStatus(@Param("actionLogId")Long actionLogId, @Param("status") int status);
+
+    RdosStreamActionLog findActionLogById(@Param("actionLogId")Long actionLogId);
+
 
 }
