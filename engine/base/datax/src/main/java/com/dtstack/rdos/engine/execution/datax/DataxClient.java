c7b58a7b4cf3f56034b141779ec6689df8b37d70
sishu@dtstack.com <sishu@dtstack.com>
2017-05-15 19:26:38 +0800

    ysq

--- engine/base/datax/src/main/java/com/dtstack/rdos/engine/execution/datax/DataxClient.java
+++ engine/base/datax/src/main/java/com/dtstack/rdos/engine/execution/datax/DataxClient.java
@@ -1,6 +1,8 @@
 package com.dtstack.rdos.engine.execution.datax;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,20 +10,30 @@ import com.dtstack.rdos.engine.execution.base.AbsClient;
 import com.dtstack.rdos.engine.execution.base.JobClient;
 import com.dtstack.rdos.engine.execution.base.enumeration.RdosTaskStatus;
 import com.dtstack.rdos.engine.execution.base.pojo.JobResult;
+import com.google.common.collect.Lists;
 
 /**
  * 
  * @author sishu.yss
  *
  */
-public class SyncClient extends AbsClient{
+public class DataxClient extends AbsClient{
 	
-    private static final Logger logger = LoggerFactory.getLogger(SyncClient.class);
+    private static final Logger logger = LoggerFactory.getLogger(DataxClient.class);
+    
+    private static String userName;
+    
+    private static String password;
+    
+    private static List<String> dataxAddresses  = Lists.newArrayList();
 
 	@Override
 	public void init(Properties prop) {
 		// TODO Auto-generated method stub
-		
+		userName = prop.getProperty("userName");
+		password = prop.getProperty("password");
+		String das = prop.getProperty("dataxAddresses");
+		dataxAddresses.addAll(Arrays.asList(das.split(",")));
 	}
 
 	@Override
