21f17ca50b453f70a25cf55ea30112357270b38e
toutian <toutian@dtstack.com>
2017-05-25 16:47:12 +0800

    errMsg 去掉

--- engine/base/flink/src/main/java/com/dtstack/rdos/engine/execution/flink120/FlinkClient.java
+++ engine/base/flink/src/main/java/com/dtstack/rdos/engine/execution/flink120/FlinkClient.java
@@ -197,8 +197,7 @@ public class FlinkClient extends AbsClient {
         Object jarPath = properties.get(JOB_JAR_PATH_KEY);
         if(jarPath == null){
             logger.error("can not submit a job without jarpath, please check it");
-            JobResult jobResult = JobResult.newInstance(true);
-            jobResult.setData("errMsg", "can not submit a job without jarpath, please check it");
+            JobResult jobResult = JobResult.createErrorResult("can not submit a job without jarpath, please check it");
             return jobResult;
         }
 
@@ -213,8 +212,7 @@ public class FlinkClient extends AbsClient {
         try{
             packagedProgram = FlinkUtil.buildProgram((String) jarPath, tmpFileDirPath, classpaths, entryPointClass, programArgs, spSettings);
         }catch (Exception e){
-            JobResult jobResult = JobResult.newInstance(true);
-            jobResult.setData("errMsg", e.getMessage());
+            JobResult jobResult = JobResult.createErrorResult(e);
             logger.error("", e);
             return jobResult;
         }
