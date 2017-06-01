b4961ad599b8eff9a42243d3fdec1348544103a5
sishu@dtstack.com <sishu@dtstack.com>
2017-06-01 16:24:31 +0800

    ysq

--- engine/service/src/main/java/com/dtstack/rdos/engine/db/callback/MyBatisConnectionFactory.java
+++ engine/service/src/main/java/com/dtstack/rdos/engine/db/callback/MyBatisConnectionFactory.java
@@ -1,5 +1,7 @@
 package com.dtstack.rdos.engine.db.callback;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.Reader;
 
 import org.apache.ibatis.io.Resources;
@@ -18,18 +20,19 @@ import com.dtstack.rdos.commom.exception.ExceptionUtil;
  *
  */
 public class MyBatisConnectionFactory {
- 
+
+    private static  String resource = System.getProperty("user.dir") + "/conf/mybatis/config.xml";
+
     private static SqlSessionFactory sqlSessionFactory;
-    
+
     private static Logger logger = LoggerFactory.getLogger(MyBatisConnectionFactory.class);
  
     static {
         try {
-            String resource = "mybatis/config.xml";
-            Reader reader = Resources.getResourceAsReader(resource);
+            FileInputStream input = new FileInputStream(new File(resource));
  
             if (sqlSessionFactory == null) {
-                sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+                sqlSessionFactory = new SqlSessionFactoryBuilder().build(input);
             }
         }
         catch (Exception e) {
