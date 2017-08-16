fb1d7e8a7993140dca946584860194442d9a6508
jingzhen <jingzhen@dtstack.com>
2017-08-16 16:28:39 +0800

    for debug

--- engine/base/common/src/main/java/com/dtstack/rdos/engine/execution/base/JobSubmitExecutor.java
+++ engine/base/common/src/main/java/com/dtstack/rdos/engine/execution/base/JobSubmitExecutor.java
@@ -202,14 +202,17 @@ public class JobSubmitExecutor{
                     return;
                 }
                 try {
+                    logger.info("debuginfo");
                     jobClient.setOperators(SqlParser.parser(jobClient.getEngineType(), jobClient.getComputeType().getComputeType(), jobClient.getSql()));
                     jobClient.setConfProperties(PublicUtil.stringToProperties(jobClient.getTaskParams()));
                     Thread.currentThread().setContextClassLoader(clusterClient.getClass().getClassLoader());
+                    logger.info("beforesubmit");
                     jobResult = clusterClient.submitJob(jobClient);
                     logger.info("submit job result is:{}.", jobResult);
                     String jobId = jobResult.getData(JobResult.JOB_ID_KEY);
                     jobClient.setEngineTaskId(jobId);
                 }catch (Exception e){//捕获未处理异常,防止跳出执行线程
+                    e.printStackTrace();
                     jobClient.setEngineTaskId(null);
                     jobResult = JobResult.createErrorResult(e);
                     logger.error("get unexpected exception", e);
