46a8d68504e896596d86d74c41cd1cae2eba03de
zoudaokoulife <zoudaokoulife@gmail.com>
2017-11-16 15:06:07 +0800

    fix 判断flink 挂掉的返回信息

--- engine/base/common/src/main/java/com/dtstack/rdos/engine/execution/base/util/EngineRestParseUtil.java
+++ engine/base/common/src/main/java/com/dtstack/rdos/engine/execution/base/util/EngineRestParseUtil.java
@@ -304,7 +304,7 @@ public class EngineRestParseUtil {
 		
 		public final static String NORESOURCE_AVAIABLE_EXCEPYION = "org.apache.flink.runtime.jobmanager.scheduler.NoResourceAvailableException: Not enough free slots available to run the job";
 
-		public final static String FLINK_ENGINE_DOWN = "Failed to retrieve the JobManager gateway";
+		public final static String FLINK_ENGINE_DOWN = "Could not connect to the leading JobManager";
 
 		private final static ObjectMapper objMapper = new ObjectMapper();
 
