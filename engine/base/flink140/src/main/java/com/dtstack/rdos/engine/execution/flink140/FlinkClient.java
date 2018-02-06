5923274e33c50dc87abdfe2d2cfa755f4d0f73c7
jingzhen <jingzhen@dtstack.com>
2018-02-06 14:07:15 +0800

    打日志

--- engine/base/flink140/src/main/java/com/dtstack/rdos/engine/execution/flink140/FlinkClient.java
+++ engine/base/flink140/src/main/java/com/dtstack/rdos/engine/execution/flink140/FlinkClient.java
@@ -816,6 +816,7 @@ public class FlinkClient extends AbsClient {
      * @return
      */
     private String getReqUrl(){
+        System.out.println("hyf getrequrl=" + client.getWebInterfaceURL());
         return client.getWebInterfaceURL();
     }
 
