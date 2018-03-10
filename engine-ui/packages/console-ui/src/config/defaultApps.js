89da459d14b06229f4318778132d598cefb31890
小威 <xiaowei@dtstack.com>
2018-03-10 19:02:25 +0800

    update

--- engine-ui/packages/console-ui/src/config/defaultApps.js
+++ engine-ui/packages/console-ui/src/config/defaultApps.js
@@ -8,13 +8,13 @@ module.exports = [
         link: 'index.html',
         filename: 'index.html',
         target: '_self',
-        enable: true,
+        enable: false,
         apiBase: '/main',
     }, {
         id: 'rdos',
         name: '开发套件',
-        filename: 'rdos.html',
-        link: 'rdos.html#/',
+        filename: 'index.html',
+        link: '#/',
         target: '_blank',
         enable: true,
         hasProject: true, // 是否拥有项目选择
@@ -26,7 +26,7 @@ module.exports = [
         link: 'dataQuality.html#/',
         filename: 'dataQuality.html',
         target: '_blank',
-        enable: true,
+        enable: false,
         apiBase: '/dataQuality',
     },{
         id: 'api',
