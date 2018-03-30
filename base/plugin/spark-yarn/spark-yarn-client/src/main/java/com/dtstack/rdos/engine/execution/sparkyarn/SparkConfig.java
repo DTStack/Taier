84dbd38a1cb447609b5fa708c6295a082fca14df
sishu@dtstack.com <sishu@dtstack.com>
2018-03-30 14:09:39 +0800

    ysq

--- base/plugin/spark-yarn/spark-yarn-client/src/main/java/com/dtstack/rdos/engine/execution/sparkyarn/SparkConfig.java
+++ base/plugin/spark-yarn/spark-yarn-client/src/main/java/com/dtstack/rdos/engine/execution/sparkyarn/SparkConfig.java
@@ -25,6 +25,18 @@ public class SparkConfig {
 
     private static String spark_master="yarn";
 
+    //executor 向driver 发送心跳的间隔时间
+    private static final String spark_executor_heartbeartinterval = "600s";
+
+    //spark 所有网络传输的超时时间
+    private static final String spark_network_timeout = "600s";
+
+    //rpc 请求操作在超时前等待的持续时间
+    private static final String spark_rpc_ask_timeout = "600s";
+
+    //如果设置为 "true" , 则执行任务的推测执行. 这意味着如果一个或多个任务在一个阶段中运行缓慢, 则将重新启动它们
+    private static final String spark_speculation = "true";
+
     public static void initDefautlConf(SparkConf sparkConf){
         try{
             Field[] fields = SparkConfig.class.getDeclaredFields();
