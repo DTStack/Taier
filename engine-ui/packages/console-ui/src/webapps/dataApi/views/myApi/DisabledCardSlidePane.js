ffd7aaaa40b196cc66da1e1b505569c8256809da
Xiao Kang <xiaokang@dtstack.com>
2018-04-24 20:15:42 +0800

    bug fix

--- engine-ui/packages/console-ui/src/webapps/dataApi/views/myApi/DisabledCardSlidePane.js
+++ engine-ui/packages/console-ui/src/webapps/dataApi/views/myApi/DisabledCardSlidePane.js
@@ -55,7 +55,7 @@ class DisabledCardSlidePane extends Component {
                 onClose={this.props.closeSlidePane}>
                 <h1 className="box-title approved-card-pane-title">详情</h1>
                 <div style={{ paddingLeft: 30, paddingTop: 20 }}>
-                    <p>管理员已禁用此接口，您可以联系管理员咨询详细情况，管理员信息</p>
+                    <p>管理员已取消此接口的授权，您可以联系管理员咨询详细情况，管理员信息</p>
                     <p style={{ marginTop: 20 }}>用户名：{this.state.dtuicUserId}</p>
                     <p>手机号码：{this.state.phone}</p>
                     <p>邮箱：{this.state.email}</p>
