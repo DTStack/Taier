eecb4ed173b4a3dd6c6310aacb7f3358b25c6ddb
小威 <xiaowei@dtstack.com>
2018-04-26 10:04:32 +0800

    DTinsight.ide_v1.7.0 DTinsight.valid_v1.0.2 DTinsight.api_v1.0.0

--- engine-ui/packages/console-ui/dist/public/dataQuality/config/config.js
+++ engine-ui/packages/console-ui/dist/public/dataQuality/config/config.js
@@ -3,8 +3,7 @@
  */
 window.APP_CONF = {
     API_BASE_URL: '/api/dq/service', // API请求根路径
-    API_SERVER: 'http://172.16.8.104:8089',
     UIC_URL: 'http://dtuic.dtstack.net',  // UIC中心地址
     UIC_DOMAIN: '.dtstack.net', // UIC域名
-    VERSION: 'dataQuality 0.0.9', // 版本
+    VERSION: 'v1.0.0', // 版本
 }
