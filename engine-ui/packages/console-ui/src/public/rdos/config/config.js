2e91b086200342ffcedbe010b64ee1757b92af3e
Xiao Kang <xiaokang@dtstack.com>
2018-05-09 11:42:47 +0800

    bug fix

--- engine-ui/packages/console-ui/src/public/rdos/config/config.js
+++ engine-ui/packages/console-ui/src/public/rdos/config/config.js
@@ -2,7 +2,7 @@
  * 应用全局配置文件
  */
 window.APP_CONF = {
-    UIC_URL: 'http://dtuic.dtstack.net',  // UIC中心地址
+    UIC_URL: 'http://dtuic.dtstack.net:8090',  // UIC中心地址
     UIC_DOMAIN: '.dtstack.net', // UIC域名
     VERSION: 'v1.6.0', // 版本
 }
