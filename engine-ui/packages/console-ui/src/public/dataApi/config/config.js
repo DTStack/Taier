a8dc2f895fb33cf36367ef67bed63418c0cc9b9f
Xiao Kang <xiaokang@dtstack.com>
2018-05-09 11:49:06 +0800

    fi

--- engine-ui/packages/console-ui/src/public/dataApi/config/config.js
+++ engine-ui/packages/console-ui/src/public/dataApi/config/config.js
@@ -3,7 +3,7 @@
  */
 window.APP_CONF = {
     API_BASE_URL: '/api/dataApi/service', // API请求根路径
-    UIC_URL: 'http://dtuic.dtstack.net:8090',  // UIC中心地址
+    UIC_URL: 'http://dtuic.dtstack.net',  // UIC中心地址
     UIC_DOMAIN: '.dtstack.net', // UIC域名
     VERSION: 'v1.0.0', // 版本
 }
