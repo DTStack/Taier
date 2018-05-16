2593849bae0760175df65922bb21416268562f1e
小威 <xiaowei@dtstack.com>
2018-05-16 14:31:29 +0800

    bugfix

--- engine-ui/packages/console-ui/src/webapps/main/views/admin/role/index.js
+++ engine-ui/packages/console-ui/src/webapps/main/views/admin/role/index.js
@@ -22,6 +22,7 @@ class AdminRole extends Component {
         data: '',
         projects: [],
         selectedProject: '',
+        currentPage: 1,
         loading: 'success',
     }
 
@@ -39,12 +40,12 @@ class AdminRole extends Component {
     loadData = () => {
         this.setState({ loading: 'loading' })
 
-        const { active, selectedProject } = this.state
+        const { active, selectedProject, currentPage } = this.state
         const app = active;
 
         const params = {
             pageSize: 10,
-            currentPage: 1,
+            currentPage: currentPage,
         }
 
         if (!selectedProject && hasProject(app)) {
@@ -99,6 +100,12 @@ class AdminRole extends Component {
             this.props.router.replace("/admin/role?app="+key)
             this.loadData();
         })
+    }
+
+    handleTableChange = (pagination, filters) => {
+        this.setState({ 
+            currentPage: pagination.current,
+        }, this.loadData)
     }
 
     onProjectSelect = (value) => {
@@ -193,6 +200,12 @@ class AdminRole extends Component {
             </Button>
         )
 
+        const pagination = {
+            total: data && data.totalCount,
+            defaultPageSize: 10,
+            current: data.currentPage,
+        };
+
         return (
             <Card 
                 bordered={false}
@@ -203,9 +216,11 @@ class AdminRole extends Component {
                 <Table 
                     rowKey="id"
                     className="m-table"
-                    columns={this.initColums()} 
+                    columns={this.initColums()}
                     loading={loading === 'loading'}
-                    dataSource={ data ? data.data : [] } 
+                    dataSource={ data ? data.data : [] }
+                    pagination={pagination}
+                    onChange={this.handleTableChange}
                 />
             </Card>
         )
