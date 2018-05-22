c199207aa5af3c534af87662b4a7b3117c3e2cc0
Xiao Kang <xiaokang@dtstack.com>
2018-05-22 11:53:21 +0800

    offspring

--- engine-ui/packages/console-ui/src/webapps/rdos/views/layout/header.js
+++ engine-ui/packages/console-ui/src/webapps/rdos/views/layout/header.js
@@ -140,7 +140,7 @@ class Header extends Component {
                                 <span className="my-menu-item">
                                 {
                                     project && project.projectName ? 
-                                        (project.projectAlias || project.projectName) : <span className="menu-text-ellipsis">项目选择</span>
+                                    <span className="menu-text-ellipsis">{(project.projectAlias || project.projectName)}</span> : <span className="menu-text-ellipsis">项目选择</span>
                                 } <Icon type="caret-down" /></span>
                             }
                         >
