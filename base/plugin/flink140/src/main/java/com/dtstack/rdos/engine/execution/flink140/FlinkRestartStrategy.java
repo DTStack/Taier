ca008cd63c745f43150ec5c5a1ba451ee684fc61
sishu@dtstack.com <sishu@dtstack.com>
2018-06-09 20:21:48 +0800

    ysq

--- base/plugin/flink140/src/main/java/com/dtstack/rdos/engine/execution/flink140/FlinkRestartStrategy.java
+++ base/plugin/flink140/src/main/java/com/dtstack/rdos/engine/execution/flink140/FlinkRestartStrategy.java
@@ -1,8 +1,10 @@
 package com.dtstack.rdos.engine.execution.flink140;
 
+import avro.shaded.com.google.common.collect.Lists;
 import com.dtstack.rdos.engine.execution.base.IClient;
 import com.dtstack.rdos.engine.execution.base.restart.IRestartStrategy;
 import org.apache.commons.lang3.StringUtils;
+import java.util.List;
 
 /**
  * Reason:
@@ -18,6 +20,8 @@ public class FlinkRestartStrategy implements IRestartStrategy {
     private final static String FLINK_ENGINE_DOWN = "Could not connect to the leading JobManager";
 
     private final static String FLINK_NO_RESOURCE_AVAILABLE_EXCEPTION = "org.apache.flink.runtime.jobmanager.scheduler.NoResourceAvailableException: Not enough free slots available to run the job";
+
+    private static List<String> errorMsgs = Lists.newArrayList(FLINK_ENGINE_DOWN,"TaskManager was lost/killed");
 
     @Override
     public boolean checkFailureForEngineDown(String msg) {
@@ -41,8 +45,12 @@ public class FlinkRestartStrategy implements IRestartStrategy {
         //Flink 只能资源部足的的任务做重启---不限制重启次数
         String reqURL = String.format(FLINK_EXCEPTION_URL, engineJobId);
         String msg = client.getMessageByHttp(reqURL);
-        if(StringUtils.isNotBlank(msg) && msg.contains(FLINK_NO_RESOURCE_AVAILABLE_EXCEPTION)){
-            return true;
+        if(StringUtils.isNotBlank(msg)){
+            for(String emsg:errorMsgs){
+                if(msg.contains(emsg)){
+                    return true;
+                }
+            }
         }
 
         return false;
