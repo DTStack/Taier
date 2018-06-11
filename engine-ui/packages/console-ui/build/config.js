4d2dc51a93d18d868841f84ca510c528d46fe882
smileskydoge <1006442647@qq.com>
2018-06-11 14:30:44 +0800

    sqlFormat searchTable tableEditor接口改变

--- engine-ui/packages/console-ui/build/config.js
+++ engine-ui/packages/console-ui/build/config.js
@@ -14,8 +14,8 @@ module.exports = {
             },
             "/api/rdos": {
                 // target: "http://172.16.8.104:9020", // formal test ser
-                target: "http://172.16.1.101:9020", // local
-                // target: "http://172.16.8.104:9020",
+                // target: "http://172.16.1.101:9020", // local
+                target: "http://172.16.8.104:9020",
                 // ignorePath: true, 
                 changeOrigin: true,
                 secure: false,
