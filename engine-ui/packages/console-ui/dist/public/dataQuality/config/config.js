69f7ace8fef31a49f36ec0c50359fdccb815292c
小威 <xiaowei@dtstack.com>
2018-06-14 11:59:39 +0800

    update

---
+++ engine-ui/packages/console-ui/dist/public/dataQuality/config/config.js
@@ -0,0 +1,8 @@
+/**
+ * 应用全局配置文件
+ */
+window.APP_CONF = {
+    UIC_URL: 'http://dtuic.dtstack.net',  // UIC中心地址
+    UIC_DOMAIN: '.dtstack.net', // UIC域名
+    API_SERVER:"http://172.16.8.104:9020"
+}
