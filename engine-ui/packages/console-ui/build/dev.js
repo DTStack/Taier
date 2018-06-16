bcb993ad992f675a36bb1c2308a68a95e9f14cba
zzg <xiaozhi@dtstack.com>
2018-06-16 19:11:11 +0800

    conflict

--- engine-ui/packages/console-ui/build/dev.js
+++ engine-ui/packages/console-ui/build/dev.js
@@ -2,12 +2,14 @@ const path = require('path');
 const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const cssLoader = require("./loader/css-loader.js").dev;
+const notifier = require('node-notifier')
 
 const MY_PATH = require('./consts');
 
 const baseConf = require('./base.js')();
 var config = require('./config');
-
 baseConf.output = {
     path: MY_PATH.BUILD_PATH,
     chunkFilename: '[name].js',
@@ -15,25 +17,37 @@ baseConf.output = {
     sourceMapFilename: '[name].map',
     publicPath: '/'
 }
-
+baseConf.mode = "development"
 baseConf.plugins.push(
-    new webpack.DefinePlugin({
-        'process.env': {
-            'NODE_ENV': JSON.stringify('development'),
+    new FriendlyErrorsWebpackPlugin({
+        clearConsole:true,
+        onErrors: function (severity, errors) {
+            if (severity !== 'error') return
+
+            const error = errors[0]
+            const filename = error.file && error.file.split('!').pop()
+
+            notifier.notify({
+                title: "构建出错啦",
+                message: severity + ': ' + error.name,
+                subtitle: filename || '',
+                icon: path.join(__dirname, 'icon.png')
+            })
+
         }
     }),
     new webpack.SourceMapDevToolPlugin({
-        filename: '[file].map'
+        filename: '[file].map',
+        columns: false,
     }),
     new webpack.HotModuleReplacementPlugin(), // 开启全局的模块热替换(HMR)
-    new webpack.NamedModulesPlugin(), // 当模块热替换(HMR)时在浏览器控制台输出对用户更友好的模块名字信息
 )
 
 
 const htmlPlugs = [];
 function loadHtmlPlugs() {
     const appConfs = require(path.resolve(MY_PATH.APP_PATH, 'config/defaultApps'));
-    for (var i = 0 ; i < appConfs.length; i++) {
+    for (var i = 0; i < appConfs.length; i++) {
         const app = appConfs[i];
         if (app.enable) {
             const tmp = path.resolve(MY_PATH.WEB_PUBLIC, `${app.id}/index.html`)
@@ -42,7 +56,7 @@ function loadHtmlPlugs() {
                     filename: app.filename,
                     template: tmp,
                     inject: 'body',
-                    chunks: ['vendor', app.id, 'manifest'],
+                    chunks: [app.id, 'manifest'],
                     showErrors: true,
                     hash: true,
                 })
@@ -53,28 +67,32 @@ function loadHtmlPlugs() {
 
 loadHtmlPlugs();
 
-const devServer = Object.assign(
-    {
-        hot: true, // 开启服务器的模块热替换
-        host: '0.0.0.0',
-        port: 8080,
-        historyApiFallback: true,
-        stats: {
-            colors: true,
-            'errors-only': true,
-            cached: true,
-        },
-        contentBase: baseConf.output.path,
-        publicPath: baseConf.output.publicPath
+const devServer = Object.assign({
+    hot: true, // 开启服务器的模块热替换
+    host: '0.0.0.0',
+    port: 8080,
+    historyApiFallback: true,
+    disableHostCheck: true,
+    quiet: true,
+    stats: {
+        colors: true,
+        'errors-only': true,
+        cached: true,
     },
-    config.server
-)
+    contentBase: baseConf.output.path,
+    publicPath: baseConf.output.publicPath
+}, config.server)
 
-const merged = function(env) {
+const merged = function (env) {
     return webpackMerge(baseConf, {
-        devtool: 'cheap-module-eval-source-map', //
         devServer: devServer,
-        plugins: htmlPlugs
+        plugins: htmlPlugs,
+        module: {
+            rules: [
+                ...cssLoader
+            ]
+        }
+
     })
 }
 
