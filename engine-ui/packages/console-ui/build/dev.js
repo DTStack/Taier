630df0c2dfe0ce0c3012dd2def721de5e8b7281a
小威 <xiaowei@dtstack.com>
2018-06-26 11:54:48 +0800

    build

--- engine-ui/packages/console-ui/build/dev.js
+++ engine-ui/packages/console-ui/build/dev.js
@@ -1,100 +1,104 @@
-const path = require('path');
-const webpack = require('webpack');
-const webpackMerge = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const webpackMerge = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
 const cssLoader = require("./loader/css-loader.js").dev;
-const notifier = require('node-notifier')
+const notifier = require("node-notifier");
 
-const MY_PATH = require('./consts');
+const MY_PATH = require("./consts");
 
-const baseConf = require('./base.js')();
-var config = require('./config');
+const baseConf = require("./base.js")();
+var config = require("./config");
 baseConf.output = {
     path: MY_PATH.BUILD_PATH,
-    chunkFilename: '[name].js',
-    filename: '[name].js',
-    sourceMapFilename: '[name].map',
-    publicPath: '/'
-}
-baseConf.mode = "development"
+    chunkFilename: "[name].js",
+    filename: "[name].js",
+    sourceMapFilename: "[name].map",
+    publicPath: "/"
+};
+baseConf.mode = "development";
 baseConf.plugins.push(
     new FriendlyErrorsWebpackPlugin({
-        clearConsole:true,
-        onErrors: function (severity, errors) {
-            if (severity !== 'error') return
+        clearConsole: true,
+        onErrors: function(severity, errors) {
+            if (severity !== "error") return;
 
-            const error = errors[0]
-            const filename = error.file && error.file.split('!').pop()
+            const error = errors[0];
+            const filename = error.file && error.file.split("!").pop();
 
             notifier.notify({
                 title: "构建出错啦",
-                message: severity + ': ' + error.name,
-                subtitle: filename || '',
-                icon: path.join(__dirname, 'icon.png')
-            })
-
+                message: severity + ": " + error.name,
+                subtitle: filename || "",
+                icon: path.join(__dirname, "icon.png")
+            });
         }
     }),
     new webpack.SourceMapDevToolPlugin({
-        filename: '[file].map',
-        columns: false,
+        filename: "[file].map",
+        columns: false
     }),
-    new webpack.HotModuleReplacementPlugin(), // 开启全局的模块热替换(HMR)
-)
-
+    new webpack.HotModuleReplacementPlugin() // 开启全局的模块热替换(HMR)
+);
 
 const htmlPlugs = [];
 function loadHtmlPlugs() {
-    const appConfs = require(path.resolve(MY_PATH.APP_PATH, 'config/defaultApps'));
+    const appConfs = require(path.resolve(
+        MY_PATH.APP_PATH,
+        "config/defaultApps"
+    ));
     for (var i = 0; i < appConfs.length; i++) {
         const app = appConfs[i];
         if (app.enable) {
-            const tmp = path.resolve(MY_PATH.WEB_PUBLIC, `${app.id}/index.html`)
+            const tmp = path.resolve(
+                MY_PATH.WEB_PUBLIC,
+                `${app.id}/index.html`
+            );
             htmlPlugs.push(
                 new HtmlWebpackPlugin({
                     filename: app.filename,
                     template: tmp,
-                    inject: 'body',
-                    chunks: [app.id, 'manifest'],
+                    inject: "body",
+                    chunks: [app.id, "manifest"],
                     showErrors: true,
-                    hash: true,
+                    hash: true
                 })
-            )
+            );
         }
     }
 }
 
 loadHtmlPlugs();
 
-const devServer = Object.assign({
-    hot: true, // 开启服务器的模块热替换
-    host: '0.0.0.0',
-    port: 8080,
-    historyApiFallback: true,
-    disableHostCheck: true,
-    quiet: true,
-    stats: {
-        colors: true,
-        'errors-only': true,
-        cached: true,
+const devServer = Object.assign(
+    {
+        hot: true, // 开启服务器的模块热替换
+        host: "0.0.0.0",
+        port: 8080,
+        historyApiFallback: true,
+        disableHostCheck: true,
+        quiet: true,
+        stats: {
+            colors: true,
+            "errors-only": true,
+            cached: true
+        },
+        contentBase: baseConf.output.path,
+        publicPath: baseConf.output.publicPath
     },
-    contentBase: baseConf.output.path,
-    publicPath: baseConf.output.publicPath
-}, config.server)
+    config.server
+);
 
-const merged = function (env) {
+const merged = function(env) {
     return webpackMerge(baseConf, {
-        devtool: 'cheap-module-eval-source-map', //
+        devtool: "cheap-module-eval-source-map", //
         devServer: devServer,
         plugins: htmlPlugs,
         module: {
-            rules: [
-                ...cssLoader
-            ]
+            rules: [...cssLoader]
         }
-
-    })
-}
+    });
+};
 
 module.exports = merged;
