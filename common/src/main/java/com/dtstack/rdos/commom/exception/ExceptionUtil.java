7ec09c2335b0c58596cfd496e1583a65a072eddd
sishu@dtstack.com <sishu@dtstack.com>
2018-07-03 11:16:10 +0800

    ysq

--- common/src/main/java/com/dtstack/rdos/commom/exception/ExceptionUtil.java
+++ common/src/main/java/com/dtstack/rdos/commom/exception/ExceptionUtil.java
@@ -12,36 +12,35 @@ import java.io.PrintWriter;
  */
 public class ExceptionUtil {
 	
-//    public static String getErrorMessage(Throwable e) {
-//            StringBuffer sb = new StringBuffer();
-//            Throwable next = e.getCause();
-//            if (next != null) {
-//                    sb.append(next.toString() + "\n");
-//                    StackTraceElement[] nexts = next.getStackTrace();
-//                    for (StackTraceElement st : nexts) {
-//                            sb.append(st.getClassName() + "." + st.getMethodName() + "("
-//                                            + st.getLineNumber() + ")" + "\n");
-//                    }
-//            } else {
-//                    sb.append(e.toString() + "\n");
-//                    StackTraceElement[] sts = e.getStackTrace();
-//                    if (sts != null) {
-//                            for (StackTraceElement st : sts) {
-//                                    sb.append(st.getClassName() + "." + st.getMethodName()
-//                                                    + "(" + st.getLineNumber() + ")" + "\n");
-//                            }
-//                    }
-//            }
-//            return sb.toString();
-//    }
+    public static String getErrorMessage(Throwable e) {
+            StringBuffer sb = new StringBuffer();
+            sb.append(e.toString() + "\n");
+            StackTraceElement[] sts = e.getStackTrace();
+            if (sts != null) {
+                    for (StackTraceElement st : sts) {
+                            sb.append(st.getClassName() + "." + st.getMethodName()
+                                    + "(" + st.getLineNumber() + ")" + "\n");
+                    }
+            }
+            Throwable next = e.getCause();
+            if (next != null) {
+                    sb.append(next.toString() + "\n");
+                    StackTraceElement[] nexts = next.getStackTrace();
+                    for (StackTraceElement st : nexts) {
+                            sb.append(st.getClassName() + "." + st.getMethodName() + "("
+                                            + st.getLineNumber() + ")" + "\n");
+                    }
+            }
+            return sb.toString();
+    }
 
-        public static String getErrorMessage(Throwable e) {
-                StringWriter sw = new StringWriter();
-                PrintWriter pw = new PrintWriter(sw, true);
-                e.printStackTrace(pw);
-                pw.flush();
-                sw.flush();
-                return sw.toString();
-        }
+//        public static String getErrorMessage(Throwable e) {
+//                StringWriter sw = new StringWriter();
+//                PrintWriter pw = new PrintWriter(sw, true);
+//                e.printStackTrace(pw);
+//                pw.flush();
+//                sw.flush();
+//                return sw.toString();
+//        }
 
 }
