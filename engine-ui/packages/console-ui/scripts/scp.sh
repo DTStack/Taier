e4ae3d92cdb275877c0b3cd777164823c80426d1
小威 <xiaowei@dtstack.com>
2018-07-06 14:42:31 +0800

    scp uploader

--- engine-ui/packages/console-ui/scripts/scp.sh
+++ engine-ui/packages/console-ui/scripts/scp.sh
@@ -4,5 +4,9 @@ source="./dist"
 target="/home/admin/app/rdos.front/dist"
 server="172.16.8.104"
 
+echo "Uploading $source to $server:$target"
+
 ### 上传文件到指定服务器
 scp -P 22 -r $source root@$server:$target
+
+echo "Upload over."
