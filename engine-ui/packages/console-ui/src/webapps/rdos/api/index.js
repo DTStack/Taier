79b9f7b98aaba46b02046dee23fec2500b5be8b1
smileskydoge <1006442647@qq.com>
2018-07-06 17:05:45 +0800

    交互接口大部分调完

--- engine-ui/packages/console-ui/src/webapps/rdos/api/index.js
+++ engine-ui/packages/console-ui/src/webapps/rdos/api/index.js
@@ -98,7 +98,12 @@ export default {
     getProjectInfo(params) {
         return http.post(req.GET_PROJECT_INFO, params)
     },
-
+    getProjectListInfo(params) {
+        return http.post(req.GET_PROJECT_LIST_INFO, params)
+    },
+    setSticky(params) {
+        return http.post(req.SET_STICKY, params)
+    },
     // ========== Role ========== //
     getRoleList(params) {
         return http.post(req.GET_ROLE_LIST, params)
