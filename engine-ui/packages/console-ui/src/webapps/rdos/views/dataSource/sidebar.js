21b7ead36862cae52c5c7ace6471513f2dd54416
smileskydoge <1006442647@qq.com>
2018-08-06 17:06:52 +0800

    跳转错误bug fixed

--- engine-ui/packages/console-ui/src/webapps/rdos/views/dataSource/sidebar.js
+++ engine-ui/packages/console-ui/src/webapps/rdos/views/dataSource/sidebar.js
@@ -7,7 +7,7 @@ export default class Sidebar extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            current: 'offline',
+            current: 'offLineData',
         }
     }
 
@@ -31,7 +31,7 @@ export default class Sidebar extends Component {
                 current = current.split('/')[0];
             }
             
-            this.setState({ current: current||"offline" })
+            this.setState({ current: current||"offLineData" })
         }
     }
 
@@ -51,14 +51,14 @@ export default class Sidebar extends Component {
                   onClick={this.handleClick}
                   style={{ height: '100%' }}
                   selectedKeys={[this.state.current]}
-                  defaultSelectedKeys={['offline']}
+                  defaultSelectedKeys={['offLineData']}
                   mode='inline'
                 >
-                    <Menu.Item key="offline">
-                        <Link to={`/database/offline`}>离线数据源</Link>
+                    <Menu.Item key="offLineData">
+                        <Link to={`/database/offLineData`}>离线数据源</Link>
                     </Menu.Item>
-                    <Menu.Item key="stream">
-                        <Link to={`/database/stream`}>实时数据源</Link>
+                    <Menu.Item key="streamData">
+                        <Link to={`/database/streamData`}>实时数据源</Link>
                     </Menu.Item>
                 </Menu>
             </div>
