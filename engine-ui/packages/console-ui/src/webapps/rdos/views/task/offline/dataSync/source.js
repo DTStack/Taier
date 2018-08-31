08d95c2a08efbb4faff26c5a26b096393559ed58
smileskydoge <1006442647@qq.com>
2018-08-31 16:04:25 +0800

    bug fixed

--- engine-ui/packages/console-ui/src/webapps/rdos/views/task/offline/dataSync/source.js
+++ engine-ui/packages/console-ui/src/webapps/rdos/views/task/offline/dataSync/source.js
@@ -651,7 +651,7 @@ class SourceForm extends React.Component {
                         })(
                             <Input
                                 placeholder="请填写分区"
-                                placeholder="pt=${bdp.system.bizdate},"
+                                placeholder="pt=${bdp.system.bizdate},\"
                                 onChange={this.submitForm.bind(this)}
                             ></Input>,
                         )}
