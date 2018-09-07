bbbc398271ce7f4de46889e41b8acca66de18436
Xiao Kang <xiaokang@dtstack.com>
2018-09-07 11:07:39 +0800

    fix

--- engine-ui/packages/console-ui/src/utils/asyncLoad.js
+++ engine-ui/packages/console-ui/src/utils/asyncLoad.js
@@ -2,30 +2,42 @@ import React from 'react'
 
 /* eslint-disable */
 export default (loader, collection) => (
-  class AsyncComponent extends React.Component {
-      constructor(props) {
-          super(props);
-          this.Component = null;
-          this.state = { Component: AsyncComponent.Component };
-      }
+    class AsyncComponent extends React.Component {
+        constructor(props) {
+            super(props);
+            this.Component = null;
+            this.state = { Component: AsyncComponent.Component, error: false };
+        }
 
-      componentWillMount() {
-          if (!this.state.Component) {
-              loader().then((Component) => {
-                  AsyncComponent.Component = Component;
-                  this.setState({ Component });
-              });
-          }
-      }
+        componentWillMount() {
+            if (!this.state.Component) {
+                loader().then((Component) => {
+                    AsyncComponent.Component = Component;
+                    this.setState({ Component });
+                }).catch(
+                    (e) => {
+                        this.setState({
+                            error: true
+                        })
+                    }
+                );
+            }
+        }
 
-      render() {
-          if (this.state.Component) {
-              return (
-                <this.state.Component { ...this.props } { ...collection } />
-              )
-          }
-          return null;
-      }
+        render() {
+            const {error} = this.state;
+            if (error) {
+                return <div>
+                    <h2 style={{textAlign:"center"}}>服务端该模块更新中，请刷新重试。</h2>
+                </div>
+            }
+            if (this.state.Component) {
+                return (
+                    <this.state.Component {...this.props} {...collection} />
+                )
+            }
+            return null;
+        }
     }
 );
 /* eslint-disable */
