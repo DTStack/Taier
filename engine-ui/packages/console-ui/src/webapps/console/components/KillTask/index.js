9ea1b3c8fdd1f1320d8c9de2a082805312c03d04
zhang <1257450210@qq.com>
2018-09-28 19:54:56 +0800

    c f80%

--- engine-ui/packages/console-ui/src/webapps/console/components/KillTask/index.js
+++ engine-ui/packages/console-ui/src/webapps/console/components/KillTask/index.js
@@ -2,16 +2,55 @@
 * @Author: 12574
 * @Date:   2018-09-18 17:36:36
 * @Last Modified by:   12574
-* @Last Modified time: 2018-09-25 13:14:12
+* @Last Modified time: 2018-09-28 16:53:11
 */
 import React, { Component } from 'react';
-import { Modal } from "antd";
+import { Modal, message } from "antd";
+import Api from "../../api/console";
+
+
 class KillTask extends Component {
 	
-	killtask() {
-		this.setState({
+	// 请求杀任务接口
+	killTask() {
+		const {killResource} = this.props;
+		// console.log(killResource.jobName);
+		// 获取集群
+		var queueName,clusterName,computeTypeInt;
+		const arr = killResource.groupName.split("_");
+		if (arr.length == 1) {
+			clusterName = killResource.groupName
+		} else {
+			for (var i = 0; i<=arr.length;i++) {
+				clusterName = arr[0];
+				queueName = arr[1];
+			}
+		}
 
+		if (killResource.computeType == "BATCH") {
+			computeTypeInt = 1
+		}else {
+			computeTypeInt = 0
+		}
+		Api.killTask({
+			computeTypeInt: computeTypeInt,
+			engineType: killResource.engineType,
+			jobId: killResource.taskId,
+			queueName:queueName,
+			clusterName: clusterName
+		}).then( (res) => {
+			if (res.code == 1) {
+				this.props.killSuccess(killResource.taskId);
+				message.success("删除成功");
+				// 异步,成功之后才能删除
+				this.props.onCancel();
+			}else {
+				message.success("删除失败");
+			}
 		})
+	}
+	confirmKilltask() {
+		this.killTask();	
 	}
 	render() {
 		return (
@@ -19,7 +58,7 @@ class KillTask extends Component {
 				title="杀任务"
 				visible={this.props.visible}
 				onCancel={this.props.onCancel}
-				onOk={this.killtask.bind(this)}
+				onOk={this.confirmKilltask.bind(this)}
 			>
 				<p>是否要删除此任务?</p>
 			</Modal>
--- engine-ui/packages/console-ui/src/webapps/console/components/viewDetail/index.js
+++ engine-ui/packages/console-ui/src/webapps/console/components/viewDetail/index.js
@@ -2,25 +2,42 @@
 * @Author: 12574
 * @Date:   2018-09-18 16:58:30
 * @Last Modified by:   12574
-* @Last Modified time: 2018-09-18 17:45:02
+* @Last Modified time: 2018-09-28 11:43:51
 */
 import React, { Component } from 'react';
-import { Modal } from "antd";
-
+import { Modal, Table } from "antd";
+import moment from "moment";
+import CodeEditor from "widgets/editor";
 class ViewDetail extends Component {
-	
+	state = {
+		editor: {
+            sql: "",
+            cursor: undefined,
+            sync: true
+        }
+	}
 	render() {
+		const {editor} = this.state;
 		return (
 			<Modal
 				title="任务详情"
 				onCancel={this.props.onCancel}
+				onOk={this.props.onCancel}
 				visible={this.props.visible}
 			>
-				<p>Some contents...</p>
-      			<p>Some contents...</p>
-      			<p>Some contents...</p>
-      			<p>Some contents...</p>
-      			<p>Some contents...</p>
+				<CodeEditor
+                        style={{ height: "400px", marginTop: "1px" }}
+                        // onChange={this.props.sqlOnChange}
+                        value={this.props.resource.taskParams}
+                        language="ini"
+                        options={
+                        	{
+                        		readOnly: true
+                        	}
+                        }
+                        // // cursor={this.props.editor.cursor}
+                        sync={true}
+                    />
 			</Modal>
 		)
 	}
