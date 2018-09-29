b2f5e32ad92df001a65171ae6e24c9ec4cce7e90
小威 <xiaowei@dtstack.com>
2018-09-29 22:14:16 +0800

    bugfix

--- engine-ui/packages/console-ui/src/webapps/rdos/views/operation/offline/taskMana.js
+++ engine-ui/packages/console-ui/src/webapps/rdos/views/operation/offline/taskMana.js
@@ -473,7 +473,7 @@ class OfflineTaskMana extends Component {
                             className="m-tabs bd-top bd-right m-slide-pane"
                             onClose={this.closeSlidePane}
                             visible={visibleSlidePane}
-                            style={{ right: '0px', width: '60%', height: '100%', minHeight: '600px', }}
+                            style={{ right: '0px', width: '75%', height: '100%', minHeight: '600px', }}
                         >
                             <Tabs animated={false} onChange={this.onTabChange}>
                                 <TabPane tab="依赖视图" key="taskFlow">
