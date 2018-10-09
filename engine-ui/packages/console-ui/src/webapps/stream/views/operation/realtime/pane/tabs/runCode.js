b2dc9e40db7b1b7dc02d96dd65bd99350a15228f
Xiao Kang <xiaokang@dtstack.com>
2018-10-09 16:40:09 +0800

    fix

--- engine-ui/packages/console-ui/src/webapps/stream/views/operation/realtime/pane/tabs/runCode.js
+++ engine-ui/packages/console-ui/src/webapps/stream/views/operation/realtime/pane/tabs/runCode.js
@@ -1,15 +1,44 @@
 import React from "react"
 
+import { Collapse } from "antd";
+
+import Editor from "widgets/editor"
+
+const Panel = Collapse.Panel;
+
 class RunCode extends React.Component {
-    componentDidMount(){
+
+    componentDidMount() {
         console.log("RunCode")
     }
-   
+
     render() {
+        const { data } = this.props;
         return (
-           <div>
-            RunCode
-           </div>
+            <div>
+                <Collapse defaultActiveKey={['code', 'env']}>
+                    <Panel header="运行代码" key="code">
+                        <Editor
+                            sync={true}
+                            style={{ height: "400px" }}
+                            options={{ readOnly: false }}
+                            language="dtsql"
+                            options={{ readOnly: true }}
+                            value={data.sqlText} 
+                        />
+                    </Panel>
+                    <Panel header="环境参数" key="env">
+                        <Editor
+                            sync={true}
+                            style={{ height: "400px" }}
+                            options={{ readOnly: false }}
+                            language="ini"
+                            options={{ readOnly: true }}
+                            value={data.taskParams} 
+                        />
+                    </Panel>
+                </Collapse>
+            </div>
         )
     }
 }
