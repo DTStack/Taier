577a52edbbbf868ef3cdaca3e7fd34a575ac4397
Xiao Kang <xiaokang@dtstack.com>
2018-10-10 10:20:46 +0800

    add runcode

--- engine-ui/packages/console-ui/src/webapps/stream/views/operation/realtime/pane/tabs/runCode.js
+++ engine-ui/packages/console-ui/src/webapps/stream/views/operation/realtime/pane/tabs/runCode.js
@@ -3,6 +3,7 @@ import React from "react"
 import { Collapse } from "antd";
 
 import Editor from "widgets/editor"
+import { TASK_TYPE } from "../../../../../../stream/comm/const";
 
 const Panel = Collapse.Panel;
 
@@ -11,21 +12,55 @@ class RunCode extends React.Component {
     componentDidMount() {
         console.log("RunCode")
     }
+    getRunCode() {
+        const { data = {} } = this.props;
+        const { taskType, sqlText, taskDesc } = data;
+        switch (taskType) {
 
+            case TASK_TYPE.SQL:
+            case TASK_TYPE.DATA_COLLECTION: {
+                return (
+                    <Editor
+                        sync={true}
+                        style={{ height: "400px" }}
+                        options={{ readOnly: false }}
+                        language={this.getEditorLanguage(taskType)}
+                        options={{ readOnly: true, minimap: { enabled: false } }}
+                        value={sqlText}
+                    />
+                )
+            } 
+            case TASK_TYPE.MR:
+            default: {
+                return (
+                    <span>
+                        MR:{taskDesc}
+                    </span>
+                )
+            }
+
+        }
+    }
+    getEditorLanguage(taskType) {
+        switch (taskType) {
+            case TASK_TYPE.SQL: {
+                return 'sql'
+            }
+            case TASK_TYPE.DATA_COLLECTION: {
+                return 'json'
+            }
+            default: {
+                return 'ini'
+            }
+        }
+    }
     render() {
         const { data } = this.props;
         return (
             <div>
                 <Collapse className="middle-collapse" defaultActiveKey={['code', 'env']}>
                     <Panel header="运行代码" key="code">
-                        <Editor
-                            sync={true}
-                            style={{ height: "400px" }}
-                            options={{ readOnly: false }}
-                            language="dtsql"
-                            options={{ readOnly: true, minimap: { enabled: false } }}
-                            value={data.sqlText} 
-                        />
+                       {this.getRunCode()}
                     </Panel>
                     <Panel header="环境参数" key="env">
                         <Editor
@@ -34,7 +69,7 @@ class RunCode extends React.Component {
                             options={{ readOnly: false }}
                             language="ini"
                             options={{ readOnly: true, minimap: { enabled: false } }}
-                            value={data.taskParams} 
+                            value={data.taskParams}
                         />
                     </Panel>
                 </Collapse>
