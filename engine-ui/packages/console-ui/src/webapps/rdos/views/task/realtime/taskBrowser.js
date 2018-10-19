5d937abdc11d825bd7b348f66b9bdd1651692927
zhang <1257450210@qq.com>
2018-10-19 11:08:40 +0800

    fix

--- engine-ui/packages/console-ui/src/webapps/rdos/views/task/realtime/taskBrowser.js
+++ engine-ui/packages/console-ui/src/webapps/rdos/views/task/realtime/taskBrowser.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Row, Tabs, Modal, Alert, message, Dropdown, Menu, Icon } from 'antd'
+import { Row, Tabs, Modal, Alert, message, Dropdown, Menu, Icon, Tooltip } from 'antd'
 
 import utils from 'utils'
 import { debounce, cloneDeep } from 'lodash';
@@ -83,7 +83,7 @@ class TaskBrowser extends Component {
                 const title = (<span>
                     <TabIcon tabData={pane} />
                     <SyncBadge notSynced={pane.notSynced} />
-                    <span className="tab-ellipsis">{pane.name}</span>
+                    <span className="tab-ellipsis">{<Tooltip title={pane.name}>{pane.name}</Tooltip>}</span>
                 </span>)
                 return (
                     <TabPane
