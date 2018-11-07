2307135c2d06088f55b1b0177f577839647b0cca
toutian <toutian@dtstack.com>
2018-11-07 17:42:21 +0800

    timeout time

--- base/plugin/dt-yarn-shell/dt-yarn-shell-core/src/main/java/com/dtstack/yarn/am/ContainerLostDetector.java
+++ base/plugin/dt-yarn-shell/dt-yarn-shell-core/src/main/java/com/dtstack/yarn/am/ContainerLostDetector.java
@@ -13,7 +13,8 @@ import java.util.concurrent.TimeUnit;
 
 public class ContainerLostDetector implements Runnable {
 
-    private static final long MAX_HEART_BEAT_WAIT_TIME = 5 * 60 * 1000L;
+    //超时3分钟 失败
+    private static final long MAX_HEART_BEAT_WAIT_TIME = 3 * 60 * 1000L;
 
     private ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
 
