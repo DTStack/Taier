96a1d584445c742c85dc37e99a8e4a3acd5e6501
古一 <guyi@dtstack.com>
2018-11-15 16:44:48 +0800

    bugfixxx

--- engine-ui/packages/console-ui/src/webapps/analyticsEngine/views/workbench/mainBench/createTable/stepTwo.js
+++ engine-ui/packages/console-ui/src/webapps/analyticsEngine/views/workbench/mainBench/createTable/stepTwo.js
@@ -62,7 +62,7 @@ const partition_mode = [
 ]
 //string、char、varchar、timestamp、date
 const bucketType = ['STRING','CHAR','VARCHAR','TIMESTAMP','DATE']
-const sortColumn = ['String','DATE','TIMESTAMP','SMALLINT','INT','BIGINT','BOOLEAN','CHAR','VARCHAR']
+const sortColumn = ['STRING','DATE','TIMESTAMP','SMALLINT','INT','BIGINT','BOOLEAN','CHAR','VARCHAR']
 
 
 const decimalPrecision = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38]
