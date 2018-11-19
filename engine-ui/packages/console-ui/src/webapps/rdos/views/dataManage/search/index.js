965005d49429978c3ffbb18b7dd37edd40cd69a3
zhang <1257450210@qq.com>
2018-11-19 21:30:58 +0800

    fix

--- engine-ui/packages/console-ui/src/webapps/rdos/views/dataManage/search/index.js
+++ engine-ui/packages/console-ui/src/webapps/rdos/views/dataManage/search/index.js
@@ -41,13 +41,17 @@ class SearchTable extends Component {
                 tableName,
                 pageSize: 20,
             },
+            ddlList: [],
+            dmlList: [],
         }
     }
 
     componentDidMount() {
         this.search();
+        this.getDdlList();
+        this.getDmlList();
     }
-
+    
     search = () => {
         this.setState({
             cardLoading: true,
@@ -72,6 +76,44 @@ class SearchTable extends Component {
             }
         })
     }
+
+    getDdlList = () => {
+        ajax.getDdlList().then(res => {
+            if(res.code === 1) {
+                const data = res.data;
+                const ddlData = data.map((item) => {
+                    return {
+                        label: item.name,
+                        value: item.value
+                    }
+                })
+                console.log(ddlData)
+                this.setState({
+                    ddlList: ddlData
+                })
+            }
+        })
+    }
+
+    getDmlList = () => {
+        ajax.getDmlList().then(res => {
+            if(res.code === 1) {
+                const data = res.data;
+                const dmlData = data.map((item) => {
+                    return {
+                        label: item.name,
+                        value: item.value
+                    }
+                })
+                console.log(dmlData)
+                this.setState({
+                    dmlList: dmlData
+                })
+            }
+        })
+    }
+
+    
 
     apply = (applyData) => {
         const { editRecord } = this.state;
@@ -115,12 +157,13 @@ class SearchTable extends Component {
         })
     }
 
-    showModal = (editRecord) => {
+    showModal = (record) => {
         this.setState({
             visible: true,
-            editRecord,
+            editRecord: record,
         });
     }
+
 
     characterProcess = (text="",maxWidth="300px") => {
         const style ={overflow: "hidden",
@@ -220,7 +263,8 @@ class SearchTable extends Component {
                             return <span>授权成功</span>
                         case 2:
                             return <span>等待授权</span>
-                        default: return '-';
+                        // default: return '-';
+                        default: return <span><a onClick={() => ctx.showModal(record)}>申请授权</a></span>;
                     }
                 }
             }
@@ -230,7 +274,7 @@ class SearchTable extends Component {
 
 
     render() {
-        const { table, queryParams, visible, editRecord, cardLoading, dataCatalogue } = this.state;
+        const { table, queryParams, visible, editRecord, cardLoading, dataCatalogue, ddlList, dmlList } = this.state;
         const { allProjects } = this.props;
         const marginTop10 = { marginTop: '8px' };
         const projectOptions = allProjects.map(proj => <Option
@@ -321,6 +365,8 @@ class SearchTable extends Component {
                             visible={visible}
                             table={editRecord}
                             onOk={this.apply}
+                            ddlList={ddlList}
+                            dmlList={dmlList}
                             onCancel={() => {this.setState({visible: false, editRecord: {} })}}
                         />
                     </div>
