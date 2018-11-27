494fb864649e4f2cbc5f154789901397cf99263c
Xiao Kang <xiaokang@dtstack.com>
2018-11-27 13:45:46 +0800

    fix realtime taskDetail

--- engine-ui/packages/console-ui/build/config.js
+++ engine-ui/packages/console-ui/build/config.js
@@ -6,7 +6,7 @@ module.exports = {
         host: '0.0.0.0',
         proxy: {
             "/api/dq": {
-                target: "http://172.16.8.104:8089",
+                target: "http://172.16.8.192:8089",
                 // target: "http://172.16.6.135:8089", // tmp server
                 // target: "http://172.16.0.79:8089",
                 // ignorePath: true, 
@@ -15,7 +15,7 @@ module.exports = {
             },
             "/api/rdos": {
                 // target: "http://172.16.10.51:9020", // formal test ser
-                target: "http://172.16.8.104:9020", // formal test ser
+                target: "http://172.16.8.192:9020", // formal test ser
                 // target: "http://172.16.8.162:9020", // tmp test server
                 // ignorePath: true,  
                 // Mock
@@ -30,20 +30,20 @@ module.exports = {
             },
             "/api/da": { // da地址
                 // target: "http://172.16.1.104:8087",
-                target: "http://172.16.8.107:8087",//测试环境
+                target: "http://172.16.8.192:8087",//测试环境
 
                 // pathRewrite:{"^/api/da/service":"/server/index.php?g=Web&c=Mock&o=simple&projectID=5&uri=/api/da"},
                 changeOrigin: true,
                 secure: false,
             },
             "/api/tag": { // 数据标签
-                target: "http://172.16.8.107:8085",//测试环境
+                target: "http://172.16.8.192:8085",//测试环境
                 // pathRewrite:{"^/api/tag/service":"/server/index.php?g=Web&c=Mock&o=simple&projectID=5&uri=/api/tag"},
                 changeOrigin: true,
                 secure: false,
             },
             "/api/console": { // 控制台
-                target: "http://172.16.8.107:8084",//测试环境
+                target: "http://172.16.8.192:8084",//测试环境
                 // target: "http://172.16.10.34:8084",
                 // target: "http://172.16.0.225:8084",
                 // pathRewrite:{"^/api/tag/service":"/server/index.php?g=Web&c=Mock&o=simple&projectID=5&uri=/api/tag"},
--- engine-ui/packages/console-ui/src/public/main/config/config.js
+++ engine-ui/packages/console-ui/src/public/main/config/config.js
@@ -2,6 +2,6 @@
  * 应用全局配置文件
  */
 window.APP_CONF = {
-    UIC_URL: 'http://dtuic.dtstack.net',  // UIC中心地址
+    UIC_URL: 'http://dtuic.dtstack.net:82',  // UIC中心地址
     UIC_DOMAIN: '.dtstack.net', // UIC域名
 }
