95db63bba96059b96627ad64e081ae77d4f25e17
小威 <xiaowei@dtstack.com>
2018-12-06 11:25:03 +0800

    build(Add lint, postchangelog script):

--- engine-ui/packages/console-ui/scripts/build.sh
+++ engine-ui/packages/console-ui/scripts/build.sh
@@ -4,22 +4,20 @@
 dev="dev"
 master="master"
 currentTime="$(date +"%Y_%m_%d_%H_%M_%S")"
-# Build dist, and push it to gitlab.
-git pull origin $dev
-echo "Git pull origin $dev."
-
-git add -A
-git commit -m "update_$currentTime"
 
 git checkout $master
 echo "Current branch is $master."
-git merge $dev
-echo "Current branch master merged $dev."
+git pull origin $master
+echo "Current pull origin $master."
+
+# Auto generate version number and tag
+standard-version
+
+git push --follow-tags origin master
 
 git push origin $master
 echo "Git push origin $master."
 
-git checkout $dev
-echo "Current branch is $dev."
+echo "Release finished."
 
 
