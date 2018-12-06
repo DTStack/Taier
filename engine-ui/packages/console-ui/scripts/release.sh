5b1d6e9811e6641cc2f13eb9fc631abe8ff50883
小威 <xiaowei@dtstack.com>
2018-12-06 17:38:01 +0800

    build:

--- engine-ui/packages/console-ui/scripts/release.sh
+++ engine-ui/packages/console-ui/scripts/release.sh
@@ -10,11 +10,10 @@ echo "Current pull origin $master."
 # Auto generate version number and tag
 standard-version --tag-prefix $prefix --infile CHANGELOG.md
 
-# git push --follow-tags origin master
+git push --follow-tags origin $master
+git push origin $master
 
-# git push origin $master
 echo "Git push origin $master"
-
 echo "Release finished."
 
 
