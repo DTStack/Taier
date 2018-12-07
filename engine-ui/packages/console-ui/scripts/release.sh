999603628c2244fcc1dd93a834df512424643ca1
小威 <xiaowei@dtstack.com>
2018-12-07 09:55:21 +0800

    build(release):

--- engine-ui/packages/console-ui/scripts/release.sh
+++ engine-ui/packages/console-ui/scripts/release.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Default as minor, the argument major, minor or patch: 
+release=$1||'minor';
+echo "Release as $release"
+
 # Release branch
 master="master"
 prefix="DTinsight_v"
@@ -8,7 +12,7 @@ git pull origin $master
 echo "Current pull origin $master."
 
 # Auto generate version number and tag
-standard-version --tag-prefix $prefix --infile CHANGELOG.md
+standard-version -r $release --tag-prefix $prefix --infile CHANGELOG.md
 
 git push --follow-tags origin $master
 git push origin $master
