7884f603dff26fb21efbefd202549c92f7307abb
小威 <xiaowei@dtstack.com>
2018-12-07 17:52:30 +0800

    style: General update

--- engine-ui/packages/console-ui/cup.config.js
+++ engine-ui/packages/console-ui/cup.config.js
@@ -6,47 +6,47 @@
  * > cup config // 按配置文件运行
  */
 
- const base = "/mock";
+const base = '/mock';
 
 module.exports = {
-	"name": "DTinsight build test",
-	"listen": 3001,
-	"root": "dist",
-    "location": {
+    'name': 'DTinsight build test',
+    'listen': 3001,
+    'root': 'dist',
+    'location': {
         // "/api/rdos/batch/batchTask/getTaskById": `${base}/task/get_task.json`,
         // "/api/task/add": `${base}/add.json`
-        // "/api/analysis/getCatalogue": `${base}/catalogue/get_catalogues.json`, 
-        // "/api/analysis/createOrUpdateDB": `${base}/is_ok.json`, 
+        // "/api/analysis/getCatalogue": `${base}/catalogue/get_catalogues.json`,
+        // "/api/analysis/createOrUpdateDB": `${base}/is_ok.json`,
         // "/api/analysis/createSql": `${base}/table/createSQL.json`,
-        // "/api/rdos/batch/batchJobJob/displayOffSpring": `${base}/task/get_task_instances.json`, 
+        // "/api/rdos/batch/batchJobJob/displayOffSpring": `${base}/task/get_task_instances.json`,
     },
-    "proxyTable": {
-        "/api/dq": {
-            target: "http://172.16.8.104:8089",
+    'proxyTable': {
+        '/api/dq': {
+            target: 'http://172.16.8.104:8089',
             changeOrigin: true,
-            secure: false,
+            secure: false
         },
-        "/api/rdos": {
+        '/api/rdos': {
             // target: "http://172.16.10.51:9020",
-            target: "http://172.16.8.104:9020", // formal test ser
+            target: 'http://172.16.8.104:9020', // formal test ser
             changeOrigin: true,
-            secure: false,
+            secure: false
         },
-        "/uic": { // UIC地址
-            target: "http://dtuic.dtstack.net",
+        '/uic': { // UIC地址
+            target: 'http://dtuic.dtstack.net',
             changeOrigin: true,
-            secure: false,
+            secure: false
         },
-        "/api/da": { // da地址
-            target: "http://172.16.8.107:8087",//测试环境
+        '/api/da': { // da地址
+            target: 'http://172.16.8.107:8087', // 测试环境
             changeOrigin: true,
-            secure: false,
+            secure: false
         },
-        "/api/tag": { // 数据标签
-            target: "http://172.16.8.107:8085",//测试环境
+        '/api/tag': { // 数据标签
+            target: 'http://172.16.8.107:8085', // 测试环境
             changeOrigin: true,
-            secure: false,
-        },
+            secure: false
+        }
         // "/api/analysis": { // 数据标签
         //     target: "http://172.16.2.157:9021",//测试环境
         //     changeOrigin: true,
