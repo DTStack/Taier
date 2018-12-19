14270f1d5bc11bb13ef4b01f6b9386a65fcde2fc
xiaowei <xiaowei@dtstack.com>
2018-12-19 10:11:10 +0800

    feat: support clone a task

--- engine-ui/packages/console-ui/src/webapps/rdos/views/task/offline/folderTree.js
+++ engine-ui/packages/console-ui/src/webapps/rdos/views/task/offline/folderTree.js
@@ -128,13 +128,12 @@ class FolderTree extends React.Component {
                             txt: '编辑',
                             cb: this.editTask.bind(this, data)
                         }, {
+                            txt: '克隆',
+                            cb: this.cloneTask.bind(this, data)
+                        }, {
                             txt: '删除',
                             cb: this.deleteTask.bind(this, data)
                         }])
-                        // , {
-                        //     txt: '克隆',
-                        //     cb: this.cloneTask.bind(this, data)
-                        // }
                     }
                 } else {
                     operations = arr.concat([{
