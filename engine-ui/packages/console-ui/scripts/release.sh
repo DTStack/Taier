697128f0d8a1f264a4091c892e0ca35d093c63e0
xiaowei <xiaowei@dtstack.com>
2018-12-28 16:33:44 +0800

    build(release): update git push command

--- engine-ui/packages/console-ui/scripts/release.sh
+++ engine-ui/packages/console-ui/scripts/release.sh
@@ -29,7 +29,7 @@ echo "Current pull origin $master."
 # Auto generate version number and tag
 standard-version -r $release --tag-prefix $prefix --infile CHANGELOG.md
 
-git push --follow-tags origin $master
+git push --follow-tags --force-with-lease origin $master
 
 echo "Release finished."
 
