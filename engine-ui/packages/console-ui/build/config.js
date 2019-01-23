cb915cde083718e396518f25713a36ae70ad18c1
Xiao Kang <xiaokang@dtstack.com>
2019-01-23 19:45:03 +0800

    feat(api): add const table of testapi

--- engine-ui/packages/console-ui/build/config.js
+++ engine-ui/packages/console-ui/build/config.js
@@ -42,8 +42,8 @@ module.exports = {
                 secure: false,
             },
             "/api/da": { // da地址
-                target: "http://172.16.10.45:8087",
-                // target: "http://172.16.8.107:8087",//开发环境
+                // target: "http://172.16.10.45:8087",
+                target: "http://172.16.8.107:8087",//开发环境
 
                 // pathRewrite:{"^/api/da/service":"/server/index.php?g=Web&c=Mock&o=simple&projectID=5&uri=/api/da"},
                 changeOrigin: true,
