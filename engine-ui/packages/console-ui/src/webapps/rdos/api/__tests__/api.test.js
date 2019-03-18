27d3e30f9faef9db85e04423b11307b59a60ecfe
xiaowei <xiaowei@dtstack.com>
2019-03-18 16:57:15 +0800

    ci(test): integrate jest testing framework

--- engine-ui/packages/console-ui/src/webapps/rdos/api/__tests__/api.test.js
+++
@@ -1,8 +0,0 @@
-// api.test.js
-const API = require('../index');
-// import API from '../index';
-
-test('getLoginedUser', async () => {
-    const res = await API.getLoginedUser();
-    expect(res.code).toBe(0);
-})
---
+++ engine-ui/packages/console-ui/src/webapps/rdos/views/task/offline/dataSync/__tests__/source-test.js
@@ -0,0 +1,6 @@
+// api.test.js
+// import Source from './source';
+test('dataSync source', async () => {
+    // const res = await API.getLoginedUser();
+    // expect(res.code).toBe(0);
+})
