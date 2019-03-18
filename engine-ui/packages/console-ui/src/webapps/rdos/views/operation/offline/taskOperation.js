d2ffe1f6d6daa0aeffde42790399806d4423cfdd
修能 <xiuneng@dtstack.com>
2019-03-18 17:41:34 +0800

    fix(rdos): change the style of status bar

--- engine-ui/packages/console-ui/src/webapps/rdos/views/operation/offline/taskOperation.js
+++ engine-ui/packages/console-ui/src/webapps/rdos/views/operation/offline/taskOperation.js
@@ -586,8 +586,8 @@ class OfflineTaskList extends Component {
 
         return (
             <div>
-                <h1 className="box-title" style={{ lineHeight: '50px', overflowX: 'auto', height: 50 }}>
-                    <div style={{ marginTop: '5px', minWidth: 1200 }}>
+                <h1 className="box-title" style={{ lineHeight: 2.5, height: 'auto', padding: '7px 20px' }}>
+                    <div style={{ marginTop: '5px' }}>
                         <span className="ope-statistics">
                             <span className="status_overview_count_font">
                                 <Circle className="status_overview_count" />&nbsp;
