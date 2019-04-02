31ac0dbdd575e5aa22ef59954a27350f1f782fc2
xiaowei <xiaowei@dtstack.com>
2019-04-02 15:35:13 +0800

    fix: uic test address

--- engine-ui/packages/console-ui/src/public/common/config.js
+++ engine-ui/packages/console-ui/src/public/common/config.js
@@ -2,7 +2,7 @@
  * 应用公用配置
  */
 window.COMMON_CONF = {
-    UIC_URL: 'http://http://dtuic.dtstack.net', // UIC中心地址
+    UIC_URL: 'http://dtuic.dtstack.net', // UIC中心地址
     UIC_DOMAIN: '.dtstack.net', // UIC域名
     prefix: 'DTinsight', // 应用前缀
     name: '数栈', // 应用名字
