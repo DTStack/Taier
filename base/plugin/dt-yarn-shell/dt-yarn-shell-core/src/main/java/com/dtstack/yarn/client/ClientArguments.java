d63d9891e2a26d0c47df8a90903cc2b804908ddd
toutian <toutian@dtstack.com>
2019-04-04 13:42:35 +0800

    bugfix

--- base/plugin/dt-yarn-shell/dt-yarn-shell-core/src/main/java/com/dtstack/yarn/client/ClientArguments.java
+++ base/plugin/dt-yarn-shell/dt-yarn-shell-core/src/main/java/com/dtstack/yarn/client/ClientArguments.java
@@ -388,7 +388,7 @@ public class ClientArguments {
 
         allOptions.addOption("files", "files", true,
                 "Location of the XLearning files used in container");
-        allOptions.addOption("algFile", "algFile", false,
+        allOptions.addOption("alg-files", "alg-files", false,
                 "the files is or not a algorithm file");
 
         allOptions.addOption("jars", "jars", true,
