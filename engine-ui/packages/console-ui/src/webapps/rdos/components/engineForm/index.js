5cda04673f971f3f0bfb4633e7150adb58530cbb
xiaowei <xiaowei@dtstack.com>
2019-05-24 11:25:36 +0800

    refactor(tableEngineSelect): update options data

--- engine-ui/packages/console-ui/src/webapps/rdos/components/engineForm/index.js
+++ engine-ui/packages/console-ui/src/webapps/rdos/components/engineForm/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
-import { Form, Select, Radio } from 'antd';
+import { Form, Select } from 'antd';
 import { isEmpty } from 'lodash';
 
-import CatalogueSelect from '../../components/catalogueSelect';
-import LifeCycleSelect from '../../components/lifeCycleSelect';
+import EngineConfigItem from './configItem';
 
 import {
     formItemLayout,
@@ -14,7 +13,6 @@ import {
 
 const FormItem = Form.Item;
 const Option = Select.Option;
-const RadioGroup = Radio.Group;
 
 export const customItemLayout = {
     labelCol: {
@@ -31,14 +29,13 @@ class EngineForm extends React.Component {
     state = {
         engineType: ENGINE_SOURCE_TYPE.SPARK_THRIFT_SERVER // Default is spark
     }
-    componentDidMount () {
-        const formData = this.props.formData;
+
+    static getDerivedStateFromProps (props, state) {
+        const formData = props.formData;
         if (!isEmpty(formData) && formData.type) { // 初始化 engineType
-            let initialState = {
-                engineType: formData.type
-            }
-            this.setState(initialState);
+            return { engineType: formData.type }
         }
+        return null
     }
 
     getProjectOptions () {
@@ -48,36 +45,14 @@ class EngineForm extends React.Component {
         });
     }
 
-    initialTypeRadios () {
-        const { engineType } = this.state;
-        switch (engineType) {
-            case ENGINE_SOURCE_TYPE.SPARK_THRIFT_SERVER: {
-                return [
-                    <Radio value={1} key={1}>创建</Radio>,
-                    <Radio value={2} key={2}>对接已有Spark Thrift Server</Radio>
-                ]
-            }
-            case ENGINE_SOURCE_TYPE.LIBRA: {
-                return [
-                    <Radio value={1} key={1}>创建Schema</Radio>,
-                    <Radio value={2} key={2}>对接已有LibrA Schema</Radio>
-                ]
-            }
-            default: return '对接未知类型';
-        }
-    }
-
     onTypeChange = (value) => {
         this.setState({ engineType: parseInt(value, 10) })
-        this.props.form.resetFields();
+        this.props.form.resetFields([{ initialType: 1 }]);
     }
 
     render () {
         const {
-            onPreviewMetaData,
-            targets,
-            disabledEngineTypes,
-            disabledTargets
+            disabledEngineTypes
         } = this.props;
         const { getFieldDecorator } = this.props.form;
 
@@ -86,18 +61,6 @@ class EngineForm extends React.Component {
                 <Option
                     key={item.value}
                     disabled={disabledEngineTypes && disabledEngineTypes.indexOf(item.value) > -1} // 禁用指定引擎类型选项
-                    value={item.value.toString()}
-                >
-                    {item.name}
-                </Option>
-            )
-        )
-
-        const addTargets = targets && targets.map(
-            item => (
-                <Option
-                    key={item.value}
-                    disabled={disabledTargets && disabledTargets.indexOf(item.value) > -1} // 禁用指定目标
                     value={item.value.toString()}
                 >
                     {item.name}
@@ -126,69 +89,11 @@ class EngineForm extends React.Component {
                         </Select>
                     )}
                 </FormItem>
-                <FormItem
-                    label='初始化方式'
-                    {...formItemLayout}
-                >
-                    {getFieldDecorator('initialType', {
-                        initialValue: 1,
-                        rules: [{
-                            required: false,
-                            message: '请选择初始化方式'
-                        }]
-                    })(
-                        <RadioGroup>
-                            {this.initialTypeRadios()}
-                        </RadioGroup>
-                    )}
-                </FormItem>
-                <FormItem
-                    label='对接目标'
-                    {...formItemLayout}
-                >
-                    {getFieldDecorator('target', {
-                        rules: [{
-                            required: true,
-                            message: '请选择对接目标'
-                        }]
-                    })(
-                        <Select
-                            placeholder="请选择对接目标"
-                        >
-                            { addTargets }
-                        </Select>
-                    )}
-                    <a onClick={onPreviewMetaData}>预览元数据</a>
-                </FormItem>
-                <FormItem
-                    label='所属类目'
-                    {...formItemLayout}
-                >
-                    {getFieldDecorator('catalogueId', {
-                        rules: [{
-                            required: true,
-                            message: '请选择所属类目'
-                        }]
-                    })(
-                        <CatalogueSelect
-                            showSearch
-                            placeholder="请选择所属类目"
-                        />
-                    )}
-                </FormItem>
-                <FormItem
-                    label='生命周期'
-                    {...formItemLayout}
-                >
-                    {getFieldDecorator('lifecycle', {
-                        rules: [{
-                            required: true,
-                            message: '生命周期不可为空'
-                        }]
-                    })(
-                        <LifeCycleSelect width={100} inputWidth={175} />
-                    )}
-                </FormItem>
+                <EngineConfigItem
+                    {...this.props}
+                    formItemLayout={formItemLayout}
+                    engineType={this.state.engineType}
+                />
             </Form>
         )
     }
