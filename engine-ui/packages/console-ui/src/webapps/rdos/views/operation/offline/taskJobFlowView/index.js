a1495b8d47dae0c119a9482366b58f1dc9728f10
Xiao Kang <xiaokang@dtstack.com>
2019-06-10 16:16:53 +0800

    fix(rdos): fix opera record shadow copy bug
    #16453

--- engine-ui/packages/console-ui/src/webapps/rdos/views/operation/offline/taskJobFlowView/index.js
+++ engine-ui/packages/console-ui/src/webapps/rdos/views/operation/offline/taskJobFlowView/index.js
@@ -196,8 +196,6 @@ class TaskJobFlowView extends Component {
             if (res.code === 1) {
                 message.success(`${msg}命令已提交!`)
                 if (reload) reload();
-            } else {
-                message.error(`${msg}提交失败！`)
             }
             this.refresh()
         })
