bae8e9313407814c4344b005b3ddd0acb936e04e
yanxi0227 <yanxi@dtstack.com>
2019-06-24 19:00:04 +0800

    rest port

--- base/plugin/flink180/src/main/java/com/dtstack/rdos/engine/execution/flink180/FlinkClient.java
+++ base/plugin/flink180/src/main/java/com/dtstack/rdos/engine/execution/flink180/FlinkClient.java
@@ -606,7 +606,7 @@ public class FlinkClient extends AbsClient {
     public String getReqUrl(ClusterClient clusterClient){
         //return clusterClient.getWebInterfaceURL();
         String ip = clusterClient.getFlinkConfiguration().getString("jobmanager.rpc.address", "");
-        String port = clusterClient.getFlinkConfiguration().getString("rest.port", "8081");
+        String port = clusterClient.getFlinkConfiguration().getString("rest.bind-port", "8081");
         return "http://" + ip + ":" + port;
     }
 
