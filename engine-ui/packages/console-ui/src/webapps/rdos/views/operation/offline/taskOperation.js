45294d4000f7b1863bef195ef5984e093bb4db5e
nanxiang <nanxiang@dtstack.com>
2019-07-29 18:00:04 +0800

    fix(style): fix the style
    fix #17648, fix #17741

--- engine-ui/packages/console-ui/src/webapps/rdos/views/operation/offline/taskOperation.js
+++ engine-ui/packages/console-ui/src/webapps/rdos/views/operation/offline/taskOperation.js
@@ -422,11 +422,11 @@ class OfflineTaskList extends Component {
             title: '任务名称',
             dataIndex: 'id',
             key: 'id',
-            width: '408px',
+            width: '390px',
             render: (text, record) => {
                 let name = record.batchTask && record.batchTask.name
                 let originText = name;
-                name = utils.textOverflowExchange(name, 45);
+                // name = utils.textOverflowExchange(name, 45);
                 let showName;
                 if (record.batchTask.isDeleted === 1) {
                     showName = `${name} (已删除)`;
