e9fa07c830fb8d7922de97a330d5c072c1197b96
xiaowei <xiaowei@dtstack.com>
2019-08-12 15:53:10 +0800

    fix: invalid data downlond display incorrect

--- engine-ui/packages/console-ui/src/webapps/dataQuality/views/taskQuery/invalidDataTable.tsx
+++ engine-ui/packages/console-ui/src/webapps/dataQuality/views/taskQuery/invalidDataTable.tsx
@@ -42,6 +42,7 @@ export default class InvalidDataTable extends React.Component<InvalidDataProps, 
     fetchData = async () => {
         const { record, rule } = this.props;
         const { pagination } = this.state;
+        if (!record || !rule) return;
         const res = await TQApi.getInvalidData({
             recordId: record.id,
             ruleId: rule.id,
@@ -92,12 +93,11 @@ export default class InvalidDataTable extends React.Component<InvalidDataProps, 
         const { record, rule } = this.props;
 
         let invalidDataTitle = (
-            !isEmpty(record) ? `不规范数据（${record.columnName} -- ${record.functionName}）` : ''
+            !isEmpty(rule) ? `不规范数据（${rule.columnName} -- ${rule.functionName}）` : ''
         )
         const downloadUrl = TQApi.getDownInvalidDataURL(record.id, rule.id);
         const columns = this.initInvalidDataTableColumns(data.result[0]);
         const scroll: any = { x: columns.length < 6 ? true : 2000, y: 250 };
-
         return (
             <Card
                 noHovering
@@ -105,12 +105,13 @@ export default class InvalidDataTable extends React.Component<InvalidDataProps, 
                 loading={false}
                 className="shadow"
                 style={{ marginTop: '10px' }}
-                title={<span>{invalidDataTitle} <small>数据表名：{data.table}，生命周期：{data.lifeCycle}天</small></span>}
+                title={<span>{invalidDataTitle} <small>数据表名：{data.table || '无'}，生命周期：{data.lifeCycle || 0}天</small></span>}
                 extra={
-                    <a href={downloadUrl} download><Icon
-                        type="download"
-                        style={{ fontSize: 16, cursor: 'pointer' }}
-                    /></a>
+                    data.result && data.result.length > 0
+                        ? <a href={downloadUrl} download><Icon
+                            type="download"
+                            style={{ fontSize: 16, cursor: 'pointer' }}
+                        /></a> : null
                 }
             >
                 <Table
