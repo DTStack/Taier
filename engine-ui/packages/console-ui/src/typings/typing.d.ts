7797c817898054484c66325cd0916961c718ad1e
xiaowei <xiaowei@dtstack.com>
2019-08-13 13:47:34 +0800

    refactor: update total from optional to required

--- engine-ui/packages/console-ui/src/typings/typing.d.ts
+++ engine-ui/packages/console-ui/src/typings/typing.d.ts
@@ -24,7 +24,7 @@ declare module 'typing' {
      */
     export interface Pagination {
         current?: number;
-        total?: number;
+        total: number;
         pageSize?: number;
     }
 
