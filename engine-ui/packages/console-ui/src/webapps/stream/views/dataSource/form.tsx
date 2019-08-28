0839a41ea4b3057c56725fc44f431e17c59bd167
薯条 <shh@bombdeMacBook-Pro.local>
2019-08-28 11:26:22 +0800

    fix: change download path

--- engine-ui/packages/console-ui/src/webapps/stream/views/dataSource/form.tsx
+++ engine-ui/packages/console-ui/src/webapps/stream/views/dataSource/form.tsx
@@ -230,7 +230,7 @@ class BaseForm extends React.Component<any, any> {
                                 <Icon type="question-circle-o" style={{ fontSize: '14px', marginTop: '8px', marginLeft: '10px' }}/>
                             </Tooltip>
                             <a
-                                href={`/api/streamapp/download/service/streamDownload/downloadKerberosXML?sourceType${getFieldValue('type')}`}
+                                href={`/api/streamapp/download/streamDownload/downloadKerberosXML?sourceType${getFieldValue('type')}`}
                                 download
                             >
                                 <div
