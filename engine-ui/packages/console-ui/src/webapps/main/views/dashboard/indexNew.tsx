b884653ae843d144ccf14870fceb62a28a5e3282
薯条 <shh@bombdeMacBook-Pro.local>
2019-09-02 15:18:27 +0800

    refactor: delete useless code

--- engine-ui/packages/console-ui/src/webapps/main/views/dashboard/indexNew.tsx
+++ engine-ui/packages/console-ui/src/webapps/main/views/dashboard/indexNew.tsx
@@ -75,24 +75,6 @@ class DashboardNew extends React.Component<any, any> {
     }
     renderApps = () => {
         const { apps, licenseApps, user } = this.props;
-        // const sections = compareEnableApp(apps, licenseApps, false).map((app: any, index: any) => {
-        //     const isShow = app.enable && (!app.needRoot || (app.needRoot && user.isRoot))
-
-        //     return isShow && app.id !== MY_APPS.MAIN && (
-        //         <Col
-        //             span={8}
-        //             style={{
-        //                 marginTop: index > 3 ? '64px' : '28px'
-        //             }}
-        //         >
-        //             <a href={app.link} className="app-new-tag" key={app.id}>
-        //                 <img className="app-logo" src={app.newIcon} alt={app.name}/>
-        //                 <div style={{ marginTop: '16px' }} className="app-new-title">{app.name}</div>
-        //                 <p style={{ wordBreak: 'break-all', marginTop: '4px' }}>{app.description}</p>
-        //             </a>
-        //         </Col>
-        //     )
-        // })
         const sections = chain(compareEnableApp(apps, licenseApps, false))
         .filter((app: any) => {
             const isShow = app.enable && (!app.needRoot || (app.needRoot && user.isRoot))
