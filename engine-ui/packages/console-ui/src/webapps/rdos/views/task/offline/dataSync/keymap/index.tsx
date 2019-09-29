8928382420ef57114505e5ca237f08f5490406e3
xiaowei <xiaowei@dtstack.com>
2019-09-29 14:54:24 +0800

    fix(keymap): add title attribute to Hive, MAXCOMPUTE
    fix #19003

--- engine-ui/packages/console-ui/src/webapps/rdos/views/task/offline/dataSync/keymap/index.tsx
+++ engine-ui/packages/console-ui/src/webapps/rdos/views/task/offline/dataSync/keymap/index.tsx
@@ -524,13 +524,13 @@ class Keymap extends React.Component<any, any> {
                 }
                 case DATA_SOURCE.MAXCOMPUTE:
                 case DATA_SOURCE.HIVE: {
-                    const name = col ? scrollText(col.value ? `'${col.key}'` : col.key) : '字段名称';
+                    const name: any = col ? scrollText(col.value ? `'${col.key}'` : col.key) : '字段名称';
                     // 仅允许常量删除操作
                     const opt = col && col.value ? cellOperation(removeOption, editOption)
                         : cellOperation(null, editOption);
 
                     return <div>
-                        <div className="cell">{name}</div>
+                        <div className="cell" title={name}>{name}</div>
                         <div className="cell" title={typeValue}>{ type }</div>
                         <div className="cell">
                             {col ? opt : '操作'}
