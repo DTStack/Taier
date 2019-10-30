f55a5b19a8b71894ae4c40ecd78252b6b7dee646
Xiao Kang <xiaokang@dtstack.com>
2019-10-30 13:56:03 +0800

    fix(stream): remove unused fields of target map

---
+++ engine-ui/packages/console-ui/src/webapps/stream/views/task/realtime/collection/guide/helper.ts
@@ -0,0 +1,21 @@
+import { cloneDeep } from 'lodash';
+import { DATA_SOURCE, writeStrategys, TASK_TYPE } from '../../../../../comm/const';
+
+export function cleanCollectionParams (data: any) {
+    let newData = cloneDeep(data);
+    if (newData.taskType != TASK_TYPE.DATA_COLLECTION) {
+        return;
+    }
+    const { sourceMap = {}, targetMap = {} } = newData;
+    if (!sourceMap || !targetMap) {
+        return;
+    }
+    const isMysqlSource = sourceMap.type == DATA_SOURCE.MYSQL;
+    if (!isMysqlSource) {
+        targetMap.analyticalRules = undefined;
+    }
+    if (targetMap.writeStrategy == writeStrategys.FILESIZE) {
+        targetMap.interval = undefined;
+    }
+    return newData;
+}
