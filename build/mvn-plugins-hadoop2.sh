6585b9dc1ba8e42ba8259d12b6c79f30d457a3f0
toutian <953372946@qq.com>
2019-11-12 19:23:39 +0800

    add hadoop2

--- build/mvn-plugins-hadoop2.sh
+++ build/mvn-plugins-hadoop2.sh
@@ -1,4 +1,4 @@
-mvn clean package -DskipTests -pl \
+mvn clean package -DskipTests -Dhadoop.version=2.7.3 -pl \
 plugins/flink/flink150-hadoop2,\
 plugins/flink/flink180-hadoop2,\
 plugins/spark/spark-hadoop2,\
