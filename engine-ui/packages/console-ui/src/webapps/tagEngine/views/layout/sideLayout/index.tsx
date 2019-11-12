ab76471998558a728d97249f76b07d22cc6c454d
Erindcl <zhifei@dtstack.com>
2019-11-12 17:53:14 +0800

    feat(tag): change Content classname

--- engine-ui/packages/console-ui/src/webapps/tagEngine/views/layout/sideLayout/index.tsx
+++ engine-ui/packages/console-ui/src/webapps/tagEngine/views/layout/sideLayout/index.tsx
@@ -96,7 +96,7 @@ class SideLayout extends React.Component<IProps, IState> {
                                 </div>
                                 <SideBar {...this.props} mode={this.state.mode} />
                             </Sider>
-                            <Content className="tag-content">{children}</Content>
+                            <Content className="inner-container">{children}</Content>
                         </React.Fragment> : (<DashBoard/>)
                     }
 
