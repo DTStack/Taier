7ba8bd2678db310026b647ce81bb8bc4001e5224
sishu@dtstack.com <sishu@dtstack.com>
2019-11-15 16:49:11 +0800

    remove no need

--- plugins/flink/flink180-HW/src/main/java/com/dtstack/engine/flink180HW/constrant/ConfigConstrant.java
+++ plugins/flink/flink180-HW/src/main/java/com/dtstack/engine/flink180HW/constrant/ConfigConstrant.java
@@ -10,28 +10,10 @@ public class ConfigConstrant {
 
     public static final String SPLIT = "_";
 
-    public static final String FLINK_CHECKPOINT_INTERVAL_KEY = "sql.checkpoint.interval";
-
-    public static final String FLINK_CHECKPOINT_MODE_KEY = "sql.checkpoint.mode";
-
-    public static final String FLINK_CHECKPOINT_TIMEOUT_KEY = "sql.checkpoint.timeout";
-
-    public static final String FLINK_MAXCONCURRENTCHECKPOINTS_KEY = "sql.max.concurrent.checkpoints";
-
-    public static final String FLINK_CHECKPOINT_CLEANUPMODE_KEY = "sql.checkpoint.cleanup.mode";
-
-    public static final String FLINK_CHECKPOINT_DATAURI_KEY = "flinkCheckpointDataURI";
-
     public static final String SQL_ENV_PARALLELISM = "sql.env.parallelism";
 
-    public static final String SQL_MAX_ENV_PARALLELISM = "sql.max.env.parallelism";
-    
     public static final String MR_JOB_PARALLELISM = "mr.job.parallelism";
     
-    public static final String SQL_BUFFER_TIMEOUT_MILLIS = "sql.buffer.timeout.millis";
-
-    public static final String FLINK_TIME_CHARACTERISTIC_KEY = "time.characteristic";
-
     public static final String FLINK_TASK_RUN_MODE_KEY = "flinkTaskRunMode";
 
 
