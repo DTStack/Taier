0891c431708fed4647a05e0c8431bc2528f42445
xiaowei <xiaowei@dtstack.com>
2019-11-22 13:27:01 +0800

    refactor: update all apps standard product name
    1. 离线开发 BatchWorks; 2. 实时开发 StreamWorks; 3. 数据科学 AIWorks 4. 数据资产 DataAssets 5. 数据质量 DataValid 6.
    数据API DataAPI 7. 分析引擎 AnalyticEngine;8. 标签引擎 TagEngine
    
    fix #19797

--- engine-ui/packages/console-ui/src/public/analyticsEngine/config/config.js
+++ engine-ui/packages/console-ui/src/public/analyticsEngine/config/config.js
@@ -2,8 +2,8 @@
  * 应用全局配置文件
  */
 window.APP_CONF = {
-    name: 'Analytics', // 应用名字
-    titleName: '分析引擎', // 网页title
+    name: 'AnalyticEngine', // 应用名字
+    titleName: 'AnalyticEngine · 分析引擎', // 网页title
     logo: 'public/analyticsEngine/img/logo.svg', // header图标
-    loadingTitle: '分析引擎' // loading的应用名字
+    loadingTitle: 'AnalyticEngine · 分析引擎' // loading的应用名字
 }
