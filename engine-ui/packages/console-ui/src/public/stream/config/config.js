276e4695047afbe8a784c210dab193a95404931c
xiaowei <xiaowei@dtstack.com>
2019-12-03 11:13:04 +0800

    refactor: rename product and description
    fix #20262

--- engine-ui/packages/console-ui/src/public/stream/config/config.js
+++ engine-ui/packages/console-ui/src/public/stream/config/config.js
@@ -3,7 +3,7 @@
  */
 window.APP_CONF = {
     name: 'StreamWorks', // 应用名字
-    loadingTitle: 'StreamWorks · 实时开发', // loading的应用名字
+    loadingTitle: 'StreamWorks', // loading的应用名字
     logo: 'public/stream/img/logo.svg', // header图标
     titleName: 'StreamWorks · 实时开发' // 网页title
 }
