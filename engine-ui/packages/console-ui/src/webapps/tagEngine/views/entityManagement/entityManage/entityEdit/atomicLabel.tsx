83ee611a00c08805b30125e4ebe5604418420a32
Erindcl <zhifei@dtstack.com>
2019-12-04 21:08:55 +0800

    fix(tag): fixbug 20561 20557

--- engine-ui/packages/console-ui/src/webapps/tagEngine/views/entityManagement/entityManage/entityEdit/atomicLabel.tsx
+++ engine-ui/packages/console-ui/src/webapps/tagEngine/views/entityManagement/entityManage/entityEdit/atomicLabel.tsx
@@ -198,7 +198,7 @@ export default class AtomicLabel extends React.Component<IProps, IState> {
             key: 'config',
             width: 120,
             render: (text: any, record: any, index: number) => {
-                return record.type == 'time' ? '' : <a onClick={this.handleConfig.bind(this, record, index)}>配置字典</a>
+                return record.type == 3 ? '' : <a onClick={this.handleConfig.bind(this, record, index)}>配置字典</a>
             }
         }, {
             title: '标签描述',
