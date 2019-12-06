8fecb1fa07045826dcc4c61747b71311924c5a17
Erindcl <zhifei@dtstack.com>
2019-12-06 10:22:28 +0800

    fix(tag): fixbug 20627

--- engine-ui/packages/console-ui/src/webapps/tagEngine/components/taskParamsModal/index.tsx
+++ engine-ui/packages/console-ui/src/webapps/tagEngine/components/taskParamsModal/index.tsx
@@ -1,13 +1,14 @@
 import './taskParamsModal.scss';
 import * as React from 'react';
 
-import { Modal, Input, Icon } from 'antd';
+import { Modal, Input, Icon, message } from 'antd';
 
 class TaskParamsModal extends React.Component<any, any> {
     state: any = {
         editValue: null,
         editType: null,
-        editKey: null
+        editKey: null,
+        editRow: null
     }
     renderEdit () {
         const { editKey, editValue, editType } = this.state;
@@ -27,17 +28,24 @@ class TaskParamsModal extends React.Component<any, any> {
         this.setState({
             editKey: null,
             editValue: null,
-            editType: null
+            editType: null,
+            editRow: null
         })
     }
     edit (key: any, data: any) {
         this.setState({
             editKey: key,
             editValue: data.value,
-            editType: data.editType
+            editType: data.editType,
+            editRow: data
         })
     }
     changeEditValue = (e: any) => {
+        const { editRow: { max } } = this.state;
+        if (max && e.target.value && e.target.value.length > max) {
+            message.warning(`输入不可超过${max}个字符！`);
+            return false;
+        }
         this.setState({
             editValue: e.target.value
         })
