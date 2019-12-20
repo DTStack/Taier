39d54da2174b3c25d1719f728028b199e5b163f7
xiaowei <xiaowei@dtstack.com>
2019-12-20 19:24:01 +0800

    build: update dt-common

--- engine-ui/packages/console-ui/scripts/update_lib.sh
+++ engine-ui/packages/console-ui/scripts/update_lib.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # update custom dependencies
-yarn add git+ssh://git@git.dtstack.cn:10022/dt-insight-front/dt-common.git --save
+#yarn add git+ssh://git@git.dtstack.cn:10022/dt-insight-front/dt-common.git --save
+yarn add git+http://insight_frontend_lib:jb02o18123K_lib@git.dtstack.cn/dt-insight-front/dt-common.git --save
