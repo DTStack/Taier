dbaa95943a3797699d030ffbf03826ecf0e2f8bd
yanxi0227 <yanxi@dtstack.com>
2019-12-28 15:53:42 +0800

    [engine][防止其他模式]

--- plugins/flink/flink180-hadoop2/src/main/java/com/dtstack/engine/flink/enums/FlinkYarnMode.java
+++ plugins/flink/flink180-hadoop2/src/main/java/com/dtstack/engine/flink/enums/FlinkYarnMode.java
@@ -1,5 +1,8 @@
 package com.dtstack.engine.flink.enums;
 
+import com.dtstack.engine.common.exception.RdosException;
+import org.apache.directory.api.util.Strings;
+
 /**
  * company: www.dtstack.com
  * author: toutian
@@ -7,18 +10,20 @@ package com.dtstack.engine.flink.enums;
  */
 public enum FlinkYarnMode {
 
-    //flink 1.5版本新模式，RestClusterClient
-    NEW,
+    //yarn session模式
+    SESSION,
 
     //flink 每一个job就是一个flink cluster 任务，部署+提交
     PER_JOB;
 
     public static FlinkYarnMode mode(String mode) {
-        if (FlinkYarnMode.NEW.name().equalsIgnoreCase(mode)) {
-            return NEW;
-        } else {
+        if (FlinkYarnMode.PER_JOB.name().equalsIgnoreCase(mode)) {
+            return SESSION;
+        } else if (FlinkYarnMode.PER_JOB.name().equalsIgnoreCase(mode)){
             return PER_JOB;
         }
+
+        throw new RdosException("not support mode: " + mode);
     }
 
     public static boolean isPerJob(FlinkYarnMode mode) {
