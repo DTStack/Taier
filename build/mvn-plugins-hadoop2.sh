d4245345b9134fa42101898f3888ed47c6f87646
toutian <953372946@qq.com>
2020-01-03 19:34:47 +0800

    pom

--- build/mvn-plugins-hadoop2.sh
+++ build/mvn-plugins-hadoop2.sh
@@ -2,7 +2,6 @@
 echo 'Dependency Hadoop-2.7.3 Building...'
 
 mvn clean package -DskipTests -Dhadoop.version=2.7.3 -pl \
-engine-entrance, \
 engine-plugins/flink/flink180-hadoop2,\
 engine-plugins/spark/spark-yarn-hadoop2,\
 engine-plugins/dtscript/dtscript-hadoop2/dtscript-client,\
@@ -10,5 +9,6 @@ engine-plugins/learning/learning-hadoop2/learning-client,\
 engine-plugins/hadoop/hadoop2,\
 engine-plugins/kylin,\
 engine-plugins/rdbs,\
-engine-plugins/odps \
+engine-plugins/odps,\
+engine-entrance \
 -am -amd
