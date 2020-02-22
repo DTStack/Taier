82e080283a26529ed60f451b7d4e85211a69e637
yanxi0227 <yanxi@dtstack.com>
2020-02-22 22:35:08 +0800

    [engine][定时更新worker信息]

--- engine-common/src/main/java/com/dtstack/engine/common/akka/ActorManager.java
+++ engine-common/src/main/java/com/dtstack/engine/common/akka/ActorManager.java
@@ -4,20 +4,32 @@ import akka.actor.ActorSelection;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.pattern.Patterns;
+import com.dtstack.engine.common.CustomThreadFactory;
 import com.dtstack.engine.common.WorkerInfo;
+import com.dtstack.engine.common.util.LogCountUtil;
 import com.google.common.collect.Maps;
 import com.typesafe.config.ConfigFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import scala.concurrent.Await;
 import scala.concurrent.Future;
 import scala.concurrent.duration.Duration;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class ActorManager implements Runnable {
 
+    private static final Logger logger = LoggerFactory.getLogger(ActorManager.class);
+
+    private int logOutput = 0;
+    private final static int MULTIPLES = 10;
+    private final static int CHECK_INTERVAL = 2000;
+    private final ScheduledExecutorService scheduledService = new ScheduledThreadPoolExecutor(1, new CustomThreadFactory("WorkerInfoListener"));
     private ActorSystem system;
     private ActorSelection actorSelection;
     private Map<String, WorkerInfo> workerInfoMap = Maps.newHashMap();
@@ -28,6 +40,11 @@ public class ActorManager implements Runnable {
         this.system = ActorSystem.create(name, ConfigFactory.load());
         system.actorOf(Props.create(Master.class), "Master");
         this.actorSelection = system.actorSelection(path);
+        scheduledService.scheduleWithFixedDelay(
+                this,
+                CHECK_INTERVAL,
+                CHECK_INTERVAL,
+                TimeUnit.MILLISECONDS);
     }
 
     public static ActorManager createMasterActorManager(String name, String path){
@@ -60,13 +77,13 @@ public class ActorManager implements Runnable {
 
     @Override
     public void run() {
-        while (true){
-            try {
-                updateWorkerActors();
-                Thread.sleep(3000);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        try {
+            updateWorkerActors();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (LogCountUtil.count(logOutput, MULTIPLES)) {
+            logger.info("Update WorkerInfos...");
         }
     }
 
