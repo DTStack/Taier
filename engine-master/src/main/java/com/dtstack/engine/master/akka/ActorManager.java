3c94686f3619abe5e87bdf925032605fdd5abe32
yanxi0227 <yanxi@dtstack.com>
2020-02-27 20:44:32 +0800

    [engine][worker信息同步到zk]

--- engine-master/src/main/java/com/dtstack/engine/master/akka/ActorManager.java
+++ engine-master/src/main/java/com/dtstack/engine/master/akka/ActorManager.java
@@ -7,16 +7,21 @@ import akka.pattern.Patterns;
 import com.dtstack.engine.common.CustomThreadFactory;
 import com.dtstack.engine.common.worker.WorkerInfo;
 import com.dtstack.engine.common.util.LogCountUtil;
+import com.dtstack.engine.master.zookeeper.ZkService;
 import com.google.common.collect.Maps;
 import com.typesafe.config.ConfigFactory;
+import org.apache.commons.collections.MapUtils;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import scala.concurrent.Await;
 import scala.concurrent.Future;
 import scala.concurrent.duration.Duration;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
@@ -27,6 +32,9 @@ public class ActorManager implements InitializingBean, Runnable {
 
     private static final Logger logger = LoggerFactory.getLogger(ActorManager.class);
 
+    private static ObjectMapper objectMapper = new ObjectMapper();
+    private final static String GET_WORKER_INFOS = "getWorkerInfos";
+    private final static String NODE_SUFFIX = "/workers";
     private int logOutput = 0;
     private final static int MULTIPLES = 10;
     private final static int CHECK_INTERVAL = 2000;
@@ -37,6 +45,9 @@ public class ActorManager implements InitializingBean, Runnable {
     private String name;
     private String path;
     private long timeout = 5000L;
+
+    @Autowired
+    private ZkService zkService;
 
     @Override
     public void run() {
@@ -51,11 +62,35 @@ public class ActorManager implements InitializingBean, Runnable {
     }
 
     private void updateWorkerActors() throws Exception {
-        Future<Object> future = Patterns.ask(actorSelection, "getWorkerInfos", 3000);
+        Future<Object> future = Patterns.ask(actorSelection, GET_WORKER_INFOS, 3000);
         HashMap<String, WorkerInfo> infos = (HashMap<String, WorkerInfo>) Await.result(future, Duration.create(3, TimeUnit.SECONDS));
-        workerInfoMap = infos.entrySet().stream()
+        updateToZk(infos);
+        workerInfoMap = getWorkersFromZk().entrySet().stream()
                 .filter(map -> System.currentTimeMillis() - map.getValue().getTimestamp() > timeout)
                 .collect(Collectors.toMap(p -> p.getKey(), p -> p.getValue()));
+    }
+
+    private void updateToZk(HashMap<String, WorkerInfo> infos) throws Exception {
+        String node = zkService.getLocalAddress() + NODE_SUFFIX;
+        zkService.createNodeIfNotExists(node, infos);
+    }
+
+    private HashMap<String, WorkerInfo> getWorkersFromZk(){
+        HashMap<String, WorkerInfo> workers = Maps.newHashMap();
+        List<String> children = zkService.getBrokersChildren();
+        for (String address : children){
+            String node = address + NODE_SUFFIX;
+            HashMap<String, WorkerInfo> nodeMap = Maps.newHashMap();
+            try {
+                nodeMap = objectMapper.readValue(zkService.getDataFromPath(node), HashMap.class);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if (nodeMap.size() > 0){
+                workers.putAll(nodeMap);
+            }
+        }
+        return workers;
     }
 
     @Override
