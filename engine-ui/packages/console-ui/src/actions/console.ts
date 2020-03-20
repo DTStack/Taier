338a611d4adf6ba5d1b1a7139c65b9d8cc9e5f10
xiaowei <xiaowei@dtstack.com>
2020-03-20 17:04:10 +0800

    fix(tenant binding): getTenantList from uic directly
    1. 修改全部租户获取接口; 2. 修复自动填充
    
    fix #23521

--- engine-ui/packages/console-ui/src/actions/console.ts
+++ engine-ui/packages/console-ui/src/actions/console.ts
@@ -1,11 +1,11 @@
-import consoleApi from '../api/console'
+import API from 'dt-common/src/api'
 import { userActions } from '../consts/consoleActions'
 
 // Action
 export function getTenantList () {
     return (dispatch: any) => {
-        consoleApi.getTenantList().then((res: any) => {
-            if (res.code === 1) {
+        API.getFullTenants().then((res: any) => {
+            if (res.success) {
                 return dispatch({
                     type: userActions.SET_TENANT_LIST,
                     data: res.data || []
