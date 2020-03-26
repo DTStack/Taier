9d68103eb322ce58a24a3c2e0f3d6ddff8095fc5
yuemo <yuemo@dtstack.com>
2020-03-26 17:07:10 +0800

    fix comment

--- sql/4.0/202002121200_v4.0.0.sql
+++ sql/4.0/202002121200_v4.0.0.sql
@@ -260,7 +260,7 @@ CREATE TABLE `rdos_job_graph_trigger`
   DEFAULT CHARSET = utf8;
 
 
----- 新增字段2
+---- 修改表名
 ALTER TABLE rdos_plugin_info RENAME TO schedule_plugin_info;
 ALTER TABLE rdos_engine_job RENAME TO schedule_engine_job;
 ALTER TABLE rdos_stream_task_checkpoint RENAME TO schedule_engine_job_checkpoint;
@@ -277,4 +277,23 @@ ALTER TABLE rdos_batch_job RENAME TO schedule_job;
 ALTER TABLE rdos_batch_job_job RENAME TO schedule_job_job;
 ALTER TABLE rdos_batch_fill_data_job RENAME TO schedule_fill_data_job;
 ALTER TABLE rdos_job_graph_trigger RENAME TO schedule_job_graph_trigger;
+
+--- 新增comment
+ALTER TABLE  `schedule_engine_job_checkpoint` modify  COLUMN `checkpoint_id` varchar(64) DEFAULT NULL COMMENT '检查点id';
+ALTER TABLE  `schedule_engine_job_retry` modify  COLUMN `retry_num` int(10) NOT NULL DEFAULT '0' COMMENT '执行时，重试的次数';
+
+ALTER TABLE  `schedule_task_shade` modify  COLUMN `task_desc` varchar(256) NOT NULL COMMENT '任务描述';
+ALTER TABLE  `schedule_task_shade` modify  COLUMN `main_class` varchar(256) NOT NULL COMMENT '入口类'; --- todo
+ALTER TABLE  `schedule_task_shade` modify  COLUMN `exe_args` text DEFAULT NULL COMMENT '额外参数';
+ALTER TABLE  `schedule_job` modify  COLUMN `job_key` text varchar(256) NOT NULL DEFAULT '' COMMENT '工作任务key';
+ALTER TABLE  `schedule_job` modify  COLUMN `job_name` text VARCHAR(256) NOT NULL DEFAULT '' COMMENT '工作任务名称';
+ALTER TABLE  `schedule_job` modify  COLUMN `dependency_type` tinyint(2) NOT NULL DEFAULT 0  COMMENT '依赖类型'; --- todo
+ALTER TABLE  `schedule_fill_data_job` modify  COLUMN `job_name` VARCHAR(64) NOT NULL DEFAULT '' COMMENT '补数据任务名称';
+ALTER TABLE  `schedule_job_graph_trigger` modify  COLUMN `trigger_time` datetime NOT NULL COMMENT '调度时间';
+
+ALTER TABLE  `console_engine` modify  COLUMN `cluster_id` int(11) NOT NULL COMMENT '集群id';
+ALTER TABLE  `console_component` modify  COLUMN `engine_id` int(11) NOT NULL COMMENT '引擎id';
+
+
+
 
