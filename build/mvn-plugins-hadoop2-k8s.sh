6f8aa120630d0cf9c2d099b217f2837aa91ab6f5
toutian <953372946@qq.com>
2020-05-15 15:26:37 +0800

    fix

--- build/mvn-plugins-hadoop2-k8s.sh
+++ build/mvn-plugins-hadoop2-k8s.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
 echo 'Dependency Hadoop-2.7.3&Kubernetes Building...'
 
-mvn clean package -DskipTests -Dhadoop.version=2.7.3 -pl \
-engine-worker/engine-plugins/dummy,\
-engine-worker/engine-plugins/flink/k8s-hdfs2-flink110,\
-engine-worker/engine-plugins/hadoop/hadoop2,\
-engine-worker/engine-plugins/kylin,\
-engine-worker/engine-plugins/rdbs,\
-engine-worker/engine-plugins/odps,\
-engine-entrance \
--am
+mvn clean package -DskipTests -Dhadoop.version=2.7.3 -Pk8s-hdfs2
