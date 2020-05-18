7feae4bc35168a9dc5deab02ccbdf319e46f9858
toutian <953372946@qq.com>
2020-05-18 10:25:14 +0800

    fix sql

--- sql/increment/202004011200_v3.9.28.sql
+++ sql/increment/202004011200_v3.9.28.sql
@@ -1,3 +1,3 @@
 ALTER TABLE schedule_engine_job_checkpoint ADD INDEX idx_task_engine_id (`task_engine_id`) COMMENT '任务的引擎id';
 ALTER TABLE schedule_plugin_job_info ADD INDEX idx_gmt_modified (`gmt_modified`) COMMENT '修改时间';
-ALTER TABLE schedule_engine_job_retry ADD INDEX idx_job_id (`job_id`) COMMENT '任务实例 id';
+ALTER TABLE schedule_engine_job_retry ADD INDEX idx_job_id (`job_id`(128)) COMMENT '任务实例 id';
