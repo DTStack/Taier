ecd54f12cabfd7ae8e6149a548fadd21c2084836
toutian <953372946@qq.com>
2020-05-19 10:41:06 +0800

    fix

--- engine-worker/engine-plugins/rdbs/oracle/src/main/java/com/dtstack/engine/rdbs/oracle/OracleConnFactory.java
+++ engine-worker/engine-plugins/rdbs/oracle/src/main/java/com/dtstack/engine/rdbs/oracle/OracleConnFactory.java
@@ -1,6 +1,9 @@
 package com.dtstack.engine.rdbs.oracle;
 
+import com.dtstack.engine.common.util.DtStringUtil;
 import com.dtstack.engine.rdbs.common.executor.AbstractConnFactory;
+
+import java.util.List;
 
 public class OracleConnFactory extends AbstractConnFactory {
 
@@ -12,5 +15,15 @@ public class OracleConnFactory extends AbstractConnFactory {
     @Override
     public String getCreateProcedureHeader(String procName) {
         return String.format("create  procedure \"%s\" Authid Current_User as\n", procName);
+    }
+
+    @Override
+    public boolean supportProcedure() {
+        return false;
+    }
+
+    @Override
+    public List<String> buildSqlList(String sql) {
+        return DtStringUtil.splitIgnoreQuota(sql, ';');
     }
 }
