82aeebfe5d5d7a0d83ff0841a3137a3d05a150ff
xiaowei <xiaowei@dtstack.com>
2020-05-21 09:51:31 +0800

    test: update jest.config.js

--- engine-ui/packages/console-ui/jest.config.js
+++ engine-ui/packages/console-ui/jest.config.js
@@ -17,21 +17,6 @@ module.exports = {
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
     moduleNameMapper: {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/mock/fileMock.js',
-        '\\.(css|scss|less)$': '<rootDir>/mock/styleMock.js',
-
-        '^utils(.*)$': '<rootDir>/src/utils$1',
-        '^widgets(.*)$': '<rootDir>/src/widgets$1',
-        '^consts(.*)$': '<rootDir>/src/consts$1',
-        '^funcs(.*)$': '<rootDir>/src/funcs$1',
-        '^config(.*)$': '<rootDir>/src/config$1',
-        '^database(.*)$': '<rootDir>/src/database$1',
-
-        '^main(.*)$': '<rootDir>/src/webapps/main$1',
-        '^rdos(.*)$': '<rootDir>/src/webapps/rdos$1',
-        '^stream(.*)$': '<rootDir>/src/webapps/stream$1',
-        '^dataQuality(.*)$': '<rootDir>/src/webapps/dataQuality$1',
-        '^dataApi(.*)$': '<rootDir>/src/webapps/dataApi$1',
-        '^dataLabel(.*)$': '<rootDir>/src/webapps/dataLabel$1',
-        '^analytics(.*)$': '<rootDir>/src/webapps/analytics$1'
+        '\\.(css|scss|less)$': '<rootDir>/mock/styleMock.js'
     }
 };
