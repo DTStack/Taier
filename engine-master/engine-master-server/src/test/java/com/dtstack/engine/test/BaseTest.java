497c2a2984758bbb30daa513719dfb96873e9b57
沈家瑜 <shenjiayu@shenjiayudeMacBook-Pro.local>
2020-05-26 21:25:31 +0800

    更加高级的方法

--- engine-master/engine-master-server/src/test/java/com/dtstack/engine/test/BaseTest.java
+++ engine-master/engine-master-server/src/test/java/com/dtstack/engine/test/BaseTest.java
@@ -7,24 +7,20 @@ import com.dtstack.engine.master.config.ThreadPoolConfig;
 import com.dtstack.engine.master.env.EnvironmentContext;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
 
+
+@RunWith(DtCenterSpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {EnvironmentContext.class, MasterServerBeanConfig.class, CacheConfig.class, ThreadPoolConfig.class,
+        MybatisConfig.class})
 public abstract class BaseTest {
-    private final static String DICTIONARY_NAME = "DAGScheduleX";
-    public static ApplicationContext context = null;
 
-    @BeforeClass
-    public static void beforeOperation() {
-        //获得项目文件的根目录
-        String s_pre = System.getProperty("user.dir");
-        int index = s_pre.indexOf(DICTIONARY_NAME);
-        System.setProperty("user.dir", s_pre.substring(0, index + DICTIONARY_NAME.length()));
-        //获取上下文环境变量，用于Spring解析
-        context = new AnnotationConfigApplicationContext(
-                EnvironmentContext.class, MasterServerBeanConfig.class, CacheConfig.class, ThreadPoolConfig.class,
-                MybatisConfig.class);
-    }
+    @Autowired
+    public  ApplicationContext context = null;
 
     @Test
     public abstract void testOperation();
