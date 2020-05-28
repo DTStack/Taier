1d40f4762581d37d1227d04ec8d82052684c75a8
maqi <maqi@dtstack.com>
2020-05-28 16:12:08 +0800

    spark k8s

---
+++ engine-worker/engine-plugins/spark/k8s-spark240/spark-k8s-client/src/main/java/com/dtstack/engine/sparkyarn/sparkyarn/SparkConfig.java
@@ -0,0 +1,55 @@
+package com.dtstack.engine.sparkyarn.sparkyarn;
+
+import org.apache.spark.SparkConf;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by sishu.yss on 2018/3/9.
+ */
+public class SparkConfig {
+
+    private static Logger logger = LoggerFactory.getLogger(SparkConfig.class);
+
+    private static String spark_executor_memory = "512m";
+
+    private static String spark_cores_max = "1";
+
+    private static String spark_executor_instances = "1";
+
+    private static String spark_executor_cores = "1";
+
+    private static String spark_submit_deployMode="cluster";
+
+    private static String spark_master="yarn";
+
+    //executor 向driver 发送心跳的间隔时间
+    private static final String SPARK_EXECUTOR_HEARTBEARTINTERVAL = "600s";
+
+    //spark 所有网络传输的超时时间
+    private static final String SPARK_NETWORK_TIMEOUT = "600s";
+
+    //rpc 请求操作在超时前等待的持续时间
+    private static final String SPARK_RPC_ASK_TIMEOUT = "600s";
+
+    //如果设置为 "true" , 则执行任务的推测执行. 这意味着如果一个或多个任务在一个阶段中运行缓慢, 则将重新启动它们
+    private static final String SPARK_SPECULATION = "true";
+
+    private static final String SPARK_YARN_MAX_APP_ATTEMPTS = "1";
+
+    public static void initDefautlConf(SparkConf sparkConf){
+        try{
+            Field[] fields = SparkConfig.class.getDeclaredFields();
+            for(Field field:fields){
+                String name = field.getName().replaceAll("_",".");
+                field.setAccessible(true);
+                sparkConf.set(name,String.valueOf(field.get(SparkConfig.class)));
+            }
+        }catch(Exception e){
+            logger.error("",e);
+        }
+
+    }
+}
---
+++ engine-worker/engine-plugins/spark/k8s-spark240/spark-k8s-client/src/main/java/com/dtstack/engine/sparkyarn/sparkyarn/SparkYarnConfig.java
@@ -0,0 +1,293 @@
+package com.dtstack.engine.sparkyarn.sparkyarn;
+
+
+import com.google.common.base.Strings;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+
+import java.util.Map;
+
+/**
+ * Created by softfly on 17/8/10.
+ */
+public class SparkYarnConfig {
+
+    private static final String DEFAULT_SPARK_YARN_ARCHIVE = "%s/sparkjars/jars";
+
+    private static final String DEFAULT_SPARK_SQL_PROXY_JAR_PATH = "%s/user/spark/spark-sql-proxy.jar";
+
+    private static final String DEFAULT_SPARK_PYTHON_EXTLIBPATH = "%s/pythons/pyspark.zip,/pythons/py4j-0.10.4-src.zip";
+
+    private static final String DEFAULT_SPARK_SQL_PROXY_MAINCLASS = "com.dtstack.sql.main.SqlProxy";
+
+    public static final String DEFAULT_CARBON_SQL_PROXY_MAINCLASS = "com.dtstack.sql.main.CarbondataSqlProxy";
+
+    private static final String HDFS_FLAG = "hdfs";
+
+    private String typeName;
+
+    private String sparkYarnArchive;
+
+    private String sparkSqlProxyPath;
+
+    private String sparkSqlProxyMainClass;
+
+    private String sparkPythonExtLibPath;
+
+    private String md5sum;
+
+    /**如果不是使用默认的配置---需要设置配置文件所在的hdfs路径*/
+    private String confHdfsPath;
+
+    private Map<String, Object> hadoopConf;
+
+    private Map<String, Object> yarnConf;
+
+    private Map<String, Object> hiveConf;
+
+    private String defaultFs;
+
+    private String hadoopUserName;
+    //队列名称
+    private String queue;
+    //是否支持弹性，true/false
+    private String elasticCapacity = "true";
+    //在yarn队列中允许等待执行的任务数量
+    private String yarnAccepterTaskNumber;
+
+    private String jvmOptions = "-XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+CMSIncrementalMode -XX:+CMSIncrementalPacing";
+
+    private boolean openKerberos;
+
+    private String carbonStorePath;
+
+    private String remoteDir;
+
+    private String principalFile;
+
+    private Map<String, String> sftpConf;
+
+    private Map<String, String> kerberosConfig;
+
+    /**
+     * krb5conf名称
+     * @return
+     */
+    private String krbName;
+
+    public String getKrbName() {
+        return krbName;
+    }
+
+    public void setKrbName(String krbName) {
+        this.krbName = krbName;
+    }
+
+    public boolean isOpenKerberos() {
+        return openKerberos;
+    }
+
+    public void setOpenKerberos(boolean openKerberos) {
+        this.openKerberos = openKerberos;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public Map<String, Object> getHiveConf() {
+        return hiveConf;
+    }
+
+    public void setHiveConf(Map<String, Object> hiveConf) {
+        this.hiveConf = hiveConf;
+    }
+
+    public String getSparkYarnArchive() {
+        if(Strings.isNullOrEmpty(sparkYarnArchive)){
+            return String.format(DEFAULT_SPARK_YARN_ARCHIVE, defaultFs);
+        }
+
+        if(!sparkYarnArchive.trim().startsWith(HDFS_FLAG)){
+            sparkYarnArchive = sparkYarnArchive.trim();
+            sparkYarnArchive = defaultFs + sparkYarnArchive;
+        }
+
+        return sparkYarnArchive;
+    }
+
+    public void setSparkYarnArchive(String sparkYarnArchive) {
+        this.sparkYarnArchive = sparkYarnArchive;
+    }
+
+    public String getSparkSqlProxyPath() {
+        if(Strings.isNullOrEmpty(sparkSqlProxyPath)){
+            return String.format(DEFAULT_SPARK_SQL_PROXY_JAR_PATH, defaultFs);
+        }
+
+        if(!sparkSqlProxyPath.trim().startsWith(HDFS_FLAG)){
+            sparkSqlProxyPath = sparkSqlProxyPath.trim();
+            sparkSqlProxyPath = defaultFs + sparkSqlProxyPath;
+        }
+
+        return sparkSqlProxyPath;
+    }
+
+    public void setSparkSqlProxyPath(String sparkSqlProxyPath) {
+        this.sparkSqlProxyPath = sparkSqlProxyPath;
+    }
+
+    public String getSparkSqlProxyMainClass() {
+
+        if(Strings.isNullOrEmpty(sparkSqlProxyMainClass)){
+            return DEFAULT_SPARK_SQL_PROXY_MAINCLASS;
+        }
+
+        return sparkSqlProxyMainClass;
+    }
+
+    public void setSparkSqlProxyMainClass(String sparkSqlProxyMainClass) {
+        this.sparkSqlProxyMainClass = sparkSqlProxyMainClass;
+    }
+
+    public String getSparkPythonExtLibPath() {
+        if(Strings.isNullOrEmpty(sparkPythonExtLibPath)){
+            return String.format(DEFAULT_SPARK_PYTHON_EXTLIBPATH, defaultFs);
+        }
+
+        if(!sparkPythonExtLibPath.startsWith(HDFS_FLAG)){
+            sparkPythonExtLibPath = sparkPythonExtLibPath.trim();
+            sparkPythonExtLibPath = defaultFs + sparkPythonExtLibPath;
+        }
+
+        return sparkPythonExtLibPath;
+    }
+
+    public void setSparkPythonExtLibPath(String sparkPythonExtLibPath) {
+        this.sparkPythonExtLibPath = sparkPythonExtLibPath;
+    }
+
+    public Map<String, Object> getHadoopConf() {
+        return hadoopConf;
+    }
+
+    public void setHadoopConf(Map<String, Object> hadoopConf) {
+        this.hadoopConf = hadoopConf;
+    }
+
+    public Map<String, Object> getYarnConf() {
+        return yarnConf;
+    }
+
+    public void setYarnConf(Map<String, Object> yarnConf) {
+        this.yarnConf = yarnConf;
+    }
+
+    public String getDefaultFs() {
+        return defaultFs;
+    }
+
+    public void setDefaultFs(String defaultFs) {
+        this.defaultFs = defaultFs;
+    }
+
+    public String getMd5sum() {
+        return md5sum;
+    }
+
+    public void setMd5sum(String md5sum) {
+        this.md5sum = md5sum;
+    }
+
+    public String getConfHdfsPath() {
+        return confHdfsPath;
+    }
+
+    public void setConfHdfsPath(String confHdfsPath) {
+        this.confHdfsPath = confHdfsPath;
+    }
+
+    public String getHadoopUserName() {
+        return hadoopUserName;
+    }
+
+    public void setHadoopUserName(String hadoopUserName) {
+        this.hadoopUserName = hadoopUserName;
+    }
+
+    public String getQueue() {
+        return StringUtils.isBlank(queue)? "default" : queue;
+    }
+
+    public void setQueue(String queue) {
+        this.queue = queue;
+    }
+
+    public boolean getElasticCapacity() {
+        return  StringUtils.isBlank(elasticCapacity) ? true: Boolean.valueOf(elasticCapacity);
+    }
+
+    public void setElasticCapacity(String elasticCapacity) {
+        this.elasticCapacity = elasticCapacity;
+    }
+
+    public int getYarnAccepterTaskNumber() {
+        return StringUtils.isBlank(yarnAccepterTaskNumber) ? 1: NumberUtils.toInt(yarnAccepterTaskNumber,2);
+    }
+
+    public void setYarnAccepterTaskNumber(String yarnAccepterTaskNumber) {
+        this.yarnAccepterTaskNumber = yarnAccepterTaskNumber;
+    }
+
+    public String getCarbonStorePath() {
+        return carbonStorePath;
+    }
+
+    public void setCarbonStorePath(String carbonStorePath) {
+        this.carbonStorePath = carbonStorePath;
+    }
+
+    public String getJvmOptions() {
+        return jvmOptions;
+    }
+
+    public void setJvmOptions(String jvmOptions) {
+        this.jvmOptions = jvmOptions;
+    }
+
+    public String getRemoteDir() {
+        return remoteDir;
+    }
+
+    public void setRemoteDir(String remoteDir) {
+        this.remoteDir = remoteDir;
+    }
+
+    public Map<String, String> getSftpConf() {
+        return sftpConf;
+    }
+
+    public void setSftpConf(Map<String, String> sftpConf) {
+        this.sftpConf = sftpConf;
+    }
+
+    public Map<String, String> getKerberosConfig() {
+        return kerberosConfig;
+    }
+
+    public void setKerberosConfig(Map<String, String> kerberosConfig) {
+        this.kerberosConfig = kerberosConfig;
+    }
+
+    public String getPrincipalFile() {
+        return principalFile;
+    }
+
+    public void setPrincipalFile(String principalFile) {
+        this.principalFile = principalFile;
+    }
+}
