158b1a0d6505d965bed976b6806029ec627e2c5b
yuebai <yuebai@dtstack.com>
2020-05-29 11:05:33 +0800

    删除default配置

--- sql/4.0/202006011200_v4.0.0.sql
+++ sql/4.0/202006011200_v4.0.0.sql
@@ -4,3 +4,10 @@ ALTER table console_component ADD COLUMN component_template text COMMENT '前端
 ALTER table console_component ADD COLUMN kerberos_file_name VARCHAR(50) DEFAULT ''  COMMENT '上传kerberos文件zip名称';
 ALTER table console_kerberos ADD COLUMN component_type int(11) COMMENT '组件类型';
 ALTER table console_kerberos ADD COLUMN krb_name VARCHAR(26) COMMENT 'krb名称';
+
+-- 删除default集群 配置
+update console_component set is_deleted = 1 WHERE engine_id IN(
+    (SELECT ce.id from console_cluster cc
+                           LEFT JOIN console_engine ce
+                                     on ce.cluster_id = cc.id
+     WHERE cc.id = -1))
