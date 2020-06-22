b36f2442eb2add8b1935980082fb60073751a901
xiuzhu <xiuzhu@dtstack.com>
2020-06-22 19:46:31 +0800

    单测com.dtstack.engine.master.impl.ScheduleTaskShadeService#getBatchTaskById#

--- engine-master/engine-master-server/src/test/java/com/dtstack/engine/master/impl/StreamTaskServiceTest.java
+++ engine-master/engine-master-server/src/test/java/com/dtstack/engine/master/impl/StreamTaskServiceTest.java
@@ -1,11 +1,21 @@
 package com.dtstack.engine.master.impl;
 
+import com.dtstack.engine.api.domain.EngineJobCache;
 import com.dtstack.engine.api.domain.EngineJobCheckpoint;
 import com.dtstack.engine.api.domain.ScheduleJob;
 import com.dtstack.engine.common.enums.RdosTaskStatus;
+import com.dtstack.engine.common.http.PoolHttpClient;
+import com.dtstack.engine.common.util.ApplicationWSParser;
 import com.dtstack.engine.master.AbstractTest;
+import com.dtstack.engine.master.akka.WorkerOperator;
 import org.apache.commons.math3.util.Pair;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Isolation;
@@ -15,16 +25,41 @@ import org.springframework.util.Assert;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 /**
  * Date: 2020/6/5
  * Company: www.dtstack.com
  * @author xiuzhu
  */
 
+@PrepareForTest({PoolHttpClient.class, WorkerOperator.class, ApplicationWSParser.class})
 public class StreamTaskServiceTest extends AbstractTest {
 
+	@Mock
+	private WorkerOperator workerOperator;
+
 	@Autowired
+	@InjectMocks
 	StreamTaskService streamTaskService;
+
+	@Before
+	public void setup() throws Exception{
+		MockitoAnnotations.initMocks(this);
+		PowerMockito.mockStatic(WorkerOperator.class);
+		when(workerOperator.getJobMaster(any(),any(),any())).thenReturn("http://dtstack01:8088/");
+
+		PowerMockito.mockStatic(PoolHttpClient.class);
+		when(PoolHttpClient.get(any())).thenReturn("{\"app\":{\"amContainerLogs\":\"http://dtstack01:8088/ws/v1/cluster/apps/application_9527\"}}");
+
+		PowerMockito.mockStatic(ApplicationWSParser.class);
+		when(ApplicationWSParser.parserAmContainerPreViewHttp(any(), any()))
+			.thenReturn(new Pair<>("http://dtstack01:8088/app/log", "1024"));
+		when(ApplicationWSParser.getAmContainerLogsUrl(any()))
+			.thenReturn("http://dtstack01:8088/app/container/log");
+
+	}
 
 	@Test
 	@Transactional(isolation = Isolation.READ_UNCOMMITTED)
@@ -86,13 +121,17 @@ public class StreamTaskServiceTest extends AbstractTest {
 	}
 
 	@Test
-	public void testGetRunningTaskLogUrl() {
-		String taskId = "91ac079e";
+	@Transactional(isolation = Isolation.READ_UNCOMMITTED)
+	@Rollback
+	public void testGetRunningTaskLogUrl() throws Exception {
 
-		Integer taskStatus = streamTaskService.getTaskStatus(taskId);
+		ScheduleJob streamJob = dataCollection.getScheduleJobStream();
+		dataCollection.getEngineJobCache();
+
+		Integer taskStatus = streamTaskService.getTaskStatus(streamJob.getJobId());
 		Assert.isTrue(RdosTaskStatus.RUNNING.getStatus().equals(taskStatus));
 
-		Pair<String, String> taskLogUrl = streamTaskService.getRunningTaskLogUrl(taskId);
+		Pair<String, String> taskLogUrl = streamTaskService.getRunningTaskLogUrl(streamJob.getJobId());
 		Assert.notNull(taskLogUrl.getKey());
 		Assert.notNull(taskLogUrl.getValue());
 	}
