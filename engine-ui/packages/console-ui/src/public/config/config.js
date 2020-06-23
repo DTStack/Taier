013f123103e88bffa09bba72c485313bb1281337
小威 <xiaowei@dtstack.com>
2020-06-23 16:55:35 +0800

    fix(config): update dev enviroment default UIC address

--- engine-ui/packages/console-ui/src/public/config/config.js
+++ engine-ui/packages/console-ui/src/public/config/config.js
@@ -9,8 +9,8 @@ window.APP_CONF = {
     titleName: 'Console · 控制台', // 网页titletheme: 'default', // 主题
     hideUserCenter: false, // 隐藏用户中心按钮
     disableHelp: false, // 禁用帮助文档
-    UIC_URL: 'http://dtuic.dtstack.net', // UIC中心地址
-    UIC_DOMAIN: '.dtstack.net', // UIC域名
+    UIC_URL: 'http://dtuic.dtstack.cn', // UIC中心地址
+    UIC_DOMAIN: '.dtstack.cn', // UIC域名
     macChrome: 'https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg', // mac版本Chrome下载地址
     windowsChrome: 'https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B8A1ECDAD-CD48-3F61-7773-5D75AC6B409A%7D%26lang%3Dzh-CN%26browser%3D4%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26brand%3DCHBD%26installdataindex%3Dempty/update2/installers/ChromeSetup.exe' // windows版本chrome下载地址
 }
