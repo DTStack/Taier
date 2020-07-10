be42db3aebae040957a74918c217d1a79949c473
xiuzhu <xiuzhu@dtstack.com>
2020-07-10 13:04:16 +0800

    k8s flink从环境参数中获取hadoop user name

--- engine-worker/engine-plugins/flink/k8s-hdfs2-flink110-core/src/main/java/com/dtstack/engine/flink/FlinkClientBuilder.java
+++ engine-worker/engine-plugins/flink/k8s-hdfs2-flink110-core/src/main/java/com/dtstack/engine/flink/FlinkClientBuilder.java
@@ -77,6 +77,9 @@ public class FlinkClientBuilder {
         String hadoopUserName = config.getString(ConfigConstrant.HADOOP_USER_NAME, "");
         if (StringUtils.isBlank(hadoopUserName)) {
             hadoopUserName = System.getenv(ConfigConstrant.HADOOP_USER_NAME);
+            if (StringUtils.isBlank(hadoopUserName)) {
+                hadoopUserName = System.getProperty(ConfigConstrant.HADOOP_USER_NAME);
+            }
         }
         config.setString(ResourceManagerOptions.CONTAINERIZED_MASTER_ENV_PREFIX + ConfigConstrant.HADOOP_USER_NAME, hadoopUserName);
         config.setString(ResourceManagerOptions.CONTAINERIZED_TASK_MANAGER_ENV_PREFIX + ConfigConstrant.HADOOP_USER_NAME, hadoopUserName);
