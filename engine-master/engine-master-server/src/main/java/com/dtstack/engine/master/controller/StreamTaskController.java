77ae341f733e1c6d02ab5218cbd6a8927b523e13
赵振智 <dazhi@dtstack.com>
2020-07-30 18:43:08 +0800

    修改接口返回值

--- engine-master/engine-master-server/src/main/java/com/dtstack/engine/master/controller/StreamTaskController.java
+++ engine-master/engine-master-server/src/main/java/com/dtstack/engine/master/controller/StreamTaskController.java
@@ -5,7 +5,6 @@ import com.dtstack.engine.api.domain.ScheduleJob;
 import com.dtstack.engine.master.impl.StreamTaskService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.apache.commons.math3.util.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
