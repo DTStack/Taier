242866626b4d73bddc7e39e9fe3273f76a1ba827
沈家瑜 <shenjiayu@shenjiayudeMacBook-Pro.local>
2020-08-04 12:02:42 +0800

    finish presto

--- engine-worker/engine-plugins/rdbs/presto/src/main/java/com/dtstack/engine/rdbs/presto/PrestoConnFactory.java
+++ engine-worker/engine-plugins/rdbs/presto/src/main/java/com/dtstack/engine/rdbs/presto/PrestoConnFactory.java
@@ -2,6 +2,7 @@ package com.dtstack.engine.rdbs.presto;
 
 import com.dtstack.engine.rdbs.common.executor.AbstractConnFactory;
 
+
 public class PrestoConnFactory extends AbstractConnFactory {
 
     public PrestoConnFactory() {
@@ -10,12 +11,27 @@ public class PrestoConnFactory extends AbstractConnFactory {
     }
 
     @Override
+    public boolean supportTransaction() {
+        return false;
+    }
+
+    @Override
+    public boolean supportProcedure(String sql) {
+        return false;
+    }
+
+    @Override
     public String getCreateProcedureHeader(String procName) {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public boolean supportTransaction() {
-        return false;
+    public String getCallProc(String procName) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getDropProc(String procName) {
+        throw new UnsupportedOperationException();
     }
 }
