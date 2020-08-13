5668e7f7956a91e4920dd6c0463d16da435fe4e6
yuebai <yuebai@dtstack.com>
2020-08-13 11:12:43 +0800

    fix merge

--- engine-master/engine-master-server/src/main/java/com/dtstack/engine/master/controller/StreamTaskController.java
+++ engine-master/engine-master-server/src/main/java/com/dtstack/engine/master/controller/StreamTaskController.java
@@ -2,10 +2,9 @@ package com.dtstack.engine.master.controller;
 
 import com.dtstack.engine.api.domain.EngineJobCheckpoint;
 import com.dtstack.engine.api.domain.ScheduleJob;
-import com.dtstack.engine.api.service.StreamTaskService;
+import com.dtstack.engine.master.impl.StreamTaskService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.apache.commons.math3.util.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,40 +19,40 @@ import java.util.List;
 public class StreamTaskController {
 
     @Autowired
-    private StreamTaskController streamTaskController;
+    private StreamTaskService streamTaskService;
 
     @RequestMapping(value="/getCheckPoint", method = {RequestMethod.POST})
     @ApiOperation(value = "查询checkPoint")
     public List<EngineJobCheckpoint> getCheckPoint(@DtRequestParam("taskId") String taskId, @DtRequestParam("triggerStart") Long triggerStart, @DtRequestParam("triggerEnd") Long triggerEnd) {
-        return streamTaskController.getCheckPoint(taskId, triggerStart, triggerEnd);
+        return streamTaskService.getCheckPoint(taskId, triggerStart, triggerEnd);
     }
 
     @RequestMapping(value="/getByTaskIdAndEngineTaskId", method = {RequestMethod.POST})
     public EngineJobCheckpoint getByTaskIdAndEngineTaskId(@DtRequestParam("taskId") String taskId, @DtRequestParam("engineTaskId") String engineTaskId) {
-        return streamTaskController.getByTaskIdAndEngineTaskId(taskId, engineTaskId);
+        return streamTaskService.getByTaskIdAndEngineTaskId(taskId, engineTaskId);
     }
 
     @RequestMapping(value="/getEngineStreamJob", method = {RequestMethod.POST})
     @ApiOperation(value = "查询stream job")
     public List<ScheduleJob> getEngineStreamJob(@DtRequestParam("taskIds") List<String> taskIds) {
-        return streamTaskController.getEngineStreamJob(taskIds);
+        return streamTaskService.getEngineStreamJob(taskIds);
     }
 
     @RequestMapping(value="/getTaskIdsByStatus", method = {RequestMethod.POST})
     @ApiOperation(value = "获取某个状态的任务task_id")
     public List<String> getTaskIdsByStatus(@DtRequestParam("status") Integer status) {
-        return streamTaskController.getTaskIdsByStatus(status);
+        return streamTaskService.getTaskIdsByStatus(status);
     }
 
     @RequestMapping(value="/getTaskStatus", method = {RequestMethod.POST})
     @ApiOperation(value = "获取任务的状态")
     public Integer getTaskStatus(@DtRequestParam("taskId") String taskId) {
-        return streamTaskController.getTaskStatus(taskId);
+        return streamTaskService.getTaskStatus(taskId);
     }
 
     @RequestMapping(value="/getRunningTaskLogUrl", method = {RequestMethod.POST})
     @ApiOperation(value = "获取实时计算运行中任务的日志URL")
-    public Pair<String, String> getRunningTaskLogUrl(@DtRequestParam("taskId") String taskId) {
-        return streamTaskController.getRunningTaskLogUrl(taskId);
+    public List<String> getRunningTaskLogUrl(@DtRequestParam("taskId") String taskId) {
+        return streamTaskService.getRunningTaskLogUrl(taskId);
     }
 }
