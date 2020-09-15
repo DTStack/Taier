6413a8560fcac2457ace9dca1145271c36d5d757
toutian <953372946@qq.com>
2020-09-15 15:41:12 +0800

    插件的构建支持hadoop2 hadoop3版本一起出包，分离插件中pom对hadoopversion的依赖

--- build/mvn-plugins-hadoop3.sh
+++ build/mvn-plugins-hadoop3.sh
@@ -1,21 +1,35 @@
 #!/usr/bin/env bash
+echo 'DAGSchedulex Building...'
 
-hadoopversion=$1
+hadoopVersion=$1
 if [ ! -n "$hadoopversion" ]
 then
-    hadoopversion=3.0.0
+    hadoopVersion=2.7.3
 fi
-echo "Dependency ${hadoopversion} Building..."
+echo "Dependency ${hadoopVersion} Building..."
 
-mvn clean package -DskipTests -Dhadoop.version=${hadoopversion} -pl \
+mvn -T 1C clean package -DskipTests -Dhadoop.version=${hadoopVersion} -pl \
 engine-worker/engine-plugins/dummy,\
-engine-worker/engine-plugins/hadoop/yarn3-hdfs3-hadoop3,\
-engine-worker/engine-plugins/dtscript/yarn3-hdfs3-dtscript/dtscript-client,\
 engine-worker/engine-plugins/flink/yarn3-hdfs3-flink180,\
-engine-worker/engine-plugins/spark/yarn3-hdfs3-spark210/spark-yarn-client,\
-engine-worker/engine-plugins/spark/yarn3-hdfs3-spark210/spark-sql-proxy,\
+engine-worker/engine-plugins/flink/yarn2-hdfs2-flink180,\
+engine-worker/engine-plugins/flink/yarnHW-hdfsHW-flink180HW,\
+engine-worker/engine-plugins/flink/k8s-hdfs2-flink110,\
+engine-worker/engine-plugins/flink/yarn3-hdfs3-flink110,\
+engine-worker/engine-plugins/flink/yarn2-hdfs2-flink110,\
 engine-worker/engine-plugins/spark/yarn3-hdfs3-spark240/spark-yarn-client,\
 engine-worker/engine-plugins/spark/yarn3-hdfs3-spark240/spark-sql-proxy,\
+engine-worker/engine-plugins/spark/yarn3-hdfs3-spark210/spark-yarn-client,\
+engine-worker/engine-plugins/spark/yarn3-hdfs3-spark210/spark-sql-proxy,\
+engine-worker/engine-plugins/spark/yarn2-hdfs2-spark210/spark-yarn-client,\
+engine-worker/engine-plugins/spark/yarn2-hdfs2-spark210/spark-sql-proxy,\
+engine-worker/engine-plugins/spark/k8s-hdfs2-spark240/spark-k8s-client,\
+engine-worker/engine-plugins/spark/k8s-hdfs2-spark240/spark-sql-proxy,\
+engine-worker/engine-plugins/dtscript/yarn3-hdfs3-dtscript/dtscript-client,\
+engine-worker/engine-plugins/dtscript/yarn2-hdfs2-dtscript/dtscript-client,\
+engine-worker/engine-plugins/learning/yarn2-hdfs2-learning/learning-client,\
+engine-worker/engine-plugins/hadoop/yarn3-hdfs3-hadoop3,\
+engine-worker/engine-plugins/hadoop/yarn2-hdfs2-hadoop2,\
+engine-worker/engine-plugins/hadoop/k8s-hdfs2-hadoop2,\
 engine-worker/engine-plugins/kylin,\
 engine-worker/engine-plugins/odps,\
 engine-worker/engine-plugins/rdbs/mysql,\
@@ -31,3 +45,4 @@ engine-worker/engine-plugins/rdbs/greenplum,\
 engine-worker/engine-plugins/rdbs/presto,\
 engine-entrance \
 -am
+
