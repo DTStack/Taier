24f2c7e2a28d6af8826c649e515316b7b152d6e7
yuebai <yuebai@dtstack.com>
2020-09-18 15:42:01 +0800

    fix npe

--- engine-master/engine-master-server/src/main/java/com/dtstack/engine/master/jobdealer/JobSubmittedDealer.java
+++ engine-master/engine-master-server/src/main/java/com/dtstack/engine/master/jobdealer/JobSubmittedDealer.java
@@ -64,8 +64,8 @@ public class JobSubmittedDealer implements Runnable {
                 if (StringUtils.isNotBlank(jobClient.getEngineTaskId())) {
                     JobResult jobResult = jobClient.getJobResult();
                     String appId = jobResult.getData(JobResult.EXT_ID_KEY);
-                    String jobGraph = jobResult.getData(JobResult.JOB_GRAPH);
-                    scheduleJobDao.updateJobSubmitSuccess(jobClient.getTaskId(), jobClient.getEngineTaskId(), appId, jobClient.getJobResult().getJsonStr(), JobGraphUtil.formatJSON(jobClient.getEngineTaskId(),jobGraph));
+                    String jobGraph = JobGraphUtil.formatJSON(jobClient.getEngineTaskId(), jobResult.getData(JobResult.JOB_GRAPH));
+                    scheduleJobDao.updateJobSubmitSuccess(jobClient.getTaskId(), jobClient.getEngineTaskId(), appId, jobClient.getJobResult().getJsonStr(), jobGraph);
                     jobDealer.updateCache(jobClient, EJobCacheStage.SUBMITTED.getStage());
                     jobClient.doStatusCallBack(RdosTaskStatus.SUBMITTED.getStatus());
                     shardCache.updateLocalMemTaskStatus(jobClient.getTaskId(), RdosTaskStatus.SUBMITTED.getStatus());
