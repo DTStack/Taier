9b84e6160099175a9810269d4f5effec13a54d52
mowen <mowen@dtstack.com>
2020-10-16 16:42:06 +0800

    fix class not found

--- engine-worker/engine-plugins/base/src/main/java/com/dtstack/engine/base/util/YarnClientUtils.java
+++ engine-worker/engine-plugins/base/src/main/java/com/dtstack/engine/base/util/YarnClientUtils.java
@@ -3,6 +3,8 @@ package com.dtstack.engine.base.util;
 import com.dtstack.engine.base.BaseConfig;
 import com.dtstack.engine.common.exception.RdosDefineException;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.yarn.api.records.ApplicationReport;
+import org.apache.hadoop.yarn.api.records.YarnApplicationState;
 import org.apache.hadoop.yarn.client.api.YarnClient;
 import org.apache.hadoop.yarn.conf.YarnConfiguration;
 import org.apache.hadoop.yarn.exceptions.YarnException;
@@ -10,6 +12,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.EnumSet;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
@@ -32,7 +36,9 @@ public class YarnClientUtils {
                 //判断下是否可用
                 CompletableFuture.supplyAsync(() -> {
                     try {
-                        yarnClient.getAllQueues();
+                        EnumSet<YarnApplicationState> enumSet = EnumSet.noneOf(YarnApplicationState.class);
+                        enumSet.add(YarnApplicationState.ACCEPTED);
+                        List<ApplicationReport> acceptedApps = yarnClient.getApplications(enumSet);
                     } catch (YarnException | IOException e) {
                         LOG.error("YarnClient is unavailable.");
                         throw new RdosDefineException("", e);
