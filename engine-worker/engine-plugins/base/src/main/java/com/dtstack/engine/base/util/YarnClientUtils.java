51e36e2fc63c1bdeca722812c651f44657ef7120
mowen <mowen@dtstack.com>
2020-10-16 16:56:59 +0800

    优化import语句

--- engine-worker/engine-plugins/base/src/main/java/com/dtstack/engine/base/util/YarnClientUtils.java
+++ engine-worker/engine-plugins/base/src/main/java/com/dtstack/engine/base/util/YarnClientUtils.java
@@ -6,7 +6,6 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.yarn.api.records.ApplicationReport;
 import org.apache.hadoop.yarn.api.records.YarnApplicationState;
 import org.apache.hadoop.yarn.client.api.YarnClient;
-import org.apache.hadoop.yarn.conf.YarnConfiguration;
 import org.apache.hadoop.yarn.exceptions.YarnException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
