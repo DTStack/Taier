af9fd2cc840029c9c79b0f77c4ff2da21cb5957e
chenfeng <chenfeng@dtstack.com>
2020-10-21 20:31:00 +0800

    perf: perfect custom hook logic

--- engine-ui/packages/console-ui/src/components/customHooks/useEnv.ts
+++ engine-ui/packages/console-ui/src/components/customHooks/useEnv.ts
@@ -1,33 +1,38 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import { ENGINE_TYPE } from '../../consts'
 
+function useEnv ({ clusterId, form, clusterList,visible }) {
+    const [queueList, setQueueList] = useState([])
+    const [env, setEnv] = useState({
+        hasHadoop: false,
+        hasLibra: false,
+        hasTiDB: false,
+        hasOracle: false,
+        hasGreenPlum: false
+    })
+    if(visible) form?.resetFields(['queueId']);
 
-export default (clusterId: string,form: any,clusterList: any[]) => {
-  form?.resetFields(['queueId']);
-  let currentCluster: any;
-  currentCluster = clusterList.filter((clusItem: any) => clusItem.clusterId == clusterId); // 选中当前集群
+    useEffect(() => {
+        if (!clusterId) return
+        const currentCluster = clusterList.filter((clusItem: any) => clusItem?.clusterId == clusterId); // 选中当前集群
+        const currentEngineList = currentCluster?.[0]?.engines || [];
+        const hadoopEngine = currentEngineList.filter((item: any) => item.engineType == ENGINE_TYPE.HADOOP);
+        const libraEngine = currentEngineList.filter((item: any) => item.engineType == ENGINE_TYPE.LIBRA);
+        const tiDBEngine = currentEngineList.filter((item: any) => item.engineType == ENGINE_TYPE.TI_DB);
+        const oracleEngine = currentEngineList.filter((item: any) => item.engineType == ENGINE_TYPE.ORACLE);
+        const greenPlumEngine = currentEngineList.filter((item: any) => item.engineType == ENGINE_TYPE.GREEN_PLUM);
 
-  const currentEngineList = currentCluster?.[0]?.engines || [];
-  const hadoopEngine = currentEngineList.filter((item: any) => item.engineType == ENGINE_TYPE.HADOOP);
-  const libraEngine = currentEngineList.filter((item: any) => item.engineType == ENGINE_TYPE.LIBRA);
-  const tiDBEngine = currentEngineList.filter((item: any) => item.engineType == ENGINE_TYPE.TI_DB);
-  const oracleEngine = currentEngineList.filter((item: any) => item.engineType == ENGINE_TYPE.ORACLE);
-  const greenPlumEngine = currentEngineList.filter((item: any) => item.engineType == ENGINE_TYPE.GREEN_PLUM);
+        setEnv({
+            hasHadoop: hadoopEngine.length >= 1,
+            hasLibra: libraEngine.length >= 1,
+            hasTiDB: tiDBEngine.length > 0,
+            hasOracle: oracleEngine.length > 0,
+            hasGreenPlum: greenPlumEngine.length > 0
+        })
 
-  const hasHadoop = hadoopEngine.length >= 1;
-  const hasLibra = libraEngine.length >= 1;
-  const hasTiDB = tiDBEngine.length > 0;
-  const hasOracle = oracleEngine.length > 0;
-  const hasGreenPlum = greenPlumEngine.length > 0;
-
-  const queueList = hadoopEngine?.[0]?.queues;
-
-  return {
-    hasHadoop,
-    hasLibra,
-    hasTiDB,
-    hasOracle,
-    hasGreenPlum,
-    queueList
-  }
+        setQueueList(hadoopEngine?.[0]?.queues || [])
+        
+    }, [ clusterList])
+    return { env, queueList }
 }
+export default useEnv
