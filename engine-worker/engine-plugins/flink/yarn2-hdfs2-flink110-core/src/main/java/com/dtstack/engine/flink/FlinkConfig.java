f792f7fd0954d0d8dd154d6b6d3cb302366a54b0
yaodong <tengzhen@dtstack.com>
2020-10-26 10:10:47 +0800

    解决合并冲突

--- engine-worker/engine-plugins/flink/yarn2-hdfs2-flink110-core/src/main/java/com/dtstack/engine/flink/FlinkConfig.java
+++ engine-worker/engine-plugins/flink/yarn2-hdfs2-flink110-core/src/main/java/com/dtstack/engine/flink/FlinkConfig.java
@@ -95,6 +95,18 @@ public class FlinkConfig extends BaseConfig {
 
     private boolean monitorAcceptedApp = false;
 
+    private String sessionCheckJarPath;
+
+    public String getSessionCheckJarPath() {
+        if(Strings.isNullOrEmpty(sessionCheckJarPath)){
+            return ConfigConstrant.DEFAULT_SESSION_CHECK_PATH;
+        }
+        return sessionCheckJarPath;
+    }
+
+    public void setSessionCheckJarPath(String sessionCheckJarPath) {
+        this.sessionCheckJarPath = sessionCheckJarPath;
+    }
 
     public boolean getMonitorAcceptedApp() {
         return monitorAcceptedApp;
