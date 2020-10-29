04ae1050244e5e909aae18bd7fd75e77780fa06d
yaodong <tengzhen@dtstack.com>
2020-10-29 14:33:50 +0800

    新增逻辑数据源

--- engine-master/lineage-service/src/main/java/com/dtstack/lineage/impl/LineageDataSourceService.java
+++ engine-master/lineage-service/src/main/java/com/dtstack/lineage/impl/LineageDataSourceService.java
@@ -1,8 +1,12 @@
 package com.dtstack.lineage.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.dtstack.engine.api.domain.LineageDataSource;
 import com.dtstack.engine.api.domain.LineageRealDataSource;
+import com.dtstack.engine.api.dto.DataSourceDTO;
 import com.dtstack.engine.common.exception.RdosDefineException;
+import com.dtstack.lineage.bo.RdbmsDataSourceConfig;
 import com.dtstack.lineage.dao.LineageDataSourceDao;
 import com.dtstack.lineage.dao.LineageRealDataSourceDao;
 import org.apache.commons.lang.StringUtils;
@@ -32,45 +36,71 @@ public class LineageDataSourceService {
 
     /**
      * 新增或删除数据源
-     * @param dataSourceId 数据源id
-     * @param dataJson 数据源配置json
-     * @param kerberosConf kerberos配置，null则不开启kerberos
-     * @param sourceType 数据源类型
-     * @param appType 应用类型
+     * @param dataSourceDTO 数据源信息
      */
-    public void addOrUpdateDataSource(Long dataSourceId,String sourceName,String dataJson,String kerberosConf,Integer sourceType,Integer appType){
+    public void addOrUpdateDataSource(DataSourceDTO dataSourceDTO){
         //TODO
         //如果存在数据源则更新
         //更新后更新物理数据源
         //如果不存在数据源则添加
         //添加后添加物理数据源，如果物理数据源已经存在，检查配置是否更新。注意：如果两个应用使用了相同的物理数据源但是使用了不同的账号，依旧算同一个物理数据源
-        if (Objects.isNull(dataSourceId)){
-            addDataSource(sourceName,dataJson,kerberosConf,sourceType,appType);
+        if (Objects.isNull(dataSourceDTO.getDataSourceId())){
+            addDataSource(dataSourceDTO);
         }else {
             //更新数据源
-            LineageDataSource one = lineageDataSourceDao.getOne(dataSourceId);
+            LineageDataSource one = lineageDataSourceDao.getOne(dataSourceDTO.getDataSourceId());
             if (Objects.isNull(one)){
                 throw new RdosDefineException("数据源不存在");
             }
-            if (!one.getAppType().equals(appType)){
+            if (!one.getAppType().equals(dataSourceDTO.getAppType())){
                 throw new RdosDefineException("数据源不存在");
             }
             //更新数据源配置信息,通常用于更新数据源
-            if (StringUtils.isNotEmpty(dataJson)){
+            if (StringUtils.isNotEmpty(dataSourceDTO.getDataJson())){
 
             }
-            one.setDataJason(dataJson);
+            one.setDataJason(dataSourceDTO.getDataJson());
 //            lineageDataSourceDao.updateDataSource();
         }
     }
 
-    private void addDataSource(String sourceName, String dataJson, String kerberosConf, Integer sourceType, Integer appType) {
+    private void addDataSource(DataSourceDTO dataSourceDTO) {
         //TODO
-        addRealDataSource(sourceName,dataJson,kerberosConf,sourceType);
+        //生成sourceKey
+        String sourceKey = generateSourceKey(dataSourceDTO.getDataJson());
+       Integer id =   addRealDataSource(dataSourceDTO,sourceKey);
     }
 
-    private void addRealDataSource(String sourceName, String dataJson, String kerberosConf, Integer sourceType) {
+    private String generateSourceKey(String dataJson) {
+        if(null == dataJson){
+            throw new RdosDefineException("dataJson不能为空");
+        }
+        JSONObject jsonObject = JSON.parseObject(dataJson);
+        RdbmsDataSourceConfig sourceConfig = new RdbmsDataSourceConfig();
+        sourceConfig.setJdbc(jsonObject.getString("jdbc.url"));
+        String sourceKey = sourceConfig.generateRealSourceKey();
+        if(null == sourceKey){
+            throw new RdosDefineException("dataJson格式有误");
+        }
+        return sourceKey;
+    }
+
+    private Integer addRealDataSource(DataSourceDTO dataSourceDTO,String sourceKey) {
         //TODO
+        //先根据sourceKey查询物理数据源是否存在
+        LineageRealDataSource oneBySourceKey = lineageRealDataSourceDao.getOneBySourceKey(sourceKey);
+        if(null == oneBySourceKey){
+            LineageRealDataSource realDataSource = new LineageRealDataSource();
+            realDataSource.setDataJason(dataSourceDTO.getDataJson());
+            realDataSource.setKerberosConf(dataSourceDTO.getKerberosConf());
+            realDataSource.setOpenKerberos( null == dataSourceDTO.getKerberosConf() ? 0 : 1);
+            realDataSource.setSourceKey(sourceKey);
+            realDataSource.setSourceName(dataSourceDTO.getSourceName());
+            realDataSource.setSourceType(dataSourceDTO.getSourceType());
+
+        }
+
+        return null;
     }
 
     /**
