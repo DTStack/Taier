5724649631ad7a5ba7d9cf738320e9be03356605
yuebai <yuebai@dtstack.com>
2020-11-02 14:31:10 +0800

    添加项目调度接口测试

--- engine-master/engine-master-server/src/test/java/com/dtstack/engine/master/impl/ProjectServiceTest.java
+++ engine-master/engine-master-server/src/test/java/com/dtstack/engine/master/impl/ProjectServiceTest.java
@@ -1,7 +1,10 @@
 package com.dtstack.engine.master.impl;
 
-import com.dtstack.engine.dao.TenantDao;
+import com.dtstack.engine.api.domain.ScheduleTaskShade;
 import com.dtstack.engine.master.AbstractTest;
+import com.dtstack.engine.master.dataCollection.DataCollection;
+import com.dtstack.schedule.common.enums.EProjectScheduleStatus;
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -19,6 +22,10 @@ public class ProjectServiceTest extends AbstractTest {
 
     @Test
     public void testUpdateProjectSchedule(){
-
+        ScheduleTaskShade scheduleTaskShade = DataCollection.getData().getScheduleTaskShade();
+        Long projectId = scheduleTaskShade.getProjectId();
+        projectService.updateSchedule(projectId,scheduleTaskShade.getAppType(), EProjectScheduleStatus.PAUSE.getStatus());
+        ScheduleTaskShade dbScheduleTaskShade = scheduleTaskShadeService.getBatchTaskById(scheduleTaskShade.getTaskId(), scheduleTaskShade.getAppType());
+        Assert.assertEquals(dbScheduleTaskShade.getProjectScheduleStatus(),EProjectScheduleStatus.PAUSE.getStatus());
     }
 }
