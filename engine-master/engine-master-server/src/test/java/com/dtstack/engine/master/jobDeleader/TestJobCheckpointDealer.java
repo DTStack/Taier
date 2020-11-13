2042300ebd2dbba828ee01be6cb93757850407d4
yaodong <tengzhen@dtstack.com>
2020-11-13 19:08:55 +0800

    jobdelear单元测试

---
+++ engine-master/engine-master-server/src/test/java/com/dtstack/engine/master/jobDeleader/TestJobCheckpointDealer.java
@@ -0,0 +1,62 @@
+package com.dtstack.engine.master.jobDeleader;
+
+import com.dtstack.engine.api.domain.EngineJobCache;
+import com.dtstack.engine.api.domain.EngineJobCheckpoint;
+import com.dtstack.engine.api.domain.ScheduleJob;
+import com.dtstack.engine.common.JobIdentifier;
+import com.dtstack.engine.master.AbstractTest;
+import com.dtstack.engine.master.bo.JobCheckpointInfo;
+import com.dtstack.engine.master.dataCollection.DataCollection;
+import com.dtstack.engine.master.jobdealer.JobCheckpointDealer;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.concurrent.ExecutionException;
+
+/**
+ * @Author tengzhen
+ * @Description:
+ * @Date: Created in 3:19 下午 2020/11/13
+ */
+public class TestJobCheckpointDealer extends AbstractTest {
+
+
+    @Autowired
+    private JobCheckpointDealer jobCheckpointDealer;
+
+    @Test
+    public void testAfterPropertiesSet(){
+
+        jobCheckpointDealer.afterPropertiesSet();
+    }
+
+    @Test
+    public void testAddCheckpointTaskForQueue() throws ExecutionException {
+
+        EngineJobCheckpoint checkpoint = DataCollection.getData().getEngineJobCheckpoint();
+        ScheduleJob jobId = DataCollection.getData().getScheduleJobDefiniteJobId();
+        EngineJobCache engineJobCache = DataCollection.getData().getEngineJobCache();
+        Integer computeType = 1;
+        String taskId = checkpoint.getTaskId();
+        JobIdentifier jobIdentifier = JobIdentifier.createInstance(jobId.getJobId(),jobId.getApplicationId(),taskId);
+        String engineTypeName = "spark";
+        jobCheckpointDealer.addCheckpointTaskForQueue(computeType,engineJobCache.getJobId(),jobIdentifier,engineTypeName);
+    }
+
+
+    @Test
+    public void testUpdateCheckpointImmediately(){
+
+        EngineJobCheckpoint checkpoint = DataCollection.getData().getEngineJobCheckpoint();
+        EngineJobCache engineJobCache = DataCollection.getData().getEngineJobCache();
+        ScheduleJob jobId = DataCollection.getData().getScheduleJobDefiniteJobId();
+        String taskEngineId = checkpoint.getTaskEngineId();
+        JobIdentifier jobIdentifier = JobIdentifier.createInstance(engineJobCache.getJobId(),jobId.getApplicationId(),engineJobCache.getJobId());
+        JobCheckpointInfo info = new JobCheckpointInfo(jobIdentifier,engineJobCache.getEngineType());
+        jobCheckpointDealer.updateCheckpointImmediately(info,taskEngineId,2);
+    }
+
+
+
+
+}
--- engine-master/engine-master-server/src/test/java/com/dtstack/engine/master/jobDeleader/TestJobComputeResourcePlain.java
+++ engine-master/engine-master-server/src/test/java/com/dtstack/engine/master/jobDeleader/TestJobComputeResourcePlain.java
@@ -1,8 +1,16 @@
 package com.dtstack.engine.master.jobDeleader;
 
-import com.dtstack.engine.api.vo.components.ComponentsResultVO;
+import com.dtstack.engine.api.domain.Cluster;
+import com.dtstack.engine.api.domain.Queue;
+import com.dtstack.engine.api.vo.ClusterVO;
+import com.dtstack.engine.api.vo.ComponentVO;
 import com.dtstack.engine.common.JobClient;
+import com.dtstack.engine.dao.ClusterDao;
+import com.dtstack.engine.dao.QueueDao;
+import com.dtstack.engine.dao.TenantDao;
 import com.dtstack.engine.master.AbstractTest;
+import com.dtstack.engine.master.enums.EComponentType;
+import com.dtstack.engine.master.impl.ClusterService;
 import com.dtstack.engine.master.impl.ComponentService;
 import com.dtstack.engine.master.jobdealer.resource.JobComputeResourcePlain;
 import com.dtstack.engine.master.utils.CommonUtils;
@@ -22,18 +30,26 @@ public class TestJobComputeResourcePlain extends AbstractTest {
     private JobComputeResourcePlain resourcePlain;
 
     @Autowired
-    private ComponentService componentService;
+    private ClusterDao clusterDao;
 
     @Test
     public void testGetJobResource() throws Exception {
-
+        Cluster cluster = addDefaultCluster();
         JobClient jobClient = CommonUtils.getJobClient();
-        //插入集群数据
-        ComponentsResultVO test_cluster = componentService.addOrCheckClusterWithName("test_cluster");
-
-
         String jobResource = resourcePlain.getJobResource(jobClient);
         Assert.assertNotNull(jobResource);
 
+    }
+
+
+    
+    private Cluster addDefaultCluster(){
+        
+        Cluster cluster = new Cluster();
+        cluster.setId(-1L);
+        cluster.setClusterName("defalut");
+        cluster.setHadoopVersion("");
+        Integer insert = clusterDao.insertWithId(cluster);
+        return cluster;
     }
 }
