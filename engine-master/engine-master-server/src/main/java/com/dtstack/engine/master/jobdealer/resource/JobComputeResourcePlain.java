d8437ed81591143fabf859dd540ca70432c1c66b
toutian <953372946@qq.com>
2020-11-16 12:04:23 +0800

    fix split

--- engine-master/engine-master-server/src/main/java/com/dtstack/engine/master/jobdealer/resource/JobComputeResourcePlain.java
+++ engine-master/engine-master-server/src/main/java/com/dtstack/engine/master/jobdealer/resource/JobComputeResourcePlain.java
@@ -3,7 +3,7 @@ package com.dtstack.engine.master.jobdealer.resource;
 import com.dtstack.engine.api.domain.Queue;
 import com.dtstack.engine.api.vo.ClusterVO;
 import com.dtstack.engine.common.JobClient;
-import com.dtstack.engine.common.constrant.ConfigConstant;
+
 import com.dtstack.engine.master.env.EnvironmentContext;
 import com.dtstack.engine.master.impl.ClusterService;
 import org.apache.commons.lang3.StringUtils;
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Objects;
+
+import static com.dtstack.engine.common.constrant.ConfigConstant.*;
 
 /**
  * company: www.dtstack.com
@@ -37,11 +39,11 @@ public class JobComputeResourcePlain {
         String plainType = environmentContext.getComputeResourcePlain();
         String jobResource = null;
         if (ComputeResourcePlain.EngineTypeClusterQueue.name().equalsIgnoreCase(plainType)) {
-            jobResource = jobClient.getEngineType() + ConfigConstant.SPLIT + jobClient.getGroupName();
+            jobResource = jobClient.getEngineType() + SPLIT + jobClient.getGroupName();
         } else {
-            jobResource = jobClient.getEngineType() + ConfigConstant.SPLIT + jobClient.getGroupName() + ConfigConstant.SPLIT + jobClient.getComputeType().name().toLowerCase();
+            jobResource = jobClient.getEngineType() + SPLIT + jobClient.getGroupName() + SPLIT + jobClient.getComputeType().name().toLowerCase();
         }
-        return jobResource + ConfigConstant.SPLIT + computeResourceType.name();
+        return jobResource + SPLIT + computeResourceType.name();
     }
 
 
@@ -53,18 +55,17 @@ public class JobComputeResourcePlain {
         }
         String clusterName = cluster.getClusterName();
         //%s_default
-        String groupName = clusterName + ConfigConstant.SPLIT + ConfigConstant.RESOURCE_NAMESPACE_OR_QUEUE_DEFAULT;
+        String groupName = clusterName + SPLIT + RESOURCE_NAMESPACE_OR_QUEUE_DEFAULT;
 
         String namespace = clusterService.getNamespace(jobClient.getParamAction(),
                 jobClient.getTenantId(), jobClient.getEngineType(), jobClient.getComputeType());
         if (StringUtils.isNotBlank(namespace)) {
-            groupName = clusterName + ConfigConstant.SPLIT + namespace;
-            jobClient.setGroupName(groupName);
-            return;
-        }
-        Queue queue = clusterService.getQueue(jobClient.getTenantId(), cluster.getClusterId());
-        if (null != queue) {
-            groupName = clusterName + ConfigConstant.SPLIT + ConfigConstant.RESOURCE_NAMESPACE_OR_QUEUE_DEFAULT + queue.getQueueName();
+            groupName = clusterName + SPLIT + namespace;
+        } else {
+            Queue queue = clusterService.getQueue(jobClient.getTenantId(), cluster.getClusterId());
+            if (null != queue) {
+                groupName = clusterName + SPLIT + queue.getQueueName();
+            }
         }
         jobClient.setGroupName(groupName);
     }
