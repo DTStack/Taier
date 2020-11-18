5c4ce444f8fe54398383fce1888e213720e136bc
chenfeng <chenfeng@dtstack.com>
2020-11-18 15:34:44 +0800

    refactor: code format

--- engine-ui/packages/console-ui/src/views/clusterManage/edit/index.tsx
+++ engine-ui/packages/console-ui/src/views/clusterManage/edit/index.tsx
@@ -192,7 +192,7 @@ class EditCluster extends React.Component<any, any> {
         const isChangeVersion = dealData.changeVersion(componentTypeCode, compVersion);
         const config = componentConfig[COMPONEMT_CONFIG_KEY_ENUM[componentTypeCode]] || {}
         const { loadTemplate = {} } = config;
-        const stateStoreType= length === 0 ? undefined : length === 1 ? saveCompsData[0]?.key : 4
+        const stateStoreType = length === 0 ? undefined : length === 1 ? saveCompsData[0]?.key : 4
         const version = dealData.getCompsVersion(Number(componentTypeCode), compVersion)
         if ((!isNeedLoadTemp && (Object.keys(loadTemplate).length === 0 || isChangeVersion || storeType))) {
             Api.getLoadTemplate({
