308a67c39394555b03d4529c9127bc0e93208798
yuebai <yuebai@dtstack.com>
2020-11-23 10:43:40 +0800

    fix merge

--- engine-common/src/main/java/com/dtstack/engine/common/constrant/ConfigConstant.java
+++ engine-common/src/main/java/com/dtstack/engine/common/constrant/ConfigConstant.java
@@ -67,6 +67,7 @@ public class ConfigConstant {
     public static final String PRINCIPAL_FILE = "principalFile";
     public static final String KRB_NAME = "krbName";
     public static final String OPEN_KERBEROS = "openKerberos";
+    public static final String KRBNAME_DEFAULT = "krb5.conf";
     public static final String JAVA_SECURITY_KRB5_CONF = "java.security.krb5.conf";
     public static final String KRB5_CONF = "krb5.conf";
     public static final String KEYTAB_SUFFIX = ".keytab";
