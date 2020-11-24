2b71cc25ebe75431ff0eed9f7e32cb6270e94107
HiLany <lanyang@pivotal.net.cn>
2020-11-24 19:28:56 +0800

    junittest

--- engine-worker/engine-plugins/dtscript/yarn2-hdfs2-dtscript-core/dtscript-client/src/test/java/com/dtstack/engine/dtscript/DtScriptClientTest.java
+++ engine-worker/engine-plugins/dtscript/yarn2-hdfs2-dtscript-core/dtscript-client/src/test/java/com/dtstack/engine/dtscript/DtScriptClientTest.java
@@ -1,14 +1,23 @@
 package com.dtstack.engine.dtscript;
 
 import com.dtstack.engine.common.JobClient;
+import com.dtstack.engine.common.JobIdentifier;
 import com.dtstack.engine.common.enums.EJobType;
+import com.dtstack.engine.common.enums.RdosTaskStatus;
 import com.dtstack.engine.common.pojo.JobResult;
 import com.dtstack.engine.dtscript.client.Client;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.math3.analysis.function.Pow;
 import org.apache.commons.math3.analysis.function.Power;
+import org.apache.hadoop.yarn.api.records.ApplicationReport;
+import org.apache.hadoop.yarn.api.records.FinalApplicationStatus;
+import org.apache.hadoop.yarn.api.records.YarnApplicationState;
+import org.apache.hadoop.yarn.api.records.impl.pb.ApplicationReportPBImpl;
 import org.apache.hadoop.yarn.client.api.YarnClient;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
@@ -16,7 +25,8 @@ import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.util.Properties;
 
-import static org.powermock.api.mockito.PowerMockito.when;
+import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.*;
 
 
 /**
@@ -26,15 +36,21 @@ import static org.powermock.api.mockito.PowerMockito.when;
  * @create: 2020/11/19 09:52
  */
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({Client.class})
+@PrepareForTest({Client.class, StringUtils.class, YarnClient.class})
 @PowerMockIgnore("javax.net.ssl.*")
 public class DtScriptClientTest {
+
+    @Mock
+    Client client;
+
+    @Mock
+    YarnClient yarnClient;
 
 
     @Test
     public void testInit() throws Exception {
         DtScriptClient dtScriptClient = PowerMockito.mock(DtScriptClient.class);
-        PowerMockito.whenNew(DtScriptClient.class).withNoArguments().thenReturn(dtScriptClient);
+        whenNew(DtScriptClient.class).withNoArguments().thenReturn(dtScriptClient);
 
 //        PowerMockito.doNothing().when(dtScriptClient.init());
     }
@@ -46,14 +62,76 @@ public class DtScriptClientTest {
         JobClient jobClient = PowerMockito.mock(JobClient.class);
         JobResult jobResult = PowerMockito.mock(JobResult.class);
 
-        PowerMockito.whenNew(DtScriptClient.class).withNoArguments().thenReturn(dtScriptClient);
-        PowerMockito.when(jobClient.getJobType()).thenReturn(EJobType.KYLIN);
-        PowerMockito.when(dtScriptClient.processSubmitJobWithType(jobClient)).thenReturn(jobResult);
+        whenNew(DtScriptClient.class).withNoArguments().thenReturn(dtScriptClient);
+        when(jobClient.getJobType()).thenReturn(EJobType.KYLIN);
+        when(dtScriptClient.processSubmitJobWithType(jobClient)).thenReturn(jobResult);
+
+        Assert.assertNotNull(dtScriptClient.processSubmitJobWithType(jobClient));
     }
 
     @Test
-    public void cancelJob() throws Exception {
+    public void testCancelJob() throws Exception {
 
+        String jobId = "40c01cd0c53928fff6a55e8d8b8b022c";
+        String appId = "application_1594003499276_1278";
+        String taskId = "taskId";
+
+        DtScriptClient dtScriptClient = PowerMockito.mock(DtScriptClient.class);
+        JobIdentifier jobIdentifier = PowerMockito.mock(JobIdentifier.class);
+        JobResult jobResult = PowerMockito.mock(JobResult.class);
+
+        whenNew(DtScriptClient.class).withNoArguments().thenReturn(dtScriptClient);
+        whenNew(JobIdentifier.class).withArguments(jobId,appId,taskId).thenReturn(jobIdentifier);
+
+
+        when(jobIdentifier.getEngineJobId()).thenReturn(jobId);
+        client.kill(jobId);
+        when(dtScriptClient.cancelJob(jobIdentifier)).thenReturn(jobResult);
+
+        Assert.assertNotNull(dtScriptClient.cancelJob(jobIdentifier));
+
+    }
+
+    @Test
+    public void testGetJobStatus() throws Exception {
+
+        String jobId = "40c01cd0c53928fff6a55e8d8b8b022c";
+        String appId = "application_1594003499276_1278";
+        String taskId = "taskId";
+
+        DtScriptClient dtScriptClient = PowerMockito.mock(DtScriptClient.class);
+        JobIdentifier jobIdentifier = PowerMockito.mock(JobIdentifier.class);
+
+        whenNew(DtScriptClient.class).withNoArguments().thenReturn(dtScriptClient);
+        whenNew(JobIdentifier.class).withArguments(jobId,appId,taskId).thenReturn(jobIdentifier);
+        mockStatic(StringUtils.class);
+
+        when(StringUtils.isEmpty(jobIdentifier.getEngineJobId())).thenReturn(false);
+        when(jobIdentifier.getEngineJobId()).thenReturn(jobId);
+
+        ApplicationReportPBImpl report = PowerMockito.mock(ApplicationReportPBImpl.class);
+        when(client.getApplicationReport(jobIdentifier.getEngineJobId())).thenReturn(report);
+        when(report.getFinalApplicationStatus()).thenReturn(FinalApplicationStatus.SUCCEEDED);
+        when(report.getYarnApplicationState()).thenReturn(YarnApplicationState.RUNNING);
+        when(report.getTrackingUrl()).thenReturn("http://dtstack01:8088");
+
+        when(dtScriptClient.getJobStatus(jobIdentifier)).thenReturn(RdosTaskStatus.RUNNING);
+
+    }
+
+    @Test
+    public void testGetJobMaster() throws Exception {
+
+        String jobId = "40c01cd0c53928fff6a55e8d8b8b022c";
+        String appId = "application_1594003499276_1278";
+        String taskId = "taskId";
+        String url = "";
+
+        YarnClient yarnClient = PowerMockito.mock(YarnClient.class);
+        when(client.getYarnClient()).thenReturn(yarnClient);
+        Assert.assertNotNull(yarnClient);
+
+        yarnClient.getNodeReports();
 
     }
 
