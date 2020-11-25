e322dfb18270eaf0bfced16ce545f0e6ea99b052
chenfeng <chenfeng@dtstack.com>
2020-11-25 11:57:51 +0800

    build: add sonar to the project

--- engine-ui/packages/console-ui/jest.config.js
+++ engine-ui/packages/console-ui/jest.config.js
@@ -14,6 +14,7 @@ module.exports = {
         '**/__tests__/**/(*.)+(spec|test).[jt]s?(x)',
         '**/test/**/(*.)+(spec|test).[jt]s?(x)'
     ],
+    testResultsProcessor: "jest-sonar-reporter",
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
     moduleNameMapper: {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/mock/fileMock.js',
