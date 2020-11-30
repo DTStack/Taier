f5072b21a8cb41146e879f15bdbe75c60a06b2dc
yuebai <yuebai@dtstack.com>
2020-11-30 22:00:13 +0800

    remove sql

--- engine-master/engine-dao/src/main/java/com/dtstack/engine/dao/ScheduleJobDao.java
+++ engine-master/engine-dao/src/main/java/com/dtstack/engine/dao/ScheduleJobDao.java
@@ -43,8 +43,6 @@ public interface ScheduleJobDao {
 
     List<Map<String, Object>> listThirtyDayJobs(@Param("statusList") List<Integer> statusList, @Param("type") Integer type, @Param("taskTypes") List<Integer> taskTypes, @Param("projectId") Long projectId, @Param("tenantId") Long tenantId);
 
-    List<ScheduleJob> listRestartBatchJobList(@Param("type") int type, @Param("lastTime") Timestamp lastTime);
-
     List<ScheduleJob> listJobByJobKeys(@Param("jobKeys") Collection<String> jobKeys);
 
     List<Long> listIdByTaskIdAndStatus(@Param("taskId") Long taskId, @Param("statuses") List<Integer> status, @Param("appType") Integer appType);
