e6c6eee865ced422c1db99fc3aea3d223eaff26d
yaodong <tengzhen@dtstack.com>
2020-12-02 19:39:05 +0800

    增加kingbase插件

--- engine-worker/engine-plugins/rdbs/kingbase/src/main/java/com/dtstack/engine/rdbs/tidb/KingBaseConnFactory.java
+++ engine-worker/engine-plugins/rdbs/kingbase/src/main/java/com/dtstack/engine/rdbs/tidb/KingBaseConnFactory.java
@@ -14,7 +14,7 @@ public class KingBaseConnFactory extends AbstractConnFactory {
 
     @Override
     public String getCreateProcedureHeader(String procName) {
-        return String.format("create  procedure \"%s\" Authid Current_User as\n", procName);
+        return String.format("create  procedure \"%s\" \n", procName);
     }
 
     @Override
