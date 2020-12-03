958e1653333364ec180fb68fc3faca58e80563f4
xiuzhu <xiuzhu@dtstack.com>
2020-12-03 14:38:08 +0800

    [hotfix-33113] add shipfile hdfs-site.xml and yarn-site.xml

--- engine-worker/engine-plugins/flink/yarn2-hdfs2-flink180-core/src/main/java/com/dtstack/engine/flink/util/HadoopConf.java
+++ engine-worker/engine-plugins/flink/yarn2-hdfs2-flink180-core/src/main/java/com/dtstack/engine/flink/util/HadoopConf.java
@@ -43,6 +43,7 @@ public class HadoopConf {
             }
         });
         configuration.setBoolean(CommonConfigurationKeys.IPC_CLIENT_FALLBACK_TO_SIMPLE_AUTH_ALLOWED_KEY, true);
+        conf.put(CommonConfigurationKeys.IPC_CLIENT_FALLBACK_TO_SIMPLE_AUTH_ALLOWED_KEY, true);
     }
 
     public void initYarnConf(Map<String, Object> conf){
@@ -57,6 +58,7 @@ public class HadoopConf {
             }
         });
         HadoopConfTool.setDefaultYarnConf(yarnConfiguration, conf);
+        conf.put(CommonConfigurationKeys.IPC_CLIENT_FALLBACK_TO_SIMPLE_AUTH_ALLOWED_KEY, true);
     }
 
     public Configuration getConfiguration(){
--- engine-worker/engine-plugins/flink/yarn2-hdfs2-flink180-core/src/main/java/org/apache/flink/yarn/AbstractYarnClusterDescriptor.java
+++ engine-worker/engine-plugins/flink/yarn2-hdfs2-flink180-core/src/main/java/org/apache/flink/yarn/AbstractYarnClusterDescriptor.java
@@ -19,6 +19,7 @@
 package org.apache.flink.yarn;
 
 import avro.shaded.com.google.common.collect.Sets;
+import com.dtstack.engine.base.util.HadoopConfTool;
 import com.dtstack.engine.common.exception.ExceptionUtil;
 import com.dtstack.engine.flink.constrant.ConfigConstrant;
 import com.google.common.base.Strings;
@@ -590,11 +591,6 @@ public abstract class AbstractYarnClusterDescriptor implements ClusterDescriptor
             }
         }
 
-        String shipFileConf = System.getProperty("user.dir") + File.separator + "/shipFileConf";
-        File file = new File(shipFileConf);
-        if (file.exists() && file.isDirectory()) {
-            shipFiles.addAll(Arrays.asList(file.listFiles()));
-        }
         // flinkx get classpath
         jobGraph.getClasspaths().forEach(jarFile -> {
             try {
@@ -873,12 +869,6 @@ public abstract class AbstractYarnClusterDescriptor implements ClusterDescriptor
         for (File file : shipFiles) {
             systemShipFiles.add(file.getAbsoluteFile());
         }
-        String shipFileConf = System.getProperty("user.dir") + File.separator + "/shipFileConf";
-        File file = new File(shipFileConf);
-        if (file.exists() && file.isDirectory()) {
-            systemShipFiles.addAll(Arrays.asList(file.listFiles()));
-        }
-
 
         String logLevel = flinkConfiguration.getString("logLevel", "info").toLowerCase();
         /**
@@ -1007,17 +997,72 @@ public abstract class AbstractYarnClusterDescriptor implements ClusterDescriptor
                 TaskManagerOptions.TASK_MANAGER_HEAP_MEMORY_MB,
                 clusterSpecification.getTaskManagerMemoryMB());
 
-        // Upload the flink configuration
-        // write out configuration file
+
         File tmpFileDir =  new File(System.getProperty("user.dir") + File.separator + "tmp180");
         if (!tmpFileDir.exists()) {
             tmpFileDir.mkdirs();
         }
 
+        // Upload the hdfs-site.xml and yarn-site.xml
+        File tmpHdfsSiteFile = null;
+        File tmpYarnSiteFile = null;
+        Path remoteHdfsSitePath = null;
+        Path remoteYarnSitePath = null;
+        try {
+
+            // Write hdfs-site.xml
+            tmpHdfsSiteFile = File.createTempFile(appId + "-hdfs-site.xml", null, tmpFileDir);
+            byte[] hadoopConfBytes = configuration.getBytes(ConfigConstrant.HADOOP_CONF_BYTES_KEY, null);
+            HadoopConfTool.writeHadoopXml(HadoopConfTool.deserializeHadoopConf(hadoopConfBytes), tmpHdfsSiteFile);
+
+            String hdfsSiteKey = "hdfs-site.xml";
+            LOG.info("Adding HDFS configuration {} to the AM container local resource bucket", hdfsSiteKey);
+            remoteHdfsSitePath = setupSingleLocalResource(
+                    hdfsSiteKey,
+                    fs,
+                    appId,
+                    new Path(tmpHdfsSiteFile.getAbsolutePath()),
+                    localResources,
+                    homeDir,
+                    "");
+            envShipFileList.append(hdfsSiteKey).append("=").append(remoteHdfsSitePath).append(",");
+            paths.add(remoteHdfsSitePath);
+            classPathBuilder.append(hdfsSiteKey).append(File.pathSeparator);
+            configuration.setString(ConfigConstants.HDFS_SITE_CONFIG, hdfsSiteKey);
+
+            // Write yarn-site.xml
+            tmpYarnSiteFile = File.createTempFile(appId + "-yarn-site.xml", null, tmpFileDir);
+            byte[] yarnConfBytes = configuration.getBytes(ConfigConstrant.YARN_CONF_BYTES_KEY, null);
+            HadoopConfTool.writeHadoopXml(HadoopConfTool.deserializeYanrConf(yarnConfBytes), tmpYarnSiteFile);
+
+            String yarnSiteKey = "yarn-site.xml";
+            LOG.info("Adding YARN configuration {} to the AM container local resource bucket", yarnSiteKey);
+            remoteYarnSitePath = setupSingleLocalResource(
+                    yarnSiteKey,
+                    fs,
+                    appId,
+                    new Path(tmpYarnSiteFile.getAbsolutePath()),
+                    localResources,
+                    homeDir,
+                    "");
+            envShipFileList.append(yarnSiteKey).append("=").append(remoteYarnSitePath).append(",");
+            paths.add(remoteYarnSitePath);
+            classPathBuilder.append(yarnSiteKey).append(File.pathSeparator);
+        } finally {
+            if (tmpHdfsSiteFile != null && !tmpHdfsSiteFile.delete()) {
+                LOG.warn("Fail to delete temporary file {}.", tmpHdfsSiteFile.toPath());
+            }
+
+            if (tmpYarnSiteFile != null && !tmpYarnSiteFile.delete()) {
+                LOG.warn("Fail to delete temporary file {}.", tmpYarnSiteFile.toPath());
+            }
+        }
+
         File tmpConfigurationFile = File.createTempFile(appId + "-flink-conf.yaml", null , tmpFileDir);
-        tmpConfigurationFile.deleteOnExit();
         BootstrapTools.writeConfiguration(configuration, tmpConfigurationFile);
 
+        // Upload the flink configuration
+        // write out configuration file
         String flinkConfigKey = "flink-conf.yaml";
         Path remotePathConf = setupSingleLocalResource(
                 flinkConfigKey,
@@ -1070,7 +1115,6 @@ public abstract class AbstractYarnClusterDescriptor implements ClusterDescriptor
         //and KRB5 configuration files. We are adding these files as container local resources for the container
         //applications (JM/TMs) to have proper secure cluster setup
         Path remoteKrb5Path = null;
-        Path remoteYarnSiteXmlPath = null;
         boolean hasKrb5 = false;
         if (System.getenv("IN_TESTS") != null) {
             String krb5Config = System.getProperty("java.security.krb5.conf");
@@ -1083,18 +1127,6 @@ public abstract class AbstractYarnClusterDescriptor implements ClusterDescriptor
                         fs,
                         appId,
                         krb5ConfPath,
-                        localResources,
-                        homeDir,
-                        "");
-
-                File f = new File(System.getenv("YARN_CONF_DIR"), Utils.YARN_SITE_FILE_NAME);
-                LOG.info("Adding Yarn configuration {} to the AM container local resource bucket", f.getAbsolutePath());
-                Path yarnSitePath = new Path(f.getAbsolutePath());
-                remoteYarnSiteXmlPath = setupSingleLocalResource(
-                        Utils.YARN_SITE_FILE_NAME,
-                        fs,
-                        appId,
-                        yarnSitePath,
                         localResources,
                         homeDir,
                         "");
@@ -1162,8 +1194,8 @@ public abstract class AbstractYarnClusterDescriptor implements ClusterDescriptor
         }
 
         //To support Yarn Secure Integration Test Scenario
-        if (remoteYarnSiteXmlPath != null && remoteKrb5Path != null) {
-            appMasterEnv.put(YarnConfigKeys.ENV_YARN_SITE_XML_PATH, remoteYarnSiteXmlPath.toString());
+        if (remoteYarnSitePath != null && remoteKrb5Path != null) {
+            appMasterEnv.put(YarnConfigKeys.ENV_YARN_SITE_XML_PATH, remoteYarnSitePath.toString());
             appMasterEnv.put(YarnConfigKeys.ENV_KRB5_PATH, remoteKrb5Path.toString());
         }
 
