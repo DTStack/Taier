45afcbeb5dc4567030d1c11cd6ba307a7919ef74
chener <chener@dtstack.com>
2020-12-03 20:09:34 +0800

    the whole create table statement

--- engine-sql/engine-sql-hive/src/test/resources/info/createTable.sql
+++ engine-sql/engine-sql-hive/src/test/resources/info/createTable.sql
@@ -3,3 +3,10 @@ create TEMPORARY table IF NOT EXISTS test.chener(
  p2 string UNIQUE default 'ppp'
 ) comment 'table comment'
 PARTITIONED BY (pt int comment 'par')
+CLUSTERED BY(p1) SORTED BY (p1 ASC) INTO 3 BUCKETS
+SKEWED BY(p1,p2) on ((1,'haha'),(2,'lala')) STORED AS DIRECTORIES
+ROW FORMAT DELIMITED FIELDS TERMINATED BY char
+STORED AS orc
+LOCATION 'lcao'
+TBLPROPERTIES (comment='asd')
+as select p1,p2 from test.chen1
---
+++ engine-sql/engine-sql-hive/src/test/resources/info/createTable2.sql
@@ -0,0 +1,2 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS test.chener like test.chener2
+location 'path'
