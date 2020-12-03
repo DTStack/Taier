780551f4bebdd421f4b2c435d50987bdd4e844ec
yaodong <tengzhen@dtstack.com>
2020-12-03 20:32:23 +0800

    优化和验证kingbase插件

--- engine-worker/engine-plugins/rdbs/kingbase/src/test/java/com/dtstack/engine/rdbs/tidb/TestKingBase.java
+++ engine-worker/engine-plugins/rdbs/kingbase/src/test/java/com/dtstack/engine/rdbs/tidb/TestKingBase.java
@@ -64,10 +64,20 @@ public class TestKingBase {
 
     public static void  createTable(){
 
-        sql = "CREATE TABLE\"tengzhen_test\"(\n" +
-                "\"name\" VARCHAR (100) NOT NULL ,\n" +
-                "\"value\" VARCHAR (11) NOT NULL\n" +
+//        sql = "CREATE TABLE\"tengzhen_test\"(\n" +
+//                "\"name\" VARCHAR (100) NOT NULL ,\n" +
+//                "\"value\" VARCHAR (11) NOT NULL\n" +
+//                ");";
+
+                sql = "CREATE TABLE temp_data_tengzhen3(\n" +
+                "name VARCHAR (100) NOT NULL ,\n" +
+                "var INTEGER NOT NULL\n" +
                 ");";
+
+//        sql = "CREATE TABLE \"temp_data_tengzhen2\"(\n" +
+//                "\"name\" VARCHAR(100) NOT NULL,\n" +
+//                "\"var\" INT(10) NOT NULL\n" +
+//                ");";
         conn = new GetConn(sql);//创建数据库对象
         try {
             int count = conn.pst.executeUpdate();
@@ -119,48 +129,20 @@ public class TestKingBase {
 
     public static void  createProcedure(){
 
-        sql = "CREATE OR REPLACE  PROCEDURE PUBLIC.TEMP_EMP_CREATETAB() \n" +
-                "\n" +
-                "// 使用的是plsql 语言\n" +
-                " LANGUAGE PLSQL\n" +
-                "\n" +
-                "//创建变量创建的表名\n" +
-                "AS     tabName VARCHAR2(200);\n" +
-                "\n" +
-                "//统计数量\n" +
-                "      tabCount NUMBER;\n" +
-                "\n" +
-                "//创建表的语句\n" +
-                "      createTableSql VARCHAR2(300);\n" +
-                " BEGIN " +
-                "//为表名赋值\n" +
-                "    tabName := 'TEMP_EMP_DATA_' || TO_CHAR(sysdate, 'yyyyMMdd');\n" +
-                "//查询是否有同名的表    \n" +
-                "\n" +
-                "SELECT COUNT(1) INTO tabCount FROM user_tables WHERE TABLE_NAME = tabName;\n" +
-                "//如果没有则继续    \n" +
-                "\n" +
-                "if tabCount = 0 THEN\n" +
-                "\n" +
-                "//创建表的语句\n" +
-                "    createTableSql := 'CREATE TABLE '||tabName||'(\n" +
-                "        \"ID\" VARCHAR(64 byte) NOT NULL   \n" +
-                "    )';\n" +
-                "\n" +
-                "//执行建表语句\n" +
-                "    EXECUTE createTableSql;\n" +
-                "   END IF;\n" +
-                "END;";
+        sql = " create  procedure \"prod_1d0eb7hcg\" as \n" +
+                "begin \n" +
+                " create table temp_data_tengzhen4( name varchar(100) not null,var integer not null);\n" +
+                " insert into temp_data_tengzhen4(name,var) values('lisi',1); \n" +
+                " end;";
         conn = new GetConn(sql);//
         try {
-            int count = conn.pst.executeUpdate();
-            if (count>0){
-                System.out.println("创建成功");
-            }
-            else {
-                System.out.println("创建失败");
-            }
-            conn.close();//关闭连接
+            Connection conn = TestKingBase.conn.conn;
+            Statement statement = conn.createStatement();
+            statement.execute(sql);
+            CallableStatement callableStatement = conn.prepareCall("call prod_1d0eb7hcg()");
+            callableStatement.execute();
+
+            TestKingBase.conn.close();//关闭连接
         } catch (SQLException e) {
             e.printStackTrace();
         }
