77812e4fef606a06ea9b317cc1b9d17b681b2029
dazhi <xwm-25257758>
2021-01-11 15:39:22 +0800

    【控制台】【参数替换】

--- engine-master/engine-alert/src/main/java/com/dtstack/engine/alert/send/customize/TencentCloudCustomizeSender.java
+++ engine-master/engine-alert/src/main/java/com/dtstack/engine/alert/send/customize/TencentCloudCustomizeSender.java
@@ -6,6 +6,9 @@ import com.dtstack.engine.alert.send.AbstractSender;
 import com.dtstack.engine.api.domain.po.ClusterAlertPO;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Auther: dazhi
  * @Date: 2020/12/3 4:32 下午
@@ -24,8 +27,11 @@ public class TencentCloudCustomizeSender extends AbstractSender {
 
     private String buildCustomize(Object data, String source ) {
         JSONObject body = new JSONObject();
+        Map<String, String> dynamicParams = new HashMap<>();
+        dynamicParams.put("message", data.toString());
         body.put("data",data);
         body.put("source",source);
+        body.put("dynamicParams", dynamicParams);
         return body.toJSONString();
     }
 }
