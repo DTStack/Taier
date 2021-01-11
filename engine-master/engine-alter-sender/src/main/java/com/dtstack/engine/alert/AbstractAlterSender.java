a9cb3b2b9a269b5238a2b09d5a5259045789e9d0
dazhi <xwm-25257758>
2021-01-11 16:04:53 +0800

    【console】【重构优化】

---
+++ engine-master/engine-alter-sender/src/main/java/com/dtstack/engine/alert/AbstractAlterSender.java
@@ -0,0 +1,14 @@
+package com.dtstack.engine.alert;
+
+/**
+ * @Auther: dazhi
+ * @Date: 2021/1/11 4:02 下午
+ * @Email:dazhi@dtstack.com
+ * @Description:
+ */
+public class AbstractAlterSender implements AlterSender {
+
+
+
+
+}
---
+++ engine-master/engine-alter-sender/src/main/java/com/dtstack/engine/alert/AlterSender.java
@@ -0,0 +1,13 @@
+package com.dtstack.engine.alert;
+
+/**
+ * @Auther: dazhi
+ * @Date: 2021/1/11 4:01 下午
+ * @Email:dazhi@dtstack.com
+ * @Description:
+ */
+public interface AlterSender {
+
+
+
+}
