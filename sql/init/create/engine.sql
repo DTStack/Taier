1ddd35e694d2540c3000c3fb65254d8a33026422
yuebai <yuebai@dtstack.com>
2021-01-20 14:59:02 +0800

    【sql】【fix sql】

--- sql/init/create/engine.sql
+++ sql/init/create/engine.sql
@@ -463,7 +463,7 @@ CREATE TABLE `schedule_task_commit` (
   `task_id` int(11) NOT NULL COMMENT '任务id',
   `app_type` int(11) NOT NULL DEFAULT '0' COMMENT 'RDOS(1), DQ(2), API(3), TAG(4), MAP(5), CONSOLE(6), STREAM(7), DATASCIENCE(8)',
   `commit_id` varchar(128) NOT NULL COMMENT '提交id',
-  `task_json` text COMMENT '额外参数',
+  `task_json` longtext COMMENT '额外参数',
   `extra_info` mediumtext COMMENT '存储task运行时所需的额外信息',
   `is_commit` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否提交：0未提交 1已提交',
   `is_deleted` tinyint(1) NOT NULL DEFAULT '0' COMMENT '过期策略：0永不过期 1过期取消',
