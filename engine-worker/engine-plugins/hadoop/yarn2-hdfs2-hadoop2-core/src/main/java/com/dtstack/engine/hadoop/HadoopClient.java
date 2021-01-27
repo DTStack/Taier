be4eb4442ccce7ba3fe6021fb8cdd09ebaa7a944
HiLany <lanyang@pivotal.net.cn>
2021-01-27 20:00:53 +0800

    [hotfix-34748][hadoop]优化handleSql方法

--- engine-worker/engine-plugins/hadoop/yarn2-hdfs2-hadoop2-core/src/main/java/com/dtstack/engine/hadoop/HadoopClient.java
+++ engine-worker/engine-plugins/hadoop/yarn2-hdfs2-hadoop2-core/src/main/java/com/dtstack/engine/hadoop/HadoopClient.java
@@ -354,13 +354,13 @@ public class HadoopClient extends AbstractClient {
         }
 
         List<String> sqlList = Lists.newArrayList(sqlArr);
-        // handle add jar statements and comment statements on the same line
-        AddJarOperator.handleFirstSql(sqlList);
         Iterator<String> sqlItera = sqlList.iterator();
         List<String> fileList = Lists.newArrayList();
 
         while (sqlItera.hasNext()){
             String tmpSql = sqlItera.next();
+            // handle add jar statements and comment statements on the same line
+            tmpSql = AddJarOperator.handleSql(tmpSql);
             if(AddJarOperator.verific(tmpSql)){
                 sqlItera.remove();
                 JarFileInfo jarFileInfo = AddJarOperator.parseSql(tmpSql);
