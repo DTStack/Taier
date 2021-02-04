c3312f6884c3e6644ad5459f746f2fcd6ac39fe5
赵振智 <dazhi@dtstack.com>
2021-02-04 10:55:30 +0800

    【控制台】【修改上传文件的】【35311】

--- engine-master/engine-master-server/src/main/java/com/dtstack/engine/master/config/MvcConfig.java
+++ engine-master/engine-master-server/src/main/java/com/dtstack/engine/master/config/MvcConfig.java
@@ -29,6 +29,11 @@ import java.util.List;
 public class MvcConfig extends DelegatingWebMvcConfiguration {
 
 
+    @Value("${spring.servlet.multipart.max-file-size:10MB}")
+    private String maxFileSize;
+
+    @Value("${spring.servlet.multipart.max-request-size:10MB}")
+    private String maxRequestSize;
 
     private static final List<String> INTERCEPT_LIST;
 
