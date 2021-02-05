cabb84a9438b07b78857e8b740460fb1304f7da8
zyd <zyd@qq.com>
2021-02-05 11:36:23 +0800

    没上线前，增量sql直接写在原来的文件里

--- sql/4.0/202102032144_v4.1.x.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `lineage_data_source`
-DROP INDEX `uni_tenant_source_key` ,
-ADD UNIQUE INDEX `uni_tenant_source_key` (`dt_uic_tenant_id`, `source_key`, `app_type`, `source_name`) USING BTREE ;
