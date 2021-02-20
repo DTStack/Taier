da72afe2712912ff2a7205d80a8b6bc47a9728a6
yuebai <yuebai@dtstack.com>
2021-02-20 17:00:09 +0800

    【fix】【remove expire】

--- engine-master/engine-master-server/src/main/java/com/dtstack/engine/master/jobdealer/JobStopDealer.java
+++ engine-master/engine-master-server/src/main/java/com/dtstack/engine/master/jobdealer/JobStopDealer.java
@@ -325,11 +325,6 @@ public class JobStopDealer implements InitializingBean, DisposableBean {
                 this.removeMemStatusAndJobCache(jobElement.jobId);
                 logger.info("jobId:{} and status:{} is StoppedAndNotFound, set job is STOPPED.", jobElement.jobId, scheduleJob.getStatus());
                 return StoppedStatus.STOPPED;
-            } else if (!RdosTaskStatus.RUNNING.getStatus().equals(scheduleJob.getStatus()) &&
-                    System.currentTimeMillis() - jobCache.getGmtCreate().getTime() >= environmentContext.getConsoleStopExpireTime()) {
-                this.removeMemStatusAndJobCache(jobElement.jobId);
-                logger.info("jobId:{} and status:{} is expire console stop time, set job is STOPPED.", jobElement.jobId, scheduleJob.getStatus());
-                return StoppedStatus.STOPPED;
             }
 
 
