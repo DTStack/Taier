32c48f71aff80ab07a2888ad0029a600852856e2
HiLany <lanyang@pivotal.net.cn>
2021-02-22 12:01:16 +0800

    [hotfix-35325][flink]fix monitor url

--- engine-worker/engine-plugins/flink/yarn2-hdfs2-flink180-core/src/main/java/org/apache/flink/yarn/AbstractYarnClusterDescriptor.java
+++ engine-worker/engine-plugins/flink/yarn2-hdfs2-flink180-core/src/main/java/org/apache/flink/yarn/AbstractYarnClusterDescriptor.java
@@ -649,7 +649,7 @@ public abstract class AbstractYarnClusterDescriptor implements ClusterDescriptor
                 if (rmValue == null) {
                     continue;
                 }
-                String requestUrl = String.format("http://%s/proxy", rmValue);
+                String requestUrl = String.format("http://%s", rmValue);
                 try {
                     PoolHttpClient.get(requestUrl, null, 1);
 
@@ -657,7 +657,7 @@ public abstract class AbstractYarnClusterDescriptor implements ClusterDescriptor
                     LOG.warn(String.format("RequestUrl [%s] can not be accessed.", requestUrl), e);
                     continue;
                 }
-                url = String.format("%s/%s",requestUrl, appId);
+                url = String.format("%s/proxy/%s",requestUrl, appId);
                 break;
             }
         }
