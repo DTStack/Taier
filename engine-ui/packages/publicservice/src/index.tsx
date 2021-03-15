e02fe37aae7ff60bc2df5e9b92c076863a6d856f
xingye <xingye@dtstack.com>
2021-03-15 20:36:24 +0800

    Update index.tsx remove comments

--- engine-ui/packages/publicservice/src/index.tsx
+++ engine-ui/packages/publicservice/src/index.tsx
@@ -6,7 +6,6 @@ import zhCN from 'antd/es/locale/zh_CN';
 import Routers from './router';
 import 'assets/styles/index.less';
 import store from './store';
-// reset.less中已经引入了antd.less，故在项目中无需再引入antd样式文件
 import 'ant-design-dtinsight-theme/theme/dt-theme/reset.less';
 import 'ant-design-dtinsight-theme/theme/dt-theme/index.less';
 
