5422a8186d3ca7d280aeddb16cfd8a77b6b2d7e5
zyd <zyd@qq.com>
2021-04-08 15:10:12 +0800

    [feat_4.2.x_1156][engine-lineage],[updateBySourceIdAndAppType接口参数加上注释]

--- engine-master/engine-master-server/src/main/java/com/dtstack/engine/master/controller/LineageDataSourceController.java
+++ engine-master/engine-master-server/src/main/java/com/dtstack/engine/master/controller/LineageDataSourceController.java
@@ -125,7 +125,7 @@ public class LineageDataSourceController {
     @RequestMapping(value="/updateBySourceIdAndAppType",method = RequestMethod.POST)
     @ApiOperation(value = "根据平台sourceId和appType修改数据源")
     @ApiImplicitParam(name="dataSourceParam",value = "老的数据源列表")
-    public Boolean updateDataSourceBySourceIdAndAppType(DataSourceDTO dataSourceDTO){
+    public Boolean updateDataSourceBySourceIdAndAppType(@RequestBody DataSourceDTO dataSourceDTO){
 
         return dataSourceService.updateDataSourceBySourceIdAndAppType(dataSourceDTO);
     }
