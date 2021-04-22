5eb7815536839e745e67ef16de1824663a57e7da
xiaowei <wewoor@gmail.com>
2021-04-22 16:34:22 +0800

    feat: import Workbench component

--- engine-ui/src/App.tsx
+++ engine-ui/src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Workbench from './workbench';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Workbench />
     </div>
   );
 }
