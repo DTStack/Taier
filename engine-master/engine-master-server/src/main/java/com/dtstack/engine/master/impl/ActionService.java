d3834f49be150a0788b5fdb3abe1ac871d18c63b
zyd <zyd@qq.com>
2021-05-26 13:55:43 +0800

    [feat_4.2.x_1729][engine-master][getByEngineName方法调用处修改]

--- engine-master/engine-master-server/src/main/java/com/dtstack/engine/master/impl/ActionService.java
+++ engine-master/engine-master-server/src/main/java/com/dtstack/engine/master/impl/ActionService.java
@@ -2,6 +2,7 @@ package com.dtstack.engine.master.impl;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.dtstack.dtcenter.common.enums.DeployMode;
 import com.dtstack.engine.api.domain.*;
 import com.dtstack.engine.api.pojo.ParamAction;
 import com.dtstack.engine.api.pojo.ParamActionExt;
@@ -741,7 +742,7 @@ public class ActionService {
         }
         // 由于/start接口taskType参数
         EComponentType componentType = useTaskType? ComponentVersionUtil.transformTaskType2ComponentType(paramActionExt.getTaskType()) :
-                EngineTypeComponentType.getByEngineName(paramActionExt.getEngineType(), Integer.valueOf(paramActionExt.getDeployMode())).getComponentType();
+                EngineTypeComponentType.getByEngineName(paramActionExt.getEngineType(), DeployMode.SESSION.getValue()).getComponentType();
         // ParamAction中的taskType并非约定的任务和组件,
         if (Objects.nonNull(componentType)){
             scheduleJob.setComponentVersion(componentDao.getDefaultComponentVersionByTenantAndComponentType(
