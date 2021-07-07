88625786faa23c1eaa8dd1dcbec45515a6563cdd
xiaowei <wewoor@gmail.com>
2021-07-07 14:35:32 +0800

    build: remove the duplicate microApp build command

--- engine-ui/scripts/build.sh
+++ engine-ui/scripts/build.sh
@@ -6,13 +6,13 @@ rm -rf ./out/;
 mkdir ./out/;
 
 echo "Start to build the console-ui";
-lerna run microApp --scope console-ui
+lerna run build --scope=console-ui
 
 echo "Copy the console-ui dist to the out";
 cp -R ./packages/console-ui/dist/* ./out/
 
 echo "Start to build the ide-ui";
-lerna run build --scope ide-ui
+lerna run build --scope=ide-ui
 
 echo "Copy the ide-ui dist to the out";
 cp -R ./packages/ide-ui/build/* ./out/
