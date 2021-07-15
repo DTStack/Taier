cb2d3d57a9fb744f56b0247f3a570e856a251c32
赵振智 <dazhi@dtstack.com>
2021-07-15 11:30:42 +0800

    [feat_4.1.x_39708_jobgraph][engine-all][刷job的sql]

--- sql/4.0/202107050000_v4.1.x.sql
+++ sql/4.0/202107050000_v4.1.x.sql
@@ -5,4 +5,8 @@ ALTER TABLE `schedule_job` ADD INDEX index_flow_job_id ( `flow_job_id` );
 
 ALTER TABLE schedule_job_graph_trigger MODIFY `min_job_id` bigint(20) NOT NULL DEFAULT '0' COMMENT '生成graph时对应的job起始id';
 
-UPDATE schedule_job_graph_trigger SET min_job_id = CONCAT(SUBSTRING(FROM_UNIXTIME(UNIX_TIMESTAMP(trigger_time),'%Y%m%d%h%i'),3),'000000000');
+UPDATE schedule_job_graph_trigger SET min_job_id = CONCAT(SUBSTRING(FROM_UNIXTIME(UNIX_TIMESTAMP(trigger_time),'%Y%m%d%H%i'),3),'000000000');
+
+UPDATE schedule_job set job_execute_order = CONCAT(SUBSTRING(cyc_time,3,LENGTH(cyc_time)-4),RIGHT(CONCAT('000000000', id),9))
+WHERE cyc_time > FROM_UNIXTIME(UNIX_TIMESTAMP(CAST(SYSDATE()AS DATE) - INTERVAL 1 DAY),'%Y%m%d%H%i%s')
+and cyc_time <  FROM_UNIXTIME(UNIX_TIMESTAMP(DATE_SUB( DATE_ADD(CURRENT_DATE, INTERVAL 1 DAY),INTERVAL 1 SECOND)),'%Y%m%d%H%i%s');
