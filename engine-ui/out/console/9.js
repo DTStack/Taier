d2522c9cf388e43c81aafd8dbc0daf01b5086439
ProfBramble <1207107376@qq.com>
2021-07-16 15:21:03 +0800

    build: remove static resource file

--- engine-ui/out/console/9.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp_console-ui"]=window["webpackJsonp_console-ui"]||[]).push([[9],{KHRF:function(o,e,n){"use strict";n("eoL8"),Object.defineProperty(e,"__esModule",{value:!0}),e.language=e.conf=void 0;e.conf={};e.language={defaultToken:"",ignoreCase:!0,tokenPostfix:".dtlog",tokenizer:{root:[[/[=]{5,}.*[=]{5,}/,"comment"]]}}}}]);
