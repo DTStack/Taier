246dfe8a6f7c89791a6b53662cef5e5628878d29
chenfeng <chenfeng@dtstack.com>
2021-08-05 15:10:44 +0800

    feat: add hash history

--- engine-ui/packages/ide-ui/src/index.tsx
+++ engine-ui/packages/ide-ui/src/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { hashHistory } from 'react-router';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App history={hashHistory} />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
