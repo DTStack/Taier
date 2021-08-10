348a4a9bfdcd61bf0f1bf9a348a31d9441cb721c
toutian <953372946@qq.com>
2021-08-10 19:24:09 +0800

    fix

--- engine-client-sdk/src/main/java/com/dtstack/engine/api/service/ClusterService.java
+++ engine-client-sdk/src/main/java/com/dtstack/engine/api/service/ClusterService.java
@@ -1,13 +1,5 @@
 package com.dtstack.engine.api.service;
 
-import com.dtstack.engine.api.dto.ClusterDTO;
-import com.dtstack.engine.api.pager.PageResult;
-import com.dtstack.engine.api.vo.ClusterVO;
-
-
-
-import java.util.List;
-
 public interface ClusterService {
 
     /**
