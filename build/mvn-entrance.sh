ca47a99dc7ac6e3aedfab47b545c837b64e91193
shengpei <fenghe@dtstack.com>
2021-08-17 14:16:52 +0800

    restore mvn-entrance.sh

--- build/mvn-entrance.sh
+++ build/mvn-entrance.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo 'DAGSchedulex Building...'
 
-mvn -s /Users/shengpei/.m2/settings2.xml clean package -DskipTests  -pl \
+mvn clean package -DskipTests  -pl \
 engine-entrance \
 -am
