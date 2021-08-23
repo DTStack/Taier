3cc207a824ff2bee90adaf02ae74174854a873f9
yuebai <yuebai@dtstack.com>
2021-08-23 15:43:08 +0800

    [feat_4.1.x_delete][engine-master][sql change]

--- sql/4.0/202107051800_v4.1.x.sql
+++ sql/4.0/202107051800_v4.1.x.sql
@@ -8,9 +8,7 @@ INSERT INTO schedule_dict (dict_code, dict_name, dict_value, dict_desc, type, so
 
 INSERT INTO schedule_dict (dict_code, dict_name, dict_value, dict_desc, type, sort, data_type, depend_name, is_default, gmt_create, gmt_modified, is_deleted) VALUES ('DATA_CLEAR_NAME', 'schedule_fill_data_job', '{"deleteDateConfig":30,"clearDateConfig":180}', null, 7, 1, 'STRING', '', 0, '2021-07-05 11:22:36', '2021-07-05 11:22:36', 0);
 
-INSERT INTO schedule_dict (dict_code, dict_name, dict_value, dict_desc, type, sort, data_type, depend_name, is_default, gmt_create, gmt_modified, is_deleted) VALUES ('DATA_CLEAR_NAME', 'schedule_engine_job_checkpoint', '{"deleteDateConfig":30,"clearDateConfig":60,"appendWhere":"and checkpoint_savepath is null"}', null, 7, 1, 'STRING', '', 0, '2021-07-05 11:22:36', '2021-07-05 11:22:36', 0);
-
-INSERT INTO schedule_dict (dict_code, dict_name, dict_value, dict_desc, type, sort, data_type, depend_name, is_default, gmt_create, gmt_modified, is_deleted) VALUES ('DATA_CLEAR_NAME', 'schedule_engine_unique_sign', '{"directDelete":true,"deleteDateConfig":240}', null, 7, 1, 'STRING', '', 0, '2021-07-05 11:22:36', '2021-07-05 11:22:36', 0);
+INSERT INTO schedule_dict (dict_code, dict_name, dict_value, dict_desc, type, sort, data_type, depend_name, is_default, gmt_create, gmt_modified, is_deleted) VALUES ('DATA_CLEAR_NAME', 'schedule_engine_unique_sign', '{"directDelete":true,"deleteDateConfig":270}', null, 7, 1, 'STRING', '', 0, '2021-07-05 11:22:36', '2021-07-05 11:22:36', 0);
 
 INSERT INTO schedule_dict (dict_code, dict_name, dict_value, dict_desc, type, sort, data_type, depend_name, is_default, gmt_create, gmt_modified, is_deleted) VALUES ('DATA_CLEAR_NAME', 'schedule_plugin_job_info', '{"deleteDateConfig":30,"clearDateConfig":60}', null, 7, 1, 'STRING', '', 0, '2021-07-05 11:22:36', '2021-07-05 11:22:36', 0);
 
