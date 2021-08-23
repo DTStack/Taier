0d67109ca1e5cfef52e4cef48bd1f23b9956ded3
xiuneng <xiuneng@dtstack.com>
2021-08-23 15:59:04 +0800

    fix: remove Output panel && and log panel

--- engine-ui/packages/ide-ui/src/views/common/panel/index.tsx
+++ engine-ui/packages/ide-ui/src/views/common/panel/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import molecule from 'molecule';
-import type { IEditorTab, IExtension } from 'molecule/esm/model';
+import { IEditorTab, IExtension, PANEL_OUTPUT } from 'molecule/esm/model';
+import { MarkdownRender } from 'dt-react-component';
 import { connect } from 'molecule/esm/react';
 import EnvParams from '../../task/envParams';
-import { ENV_PARAMS } from '../utils/const';
+import { ENV_PARAMS, OUTPUT_LOG } from '../utils/const';
 
 function initEnvParams() {
     const EnvParamsView = connect(molecule.editor, EnvParams);
@@ -25,5 +26,17 @@ function initEnvParams() {
 export default class PanelExtension implements IExtension {
     activate() {
         initEnvParams();
+
+        molecule.panel.remove(PANEL_OUTPUT);
+        molecule.panel.add({
+            id: OUTPUT_LOG,
+            name: '日志',
+            sortIndex: 1,
+            renderPane: () => (
+                <div>
+                    <MarkdownRender text="[test](https://www.baidu.com)" />
+                </div>
+            ),
+        });
     }
 }
