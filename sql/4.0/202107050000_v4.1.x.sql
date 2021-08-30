f0b0332fa371a08c93cc14b22b975e5da0920724
赵振智 <dazhi@dtstack.com>
2021-08-30 10:39:54 +0800

    [hotfix_4.2.x_42814][engine-all][删除索引]

--- sql/4.0/202107050000_v4.1.x.sql
+++ sql/4.0/202107050000_v4.1.x.sql
@@ -1,7 +1,6 @@
 ALTER TABLE `schedule_job` ADD COLUMN `job_execute_order` BIGINT NOT NULL DEFAULT 0 COMMENT '按照计算时间排序字段';
 
 ALTER TABLE `schedule_job` ADD INDEX index_job_execute_order ( `job_execute_order` );
-ALTER TABLE `schedule_job` ADD INDEX index_flow_job_id ( `flow_job_id` );
 
 ALTER TABLE schedule_job_graph_trigger MODIFY `min_job_id` bigint(20) NOT NULL DEFAULT '0' COMMENT '生成graph时对应的job起始id';
 
