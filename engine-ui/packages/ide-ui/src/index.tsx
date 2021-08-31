e9212f85574f78e035abc123d20f353b150c8251
jingyu <jingyu@dtstack.com>
2021-08-31 15:08:04 +0800

    fix: add dt-react-component css

--- engine-ui/packages/ide-ui/src/index.tsx
+++ engine-ui/packages/ide-ui/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { hashHistory } from 'react-router';
+import 'dt-react-component/lib/style/index.css';
 
 import App from './views/App';
 import '@/styles/task/task.scss';
