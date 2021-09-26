c5533acfc5af7e2e4c82ca32020aeb3d0df6f407
小威 <xiaowei@dtstack.com>
2021-09-26 20:15:48 +0800

    Revert "Merge branch 'refactor/taskInfo' into 'master'"
    This reverts merge request !51

--- engine-ui/packages/ide-ui/src/views/task/taskInfo.tsx
+++ engine-ui/packages/ide-ui/src/views/task/taskInfo.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { Col, Row, Tag } from 'antd';
+import { Col, Row } from 'antd';
 import { IEditor } from 'molecule/esm/model';
 import { TaskType } from '../../components/status';
 import { formatDateTime } from '../../comm';
-import { TASK_TYPE } from '../../comm/const';
-import './taskInfo.scss';
 
 export default function TaskInfo({ current }: IEditor) {
     if (!current?.activeTab)
@@ -13,87 +11,48 @@ export default function TaskInfo({ current }: IEditor) {
                 无法提供活动属性
             </div>
         );
-    const taskInfo = current.tab!.data;
+    const tab = current.tab!;
     const labelPrefix = '任务';
     return (
-        <Row className="task-info">
-            <Row className="task-info-line">
-                <Col span={10} className="txt-left">
-                    {labelPrefix}名称
+        <Row className="task-info" style={{ padding: '5px 20px' }}>
+            <Row>
+                <Col span={8} >
+                    {labelPrefix}名称：
                 </Col>
-                <Col span={14} className="txt-right">
-                    {taskInfo.name}
-                </Col>
+                <Col span={8}>{tab.name}</Col>
             </Row>
-            <Row className="task-info-line">
-                <Col span={10} className="txt-left">
-                    {labelPrefix}类型
+            <Row>
+                <Col span={8}>
+                    {labelPrefix}类型：
                 </Col>
-                <Col span={14} className="txt-right">
-                    <TaskType value={taskInfo.taskType} />
-                </Col>
-            </Row>
-            {(taskInfo.taskType === TASK_TYPE.SQL ||
-                taskInfo.taskType === TASK_TYPE.MR ||
-                taskInfo.taskType === TASK_TYPE.PYTHON) && (
-                <Row className="task-info-line">
-                    <Col span={10} className="txt-left">
-                        Spark引擎版本
-                    </Col>
-                    <Col span={14} className="txt-right">
-                        {taskInfo.componentVersion}
-                    </Col>
-                </Row>
-            )}
-            {taskInfo.resourceList &&
-                taskInfo.resourceList.length > 0 &&
-                taskInfo.resourceList !== [] && (
-                    <Row className="task-info-line">
-                        <Col span={10} className="txt-left">
-                            资源
-                        </Col>
-                        <Col span={14} className="txt-right">
-                            {taskInfo.resourceList.map((o: any) => (
-                                <Tag
-                                    key={o.id}
-                                    color="blue"
-                                    style={{ marginTop: 10 }}
-                                >
-                                    {o.resourceName}
-                                </Tag>
-                            ))}
-                        </Col>
-                    </Row>
-                )}
-            <Row className="task-info-line">
-                <Col span={10} className="txt-left">
-                    创建时间
-                </Col>
-                <Col span={14} className="txt-right">
-                    {formatDateTime(taskInfo.gmtCreate)}
-                </Col>
-            </Row>
-            <Row className="task-info-line">
-                <Col span={10} className="txt-left">
-                    最近修改时间
-                </Col>
-                <Col span={14} className="txt-right">
-                    {formatDateTime(taskInfo.gmtModified)}
+                <Col span={14}>
+                    <TaskType value={tab.data.taskType} />
                 </Col>
             </Row>
             <Row>
-                <Col span={10} className="txt-left">
-                    描述
+                <Col span={8}>
+                    创建时间：
+                </Col>
+                <Col span={14}>{formatDateTime(tab.data.gmtCreate)}</Col>
+            </Row>
+            <Row>
+                <Col span={8}>
+                    修改时间：
+                </Col>
+                <Col span={14}>{formatDateTime(tab.data.gmtModified)}</Col>
+            </Row>
+            <Row>
+                <Col span={8}>
+                    描述：
                 </Col>
                 <Col
                     span={14}
                     style={{
-                        lineHeight: '40px',
+                        lineHeight: '20px',
                         padding: '10 0',
                     }}
-                    className="txt-right"
                 >
-                    {taskInfo.taskDesc ? taskInfo.taskDesc : '—'}
+                    {tab.data.taskDesc || '-'}
                 </Col>
             </Row>
         </Row>
