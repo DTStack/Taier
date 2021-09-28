bea296bf4c8f46e928f72451d78be0d4e419ccb4
chenfeng <profbramble@gmail.com>
2021-09-28 11:56:25 +0800

    build: add the fast Deploy script (#57)
    * feat: add fast deploy script
    
    * feat: adjust deploy process
    
    * feat: add micro application constant
    
    * feat: add deploy script
    
    * feat: dynamic cup configuration
    
    * docs: add deployment documentation
    
    * docs: add description and deployment
    
    * feat: adjust the acquisition method of bundle and iconfont
    
    * feat: add micro app config
    
    * refactor: decouple startup and deployment processes
    
    * feat: basic commands for composite projects

---
+++ engine-ui/scripts/deploy/build.js
@@ -0,0 +1,54 @@
+ const path = require('path');
+ const fs = require('fs');
+ const {
+     packagesPath,
+     scriptsPath,
+     isDirectory,
+     useSpawn,
+     logger,
+     microAdaptation,
+     resetContent
+ } = require('./base.js');
+ 
+ const fastBuild = async () => {
+     try {
+         const files = fs.readdirSync(packagesPath);
+         const projects = files.filter((item) => {
+             return isDirectory(path.join(packagesPath, item));
+         });
+ 
+         /**
+          * git synchronizes the latest code
+          */
+         await useSpawn('yarn', ['pull'], true);
+ 
+         /**
+          * update project dependencies
+          */
+         await useSpawn('lerna', ['bootstrap'], true);
+ 
+         /**
+          * process webpack to add a common prefix and process iconfont
+          */
+         microAdaptation(projects);
+ 
+         /**
+          * lerna build
+          */
+         await useSpawn('sh', [`${scriptsPath}/build.sh`], true);
+ 
+         /**
+          * restore the scene
+          */
+         resetContent();
+     } catch (error) {
+         /**
+          * restore the file contents and interrupt the process
+          */
+         resetContent();
+         return logger(error);
+     }
+ };
+ 
+ fastBuild();
+ 
