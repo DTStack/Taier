2e9f136d9d5bdbbc88797a3b4283eb6202d4fcbe
jiangyue <jiangyue@dtstack.com>
2021-12-29 19:49:19 +0800

    [feat_master][engine-datadevelop] modify get ClusterService.pluginInfoForType

--- engine-datadevelop/src/main/java/com/dtstack/batch/dao/BatchTaskResourceDao.java
+++ engine-datadevelop/src/main/java/com/dtstack/batch/dao/BatchTaskResourceDao.java
@@ -28,17 +28,15 @@ public interface BatchTaskResourceDao {
     /**
      * @param taskId
      * @param resourceType  资源类型 -- ResourceRefType
-     * @param projectId
      * @return
      */
-    List<BatchTaskResource> listByTaskId(@Param("taskId") long taskId, @Param("resourceType") Integer resourceType, @Param("projectId") long projectId);
+    List<BatchTaskResource> listByTaskId(@Param("taskId") Long taskId, @Param("resourceType") Integer resourceType);
 
-    BatchTaskResource getByTaskIdAndResourceId(@Param("taskId") long taskId, @Param("projectId") long projectId,
-                                               @Param("resourceId") long resourceId, @Param("resourceType") Integer resourceType);
+    BatchTaskResource getByTaskIdAndResourceId(@Param("taskId") Long taskId, @Param("resourceId") Long resourceId, @Param("resourceType") Integer resourceType);
 
-    Integer deleteByTaskId(@Param("taskId") long taskId, @Param("projectId") Long projectId, @Param("resourceType") Integer resourceType);
+    Integer deleteByTaskId(@Param("taskId") Long taskId, @Param("resourceType") Integer resourceType);
 
-    Integer logicDeleteByTaskId(@Param("taskId") long taskId, @Param("projectId") Long projectId, @Param("resourceType") Integer resourceType);
+    Integer logicDeleteByTaskId(@Param("taskId") Long taskId, @Param("resourceType") Integer resourceType);
 
     List<BatchTaskResource> listByResourceId(@Param("resourceId") Long resourceId);
 
