f7dfd2568b062e7b9870490a2c0623fa1b7384e0
jiangyue <jiangyue@dtstack.com>
2021-12-29 20:09:18 +0800

    [feat_master][engine-datadevelop] modify get ClusterService.pluginInfoForType

--- engine-datadevelop/src/main/java/com/dtstack/batch/web/task/vo/query/BatchTaskResourceGetResourcesVO.java
+++ engine-datadevelop/src/main/java/com/dtstack/batch/web/task/vo/query/BatchTaskResourceGetResourcesVO.java
@@ -30,9 +30,6 @@ public class BatchTaskResourceGetResourcesVO extends DtInsightAuthParam {
     @ApiModelProperty(value = "任务 ID", example = "1", required = true)
     private Long taskId;
 
-    @ApiModelProperty(value = "项目 ID", hidden = true)
-    private Long projectId;
-
     @ApiModelProperty(value = "类型", example = "1", required = true)
     private Integer type;
 
