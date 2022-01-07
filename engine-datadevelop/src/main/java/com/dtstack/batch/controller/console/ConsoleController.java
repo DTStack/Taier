1daed4711c893fbb98586609e60880455f5eec7d
vainhope <845869847@qq.com>
2022-01-07 14:01:29 +0800

    [feat_master][engine-all] remove plugins

--- engine-datadevelop/src/main/java/com/dtstack/batch/controller/console/ConsoleController.java
+++ engine-datadevelop/src/main/java/com/dtstack/batch/controller/console/ConsoleController.java
@@ -19,13 +19,17 @@
 package com.dtstack.batch.controller.console;
 
 import com.dtstack.batch.service.console.ConsoleService;
+import com.dtstack.engine.common.lang.web.R;
 import com.dtstack.engine.master.vo.console.ConsoleJobVO;
 import com.dtstack.engine.pager.PageResult;
 import com.dtstack.engine.pluginapi.pojo.ClusterResource;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 import java.util.List;
@@ -40,68 +44,71 @@ public class ConsoleController {
     private ConsoleService consoleService;
 
     @PostMapping(value="/nodeAddress")
-    public List<String> nodeAddress() {
-        return consoleService.nodeAddress();
+    public R<List<String>> nodeAddress() {
+        return R.ok(consoleService.nodeAddress());
     }
 
     @PostMapping(value="/searchJob")
-    public ConsoleJobVO searchJob(@RequestParam("jobName") String jobName) {
-        return consoleService.searchJob(jobName);
+    public R<ConsoleJobVO> searchJob(@RequestParam("jobName") String jobName) {
+        return R.ok(consoleService.searchJob(jobName));
     }
 
     @PostMapping(value="/listNames")
-    public List<String> listNames(@RequestParam("jobName") String jobName) {
-        return consoleService.listNames(jobName);
+    public R<List<String>> listNames(@RequestParam("jobName") String jobName) {
+        return R.ok(consoleService.listNames(jobName));
     }
 
     @PostMapping(value="/jobResources")
-    public List<String> jobResources() {
-        return consoleService.jobResources();
+    public R<List<String>> jobResources() {
+        return R.ok(consoleService.jobResources());
     }
 
     @PostMapping(value="/overview")
     @ApiOperation(value = "根据计算引擎类型显示任务")
-    public Collection<Map<String, Object>> overview(@RequestParam("nodeAddress") String nodeAddress, @RequestParam("clusterName") String clusterName) {
-        return consoleService.overview(nodeAddress, clusterName);
+    public R<Collection<Map<String, Object>>> overview(@RequestParam("nodeAddress") String nodeAddress, @RequestParam("clusterName") String clusterName) {
+        return R.ok(consoleService.overview(nodeAddress, clusterName));
     }
 
     @PostMapping(value="/groupDetail")
-    public PageResult groupDetail(@RequestParam("jobResource") String jobResource,
+    public R<PageResult> groupDetail(@RequestParam("jobResource") String jobResource,
                                   @RequestParam("nodeAddress") String nodeAddress,
                                   @RequestParam("stage") Integer stage,
                                   @RequestParam("pageSize") Integer pageSize,
                                   @RequestParam("currentPage") Integer currentPage) {
-        return consoleService.groupDetail(jobResource, nodeAddress, stage, pageSize, currentPage);
+        return R.ok(consoleService.groupDetail(jobResource, nodeAddress, stage, pageSize, currentPage));
     }
 
     @PostMapping(value="/jobStick")
-    public Boolean jobStick(@RequestParam("jobId") String jobId) {
-        return consoleService.jobStick(jobId);
+    public R<Boolean> jobStick(@RequestParam("jobId") String jobId) {
+        return R.ok(consoleService.jobStick(jobId));
     }
 
     @PostMapping(value="/stopJob")
-    public void stopJob(@RequestParam("jobId") String jobId) throws Exception {
+    public R<Void> stopJob(@RequestParam("jobId") String jobId) throws Exception {
         consoleService.stopJob(jobId);
+        return R.empty();
     }
 
     @ApiOperation(value = "概览，杀死全部")
     @PostMapping(value="/stopAll")
-    public void stopAll(@RequestParam("jobResource") String jobResource,
+    public R<Void> stopAll(@RequestParam("jobResource") String jobResource,
                         @RequestParam("nodeAddress") String nodeAddress) throws Exception {
         consoleService.stopAll(jobResource, nodeAddress);
+        return R.empty();
     }
 
     @PostMapping(value="/stopJobList")
-    public void stopJobList(@RequestParam("jobResource") String jobResource,
+    public R<Void> stopJobList(@RequestParam("jobResource") String jobResource,
                             @RequestParam("nodeAddress") String nodeAddress,
                             @RequestParam("stage") Integer stage,
                             @RequestParam("jobIdList") List<String> jobIdList) throws Exception {
         consoleService.stopJobList(jobResource, nodeAddress, stage, jobIdList);
+        return R.empty();
     }
 
     @PostMapping(value="/clusterResources")
-    public ClusterResource clusterResources(@RequestParam("clusterName") String clusterName) {
-        return consoleService.clusterResources(clusterName);
+    public R<ClusterResource> clusterResources(@RequestParam("clusterName") String clusterName) {
+        return R.ok(consoleService.clusterResources(clusterName));
     }
 
 }
