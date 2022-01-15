d2a2764dd9fe960ed6bd397f2e8ea74221924bdc
hurricaneyang <1591884898@qq.com>
2022-01-15 20:34:28 +0800

    [feat_master_remove_engine_dao][engine-dao]remove scheduleJobJobDao

--- engine-dao/src/main/java/com/dtstack/engine/mapper/ScheduleJobJobMapper.java
+++ engine-dao/src/main/java/com/dtstack/engine/mapper/ScheduleJobJobMapper.java
@@ -2,6 +2,11 @@ package com.dtstack.engine.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.dtstack.engine.domain.ScheduleJobJob;
+import com.dtstack.engine.dto.ScheduleJobJobTaskDTO;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Collection;
+import java.util.List;
 
 /**
  * @Auther: dazhi
@@ -10,4 +15,23 @@ import com.dtstack.engine.domain.ScheduleJobJob;
  * @Description:
  */
 public interface ScheduleJobJobMapper extends BaseMapper<ScheduleJobJob> {
+    List<ScheduleJobJob> listByJobKey(@Param("jobKey") String jobKey);
+
+    List<ScheduleJobJob> listByJobKeys(@Param("list") List<String> jobKeys);
+
+    List<ScheduleJobJob> listByParentJobKey(@Param("jobKey") String jobKey);
+
+    Integer batchInsert(Collection batchJobJobs);
+
+    Integer update(ScheduleJobJob scheduleJobJob);
+
+    List<ScheduleJobJob> listSelfDependency(@Param("pjobKey") String pjobKey);
+
+    List<ScheduleJobJob> listByParentJobKeys(@Param("list") List<String> list);
+
+    List<ScheduleJobJobTaskDTO> listByParentJobKeysWithOutSelfTask(@Param("jobKeyList") List<String> jobKeyList);
+
+    List<ScheduleJobJobTaskDTO> listByJobKeysWithOutSelfTask(@Param("jobKeyList") List<String> jobKeys);
+
+    void deleteByJobKey(@Param("jobKeyList") List<String> jobKeyList);
 }
