9bda508c3f878e2c20eeeb99e02503b122c171c5
zhaiyue <zhaiyue@dtstack.com>
2022-02-11 16:11:25 +0800

    [feat_master][taiga-all] add datasource create target table

--- taiga-data-develop/src/main/java/com/dtstack/taiga/develop/utils/develop/sync/format/writer/PostgreSqlWriterFormat.java
+++ taiga-data-develop/src/main/java/com/dtstack/taiga/develop/utils/develop/sync/format/writer/PostgreSqlWriterFormat.java
@@ -1,27 +1,10 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.dtstack.taiga.develop.utils.develop.sync.format.writer;
 
 import com.dtstack.taiga.develop.utils.develop.sync.format.ColumnType;
 import com.dtstack.taiga.develop.utils.develop.sync.format.TypeFormat;
 
-public class HiveWriterFormat implements TypeFormat {
+
+public class PostgreSqlWriterFormat implements TypeFormat {
 
     @Override
     public String formatToString(String str) {
@@ -33,9 +16,7 @@ public class HiveWriterFormat implements TypeFormat {
         switch (originType) {
             case BIT:
             case TINYINT:
-                return ColumnType.TINYINT;
             case SMALLINT:
-            case SMALLSERIAL:
                 return ColumnType.SMALLINT;
             case INT:
             case MEDIUMINT:
@@ -44,29 +25,18 @@ public class HiveWriterFormat implements TypeFormat {
             case INT2:
             case INT4:
             case INT8:
-            case UINT8:
-            case UINT16:
-            case UINT32:
-            case INT16:
-            case INT32:
-            case SERIAL:
                 return ColumnType.INT;
             case BIGINT:
-            case UINT64:
-            case INT64:
-            case BIGSERIAL:
                 return ColumnType.BIGINT;
             case REAL:
             case FLOAT:
             case FLOAT2:
             case FLOAT4:
             case FLOAT8:
-            case FLOAT32:
                 return ColumnType.FLOAT;
             case DOUBLE:
             case BINARY_DOUBLE:
-            case FLOAT64:
-                return ColumnType.DOUBLE;
+                return ColumnType.DOUBLE_PRECISION;
             case NUMERIC:
             case NUMBER:
             case DECIMAL:
@@ -87,7 +57,7 @@ public class HiveWriterFormat implements TypeFormat {
             case NVARCHAR2:
             case CLOB:
             case BINARY:
-                return ColumnType.STRING;
+                return ColumnType.VARCHAR;
             case BOOLEAN:
                 return ColumnType.BOOLEAN;
             case DATE:
@@ -95,11 +65,11 @@ public class HiveWriterFormat implements TypeFormat {
             case TIME:
             case DATETIME:
             case SMALLDATETIME:
-                return ColumnType.STRING;
+                return ColumnType.VARCHAR;
             case TIMESTAMP:
                 return ColumnType.TIMESTAMP;
             default:
-                throw new IllegalArgumentException(originType + " is an illegal column Type");
+                throw new IllegalArgumentException();
         }
     }
 }
---
+++ taiga-data-develop/src/main/java/com/dtstack/taiga/develop/utils/develop/sync/util/ADBForPGUtil.java
@@ -0,0 +1,102 @@
+package com.dtstack.taiga.develop.utils.develop.sync.util;
+
+import com.alibaba.fastjson.JSONObject;
+import com.dtstack.taiga.develop.utils.develop.common.util.SqlFormatUtil;
+import com.dtstack.taiga.develop.utils.develop.sync.format.ColumnType;
+import org.apache.commons.lang.StringUtils;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ADBForPGUtil {
+
+    private static String TABLE_TEMPLATE = "create table %s ( %s ); %s";
+
+    // 表备注 举例：COMMENT ON TABLE schema.mytable IS 'This is my table.';
+    private static String ADD_TABLE_COMMENT = "COMMENT ON TABLE %s.%s IS '%s';";
+
+    // 字段注释 举例：COMMENT ON COLUMN mytable.a IS '字段a';
+    private static String ADD_COLUMN_COMMENT = "COMMENT ON COLUMN %s.%s.%s IS '%s';";
+
+    /**
+     * 源表中的字段 转换成ADB For PG 字段
+     *
+     * @param dbColumns 源表中的字段
+     * @return
+     * @apiNote ：目前字段类型统一转换成String，
+     */
+    public static List convertADBForPGWriterColumns(List<JSONObject> dbColumns) {
+        List<JSONObject> hiveColumns = new ArrayList<>(dbColumns.size());
+        for (int i = 0; i < dbColumns.size(); i++) {
+            JSONObject hiveColumn = new JSONObject(4);
+            JSONObject dbColumn = dbColumns.get(i);
+            hiveColumn.put("key", dbColumn.getString("key"));
+            hiveColumn.put("index", i);
+            hiveColumn.put("comment", dbColumn.getString("comment"));
+            hiveColumn.put("type", ColumnType.VARCHAR.name());
+            hiveColumns.add(hiveColumn);
+        }
+        return hiveColumns;
+    }
+
+    /**
+     * 构建建表语句
+     *
+     * @param schema
+     * @param tableName
+     * @param columns
+     * @return
+     */
+    public static String generalCreateSql(String schema, String tableName, List<JSONObject> columns, String tableComment) {
+        String tablePath = buildTablePath(schema, tableName);
+        String columnInfo = buildColumnInfo(columns);
+        String tableAndColumnComment = createTableAndColumnComment(columns, tableName, tableComment, schema);
+        return SqlFormatUtil.formatSql(String.format(TABLE_TEMPLATE, tablePath, columnInfo, tableAndColumnComment));
+    }
+
+    /**
+     * 组装表信息（指定schema时拼接schema名）
+     *
+     * @param schema
+     * @param tableName
+     * @return
+     */
+    private static String buildTablePath(String schema, String tableName) {
+        if (StringUtils.isBlank(schema)) {
+            return tableName;
+        }
+        return schema + "." + tableName;
+    }
+
+    /**
+     * 组装字段信息
+     *
+     * @param columns
+     * @return
+     */
+    private static String buildColumnInfo(List<JSONObject> columns) {
+        List<String> columnInfoList = columns.stream().map(columnObj ->
+                columnObj.getString("key") + " " + ColumnType.VARCHAR.name() + "(255)"
+        ).collect(Collectors.toList());
+        return StringUtils.join(columnInfoList, ",");
+    }
+
+    /**
+     * 拼接表注释
+     * 拼接字段注释
+     *
+     * @param columns 字段信息
+     * @return
+     */
+    private static String createTableAndColumnComment(List<JSONObject> columns, String tableName, String tableComment, String schema) {
+        List<String> tableAndColumnComments = new ArrayList<>();
+        tableAndColumnComments.add(String.format(ADD_TABLE_COMMENT, schema, tableName,
+                StringUtils.isBlank(tableComment) ? "" : tableComment));
+        columns.forEach(writerColumn -> {
+            tableAndColumnComments.add(String.format(ADD_COLUMN_COMMENT, schema, tableName,writerColumn.get("key"),
+                    StringUtils.isBlank(writerColumn.getString("comment")) ? "" : writerColumn.getString("comment")));
+        });
+        return StringUtils.join(tableAndColumnComments, "\n");
+    }
+
+}
