71d7cade240ea7b35e3b01cbdb6c645354c2b4ef
mortalYoung <yangwei1@outlook.com>
2022-03-11 14:53:42 +0800

    [taier-ui]fix log error when saving SparkSQL task

--- taier-ui/src/components/dataSync/help.ts
+++ taier-ui/src/components/dataSync/help.ts
@@ -1,6 +1,6 @@
 import { message } from 'antd';
 import { cloneDeep } from 'lodash';
-import { DATA_SYNC_MODE } from '@/constant';
+import { DATA_SYNC_MODE, TASK_TYPE_ENUM } from '@/constant';
 import type { IOfflineTaskProps } from '@/interface';
 import molecule from '@dtinsight/molecule';
 import api from '@/api';
@@ -19,58 +19,61 @@ type IDataSyncParamProps = IOfflineTaskProps &
  */
 export function generateRqtBody() {
 	const currentTabData = molecule.editor.getState().current?.tab?.data;
-	const DATASYNC_FIELDS = ['keymap', 'setting', 'sourceMap', 'targetMap'];
-	if (DATASYNC_FIELDS.every((f) => currentTabData.hasOwnProperty(f) && currentTabData[f])) {
-		const reqBody: IDataSyncParamProps = cloneDeep(currentTabData);
-		const isIncrementMode =
-			currentTabData.syncModel !== undefined &&
-			DATA_SYNC_MODE.INCREMENT === currentTabData.syncModel;
+	const reqBody: IDataSyncParamProps = cloneDeep(currentTabData);
 
-		const { sourceMap, targetMap, keymap, setting } = reqBody;
-		// 接口要求keymap中的连线映射数组放到sourceMap中
-		sourceMap!.column = keymap!.source;
-		targetMap!.column = keymap!.target;
-		// 把 type 类型下的所有真值放到 sourceMap 下，type 只存放原 sourceMap.type.type 的值
-		// put it into a tmp obj prevent lost
-		let tmpObj = sourceMap!.type!;
-		Object.keys(tmpObj).forEach((key) => {
-			if ((tmpObj as any)[key] !== undefined) {
-				sourceMap![key] = (tmpObj as any)[key];
+	if (currentTabData.taskType === TASK_TYPE_ENUM.SYNC) {
+		const DATASYNC_FIELDS = ['keymap', 'setting', 'sourceMap', 'targetMap'];
+		if (DATASYNC_FIELDS.every((f) => currentTabData.hasOwnProperty(f) && currentTabData[f])) {
+			const isIncrementMode =
+				currentTabData.syncModel !== undefined &&
+				DATA_SYNC_MODE.INCREMENT === currentTabData.syncModel;
+			if (!isIncrementMode) {
+				reqBody.sourceMap!.increColumn = undefined; // Delete increColumn
 			}
-		});
 
-		tmpObj = targetMap!.type!;
-		// targetMap should keep consistent with sourceMap
-		Object.keys(tmpObj).forEach((key) => {
-			if ((tmpObj as any)![key] !== undefined) {
-				targetMap![key] = (tmpObj as any)![key];
-			}
-		});
+			const { sourceMap, targetMap, keymap, setting } = reqBody;
+			// 接口要求keymap中的连线映射数组放到sourceMap中
+			sourceMap!.column = keymap!.source;
+			targetMap!.column = keymap!.target;
+			// 把 type 类型下的所有真值放到 sourceMap 下，type 只存放原 sourceMap.type.type 的值
+			// put it into a tmp obj prevent lost
+			let tmpObj = sourceMap!.type!;
+			Object.keys(tmpObj).forEach((key) => {
+				if ((tmpObj as any)[key] !== undefined) {
+					sourceMap![key] = (tmpObj as any)[key];
+				}
+			});
 
-		// put setting into settingMap field and delete setting field
-		reqBody.settingMap = setting;
+			tmpObj = targetMap!.type!;
+			// targetMap should keep consistent with sourceMap
+			Object.keys(tmpObj).forEach((key) => {
+				if ((tmpObj as any)![key] !== undefined) {
+					targetMap![key] = (tmpObj as any)![key];
+				}
+			});
 
-		if (!isIncrementMode) {
-			reqBody.sourceMap!.increColumn = undefined; // Delete increColumn
+			// put setting into settingMap field and delete setting field
+			reqBody.settingMap = setting;
+
+			Reflect.deleteProperty(reqBody, 'keymap');
+			Reflect.deleteProperty(reqBody, 'setting');
+		} else {
+			message.error('请检查数据同步任务是否填写正确');
+			return null;
 		}
-
-		// 修改task配置时接口要求的标记位
-		reqBody.preSave = true;
-
-		// 接口要求上游任务字段名修改为dependencyTasks
-		if (reqBody.taskVOS) {
-			reqBody.dependencyTasks = reqBody.taskVOS.concat();
-			reqBody.taskVOS = null;
-		}
-
-		Reflect.deleteProperty(reqBody, 'keymap');
-		Reflect.deleteProperty(reqBody, 'setting');
-
-		// 数据拼装结果
-		return reqBody;
 	}
-	message.error('请检查数据同步任务是否填写正确');
-	return null;
+
+	// 修改task配置时接口要求的标记位
+	reqBody.preSave = true;
+
+	// 接口要求上游任务字段名修改为dependencyTasks
+	if (reqBody.taskVOS) {
+		reqBody.dependencyTasks = reqBody.taskVOS.map((o: any) => o);
+		reqBody.taskVOS = null;
+	}
+
+	// 数据拼装结果
+	return reqBody;
 }
 
 /**
