0dc6d25bf14a07337f4523e156cb496d5928ddaf
kongshan <kongshan@dtstack.com>
2022-03-16 18:56:33 +0800

    [fix_sketch][taier-ui]add useCalcTableScroll calc table scrollY

---
+++ taier-ui/src/components/calcTableScroll/index.tsx
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { useEffect, useState, useCallback } from 'react';
+import { TableProps } from 'antd';
+
+interface IProps {
+    className: string
+}
+
+type IScroll<T> = Pick<Partial<TableProps<T>>,'scroll'>['scroll']
+
+function useCalcTableScroll<T>(props: IProps) {
+    const { className } = props
+    const [scroll, setScroll] = useState<IScroll<T>>({})
+
+    const calcTableScroll = useCallback((targetTableEle: HTMLElement) => {
+        return () => {
+            const tableContentHeight = targetTableEle.offsetHeight
+            const tableHeader = targetTableEle.querySelector('.ant-table-thead') as HTMLElement
+            const tableFooter = targetTableEle.querySelector('.ant-table-footer') as HTMLElement
+            setScroll({ y: tableContentHeight - tableHeader.offsetHeight - tableFooter.offsetHeight})
+        }
+    },[])
+
+    useEffect(() => {
+        const targetTableEle = document.querySelector(`.${className}`) as HTMLElement | null
+        if(!targetTableEle) return
+        const resizeObserver = new ResizeObserver(calcTableScroll(targetTableEle))
+        resizeObserver.observe(targetTableEle)
+
+        return () => {
+            resizeObserver.unobserve(targetTableEle)
+        }
+    }, [])
+
+    return { scroll }
+}
+
+export default useCalcTableScroll
