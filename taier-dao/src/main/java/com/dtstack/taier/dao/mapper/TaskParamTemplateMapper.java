7d413662cc4ab4d8801d437d1d38e15ea6ada2c9
LLLLLL_J <1401131298@qq.com>
2022-04-13 15:32:18 +0800

    [1.1_dev_taskDependAndTaskTemplate][taier-all][add taskDepend and TaskTemplate]

--- taier-dao/src/main/java/com/dtstack/taier/dao/mapper/TaskParamTemplateMapper.java
+++
@@ -1,7 +0,0 @@
-package com.dtstack.taier.dao.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.dtstack.taier.dao.domain.TaskParamTemplate;
-
-public interface TaskParamTemplateMapper extends BaseMapper<TaskParamTemplate> {
-}
