788b959133bce04be7dc97675be2acba92727cd6
ghm02708 <ghm02708@gmail.com>
2022-04-28 14:32:51 +0800

    [1.1_dev][taier-data-develop] rename controller class

--- taier-data-develop/src/main/java/com/dtstack/taier/develop/service/develop/impl/FlinkDownloadLogService.java
+++ taier-data-develop/src/main/java/com/dtstack/taier/develop/service/develop/impl/FlinkDownloadLogService.java
@@ -1,7 +1,6 @@
 package com.dtstack.taier.develop.service.develop.impl;
 
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.dtstack.dtcenter.loader.IDownloader;
 import com.dtstack.dtcenter.loader.utils.AssertUtils;
