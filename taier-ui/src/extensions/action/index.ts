3901dbcd25ffdd154a79e89c8b82e7a1561c3838
野迂迂 <yangwei1@outlook.com>
2022-05-06 14:53:18 +0800

    [fix_ui][taier-ui]fix the spells of file's name when importing (#406)

--- taier-ui/src/extensions/action/index.ts
+++ taier-ui/src/extensions/action/index.ts
@@ -1,7 +1,7 @@
 import type { UniqueId } from '@dtinsight/molecule/esm/common/types';
 import type { IExtension } from '@dtinsight/molecule/esm/model';
 import type { IExtensionService } from '@dtinsight/molecule/esm/services';
-import QuickRunSQLAction from './quickRunSqlAction';
+import QuickRunSQLAction from './quickRunSQLAction';
 import QuickSaveTaskAction from './quickSaveTaskAction';
 
 export default class ActionExtensions implements IExtension {
