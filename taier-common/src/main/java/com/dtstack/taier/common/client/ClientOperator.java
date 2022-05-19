55378e4f0b03dece59710bc160f38ab6b59653a2
zekai-li <58294989+zekai-li@users.noreply.github.com>
2022-05-19 21:06:11 +0800

    [issue_482][taier-all]fix session mode no appid

--- taier-common/src/main/java/com/dtstack/taier/common/client/ClientOperator.java
+++ taier-common/src/main/java/com/dtstack/taier/common/client/ClientOperator.java
@@ -116,7 +116,7 @@ public class ClientOperator {
 
 
     public JobResult stopJob(JobClient jobClient) throws Exception {
-        if (jobClient.getApplicationId() == null) {
+        if (jobClient.getEngineTaskId() == null) {
             return JobResult.createSuccessResult(jobClient.getJobId());
         }
         JobIdentifier jobIdentifier = new JobIdentifier(jobClient.getEngineTaskId(), jobClient.getApplicationId(), jobClient.getJobId()
