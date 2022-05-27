42ae0b7d4742fcd8b34f931e86298d3dffc2e2ec
seeTheWord <43259578+seeTheWord@users.noreply.github.com>
2022-05-27 19:18:03 +0800

    [issues_541][taier-common] 自定义函数名校验

--- taier-common/src/main/java/com/dtstack/taier/common/constant/PatternConstant.java
+++ taier-common/src/main/java/com/dtstack/taier/common/constant/PatternConstant.java
@@ -3,7 +3,7 @@ package com.dtstack.taier.common.constant;
 
 public interface PatternConstant {
 
-	String FUNCTION_PATTERN = "[a-z0-9A-Z_]+";
+	String FUNCTION_PATTERN = "[a-z0-9_]+";
 
     /** 字符串查找是否存在密码字段的正则表达式 **/
 	String PASSWORD_FIELD_REGEX = "\"(pass(word)?|accesskey)\"\\s*:\\s*\"\\*{6}\"";
