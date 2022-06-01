9a8c0fe831c96fc8a44672df4d79a9b19ff11ad5
mortalYoung <yangwei1@outlook.com>
2022-06-01 18:36:07 +0800

    [test/utils][taier-ui]add test cases for utils

---
+++ taier-ui/__mocks__/molecule.js
@@ -0,0 +1,10 @@
+module.exports = {
+	Component: class {
+		subscribe = jest.fn();
+	},
+	FolderTreeService: jest.fn(),
+	GlobalEvent: class {
+		subscribe = jest.fn();
+	},
+	connect: jest.fn(),
+};
