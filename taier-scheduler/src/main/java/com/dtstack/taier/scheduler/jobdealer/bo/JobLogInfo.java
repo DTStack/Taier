f8ff3269266179107a5b9965eb45cc4817785aaf
vainhope <845869847@qq.com>
2022-07-05 15:21:36 +0800

    [feat_1.2][plugins.flink] add logDealer

--- taier-scheduler/src/main/java/com/dtstack/taier/scheduler/jobdealer/bo/JobLogInfo.java
+++ taier-scheduler/src/main/java/com/dtstack/taier/scheduler/jobdealer/bo/JobLogInfo.java
@@ -1,40 +1,41 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.dtstack.taier.scheduler.jobdealer.bo;
 
 import com.dtstack.taier.pluginapi.JobIdentifier;
+import com.dtstack.taier.scheduler.enums.EJobLogType;
 
 import java.util.concurrent.Delayed;
 import java.util.concurrent.TimeUnit;
 
-public class JobCompletedInfo implements Delayed {
+public class JobLogInfo implements Delayed {
 
     private String jobId;
     private JobIdentifier jobIdentifier;
     private int computeType;
     private long expired;
+    private String customLog;
+    private EJobLogType logType;
 
-    public JobCompletedInfo(String jobId, JobIdentifier jobIdentifier, int computeType, long delay){
+    public JobLogInfo(String jobId, JobIdentifier jobIdentifier, int computeType, long delay, EJobLogType logType){
         this.jobId = jobId;
         this.jobIdentifier = jobIdentifier;
         this.computeType = computeType;
         this.expired = System.currentTimeMillis() + delay;
+        this.logType = logType;
+    }
+
+    public JobLogInfo() {
+    }
+
+    public EJobLogType getLogType() {
+        return logType;
+    }
+
+    public String getCustomLog() {
+        return customLog;
+    }
+
+    public void setCustomLog(String customLog) {
+        this.customLog = customLog;
     }
 
     public String getJobId() {
@@ -44,7 +45,6 @@ public class JobCompletedInfo implements Delayed {
     public void setJobId(String jobId) {
         this.jobId = jobId;
     }
-
 
     public int getComputeType() {
         return computeType;
