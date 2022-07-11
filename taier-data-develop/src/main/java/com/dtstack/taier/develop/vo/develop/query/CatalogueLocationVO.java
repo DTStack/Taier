e7660d54dccd823cbfc42a3a4e2b7503a72fff97
ghm02708 <ghm02708@gmail.com>
2022-07-11 14:08:17 +0800

    [feat_1.2_codeRefactoring][taier-data-develop]taier-develop remove lombok

--- taier-data-develop/src/main/java/com/dtstack/taier/develop/vo/develop/query/CatalogueLocationVO.java
+++ taier-data-develop/src/main/java/com/dtstack/taier/develop/vo/develop/query/CatalogueLocationVO.java
@@ -3,14 +3,12 @@ package com.dtstack.taier.develop.vo.develop.query;
 
 import com.dtstack.taier.common.param.DtInsightAuthParam;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.Data;
 
 /**
  * @author qianyi
  * @version 1.0
  * @date 2021/1/7 4:50 下午
  */
-@Data
 public class CatalogueLocationVO extends DtInsightAuthParam {
 
     @ApiModelProperty(value = "ID", example = "1", required = true)
@@ -24,4 +22,38 @@ public class CatalogueLocationVO extends DtInsightAuthParam {
 
     @ApiModelProperty(value = "租户ID", example = "1",  required = true)
     private Long tenantId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCatalogueType() {
+        return catalogueType;
+    }
+
+    public void setCatalogueType(String catalogueType) {
+        this.catalogueType = catalogueType;
+    }
+
+    @Override
+    public Long getTenantId() {
+        return tenantId;
+    }
+
+    @Override
+    public void setTenantId(Long tenantId) {
+        this.tenantId = tenantId;
+    }
 }
