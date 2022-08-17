4274f0192d28e82b426d75ca0b78158d8e02a625
bnyte <bnytezz@gmail.com>
2022-08-17 19:44:40 +0800

    updated SqlRegexUtilTest assert program

--- taier-common/src/test/java/com/dtstack/taier/common/util/SqlRegexUtilTest.java
+++ taier-common/src/test/java/com/dtstack/taier/common/util/SqlRegexUtilTest.java
@@ -25,16 +25,50 @@ public class SqlRegexUtilTest {
 
     @Test
     public void testIsSelect() {
+        // check can execute a query sql so assert true
         String sql = "SEleCT\n \nway, \nrom  \n" +
                 "from\n default.test\n;";
         Assert.assertTrue(SqlRegexUtil.isSelect(sql));
+
+        // check can execute a query sql so assert true
+        sql = "select way, rom from default.test;";
+        Assert.assertTrue(SqlRegexUtil.isSelect(sql));
+
+        // check can execute a query sql so assert true
+        sql = "SELECT way, rom from default.test;";
+        Assert.assertTrue(SqlRegexUtil.isSelect(sql));
+
+        // check can execute a query sql so assert true
+        sql = "SEleCT\r\nway, rom from default.test;";
+        Assert.assertTrue(SqlRegexUtil.isSelect(sql));
+
+        sql = "SEleCTway, rom from default.test;";
+        // check not a query sql so assert false
+        Assert.assertFalse(SqlRegexUtil.isSelect(sql));
     }
 
 
     @Test
     public void isExplainSql() {
+        // check can execute a explain sql so assert true
         String sql = "ExPlain SEleCT\n \nway, \nrom  \n" +
                 "from\n default.test\n;";
         Assert.assertTrue(SqlRegexUtil.isExplainSql(sql));
+
+        // check can execute a explain sql so assert true
+        sql = "explain select way, rom from taier;";
+        Assert.assertTrue(SqlRegexUtil.isExplainSql(sql));
+
+        // check can execute a explain sql so assert true
+        sql = "EXPLAIN SEleCT way, rom from taier;";
+        Assert.assertTrue(SqlRegexUtil.isExplainSql(sql));
+
+        // check can execute a explain sql so assert true
+        sql = "explain\r\n SEleCT way, rom from taier;";
+        Assert.assertTrue(SqlRegexUtil.isExplainSql(sql));
+
+        // check not a explain sql so assert false
+        sql = "ExPlainSEleCT from default.test;";
+        Assert.assertFalse(SqlRegexUtil.isExplainSql(sql));
     }
 }
