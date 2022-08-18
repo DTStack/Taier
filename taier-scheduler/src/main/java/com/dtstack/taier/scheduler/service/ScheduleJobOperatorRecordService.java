4fe1244cafd528756ec64b61360f9eb97f276529
vainhope <845869847@qq.com>
2022-08-18 14:38:58 +0800

    [issue_712][taier-scheduler] fix repeat stop error fixes #712
    Former-commit-id: a72891f621591db1c023e736da88bd672f8c05e0

--- taier-scheduler/src/main/java/com/dtstack/taier/scheduler/service/ScheduleJobOperatorRecordService.java
+++ taier-scheduler/src/main/java/com/dtstack/taier/scheduler/service/ScheduleJobOperatorRecordService.java
@@ -4,8 +4,7 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.dtstack.taier.dao.domain.ScheduleJobOperatorRecord;
 import com.dtstack.taier.dao.mapper.ScheduleJobOperatorRecordMapper;
 import com.google.common.collect.Lists;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
@@ -20,32 +19,34 @@ import java.util.Set;
  */
 @Service
 public class ScheduleJobOperatorRecordService extends ServiceImpl<ScheduleJobOperatorRecordMapper, ScheduleJobOperatorRecord> {
-    private static final Logger LOGGER = LoggerFactory.getLogger(ScheduleJobOperatorRecordService.class);
 
     /**
      * 扫描操作记录
      *
-     * @param startSort 开始位置
+     * @param startSort   开始位置
      * @param nodeAddress 节点
-     * @param type 操作类型
-     * @param isEq 是否查询开始位置
+     * @param type        操作类型
+     * @param isEq        是否查询开始位置
      * @return 操作记录
      */
     public List<ScheduleJobOperatorRecord> listOperatorRecord(Long startSort, String nodeAddress, Integer type, Boolean isEq) {
         if (startSort != null && startSort >= 0) {
-            return this.baseMapper.listOperatorRecord(startSort,nodeAddress,type,isEq);
+            return this.baseMapper.listOperatorRecord(startSort, nodeAddress, type, isEq);
         }
         return Lists.newArrayList();
     }
 
     public Integer updateOperatorExpiredVersion(Long id, Timestamp operatorExpired, Integer version) {
         if (id != null && id > 0 && version != null) {
-            return this.baseMapper.updateOperatorExpiredVersion(id,operatorExpired,version);
+            return this.baseMapper.updateOperatorExpiredVersion(id, operatorExpired, version);
         }
         return 0;
     }
 
-    public void insertBatch(Set<ScheduleJobOperatorRecord> scheduleJobOperatorRecords){
+    public void insertBatch(Set<ScheduleJobOperatorRecord> scheduleJobOperatorRecords) {
+        if (CollectionUtils.isEmpty(scheduleJobOperatorRecords)) {
+            return;
+        }
         this.baseMapper.insertIgnoreBatch(scheduleJobOperatorRecords);
     }
 
