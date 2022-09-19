4a5adfdaaef44522df5e1de214eba9f140597dd5
mortalYoung <yangwei1@outlook.com>
2022-09-19 18:03:09 +0800

    feat: develop website

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -3,15 +3,17 @@ import Layout from "@theme/Layout";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Intro from "../components/intro";
 import Features from "../components/features";
-import Case from '../components/case';
+import Case from "../components/case";
 import "./index.scss";
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout title={siteConfig.title} description={siteConfig.tagline}>
-      <Intro />
-      <Features />
+      <div className="banner">
+        <Intro />
+        <Features />
+      </div>
       <Case />
     </Layout>
   );
