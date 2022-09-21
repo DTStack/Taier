08e5ab1c3aa0ce9c4e4ba962994b2e5fddb9fd4d
野迂迂 <yangwei1@outlook.com>
2022-09-21 19:39:47 +0800

    [feat/support][taier-ui]get supported source from request (#754)

--- taier-ui/src/services/taskRenderService.tsx
+++ taier-ui/src/services/taskRenderService.tsx
@@ -3,7 +3,7 @@ import { singleton } from 'tsyringe';
 import { lazy, Suspense } from 'react';
 import { Modal } from 'antd';
 import api from '@/api';
-import { TASK_TYPE_ENUM } from '@/constant';
+import { DATA_SOURCE_ENUM, TASK_TYPE_ENUM } from '@/constant';
 import type { FormInstance } from 'antd';
 import {
 	DataCollectionIcon,
@@ -30,6 +30,36 @@ import { Component } from '@dtinsight/molecule/esm/react';
 
 export interface ITaskRenderState {
 	supportTaskList: ISupportJobTypes[];
+	supportSourceList: {
+		/**
+		 * 数据同步 writers 支持的数据源类型
+		 */
+		writers: DATA_SOURCE_ENUM[];
+		/**
+		 * 数据同步 readers 支持的数据源类型
+		 */
+		readers: DATA_SOURCE_ENUM[];
+		/**
+		 * 实时采集 writer 支持的数据源类型
+		 */
+		dataAcquisitionWriter: DATA_SOURCE_ENUM[];
+		/**
+		 * 实时采集 reader 支持的数据源类型
+		 */
+		dataAcquisitionReader: DATA_SOURCE_ENUM[];
+		/**
+		 * flinkSql 源表支持的数据源类型
+		 */
+		flinkSqlSources: DATA_SOURCE_ENUM[];
+		/**
+		 * flinkSql 结果表支持的数据源类型
+		 */
+		flinkSqlSinks: DATA_SOURCE_ENUM[];
+		/**
+		 * flinkSql 维表支持的数据源类型
+		 */
+		flinkSqlSides: DATA_SOURCE_ENUM[];
+	};
 }
 
 @singleton()
@@ -39,11 +69,21 @@ export default class TaskRenderService extends Component<ITaskRenderState> {
 		 * 当前支持的全部任务列表
 		 */
 		supportTaskList: [],
+		supportSourceList: {
+			writers: [],
+			readers: [],
+			dataAcquisitionWriter: [],
+			dataAcquisitionReader: [],
+			flinkSqlSources: [],
+			flinkSqlSinks: [],
+			flinkSqlSides: [],
+		},
 	};
 
 	constructor() {
 		super();
 		this.getTaskTypes();
+		this.getSupportSource();
 	}
 
 	// 获取当前支持的任务类型
@@ -57,6 +97,33 @@ export default class TaskRenderService extends Component<ITaskRenderState> {
 				notification.error({
 					key: 'FailedJob',
 					message: `获取支持的类型失败，将无法创建新的任务！`,
+				});
+			}
+		});
+	}
+
+	public getSupportSource() {
+		api.getSupportSource<ITaskRenderState['supportSourceList']>({}).then((res) => {
+			if (res.code === 1) {
+				const {
+					writers = [],
+					readers = [],
+					dataAcquisitionReader = [],
+					dataAcquisitionWriter = [],
+					flinkSqlSides = [],
+					flinkSqlSinks = [],
+					flinkSqlSources = [],
+				} = res.data;
+				this.setState({
+					supportSourceList: {
+						writers,
+						readers,
+						dataAcquisitionReader,
+						dataAcquisitionWriter,
+						flinkSqlSides,
+						flinkSqlSinks,
+						flinkSqlSources,
+					},
 				});
 			}
 		});
