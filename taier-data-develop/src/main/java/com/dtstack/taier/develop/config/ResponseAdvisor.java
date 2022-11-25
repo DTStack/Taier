da3e7dc1822dd204b5f475eb07b550a3991ca739
vainhope <845869847@qq.com>
2022-11-25 16:54:32 +0800

    [feat_exception][taier-data-develop] adjust exception message (#916)

--- taier-data-develop/src/main/java/com/dtstack/taier/develop/config/ResponseAdvisor.java
+++ taier-data-develop/src/main/java/com/dtstack/taier/develop/config/ResponseAdvisor.java
@@ -24,7 +24,7 @@ import com.dtstack.taier.common.exception.ExceptionEnums;
 import com.dtstack.taier.common.exception.TaierDefineException;
 import com.dtstack.taier.common.lang.web.R;
 import com.dtstack.taier.pluginapi.constrant.ConfigConstant;
-import com.dtstack.taier.pluginapi.exception.ExceptionUtil;
+import org.apache.commons.lang.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.MethodParameter;
@@ -44,6 +44,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 import javax.validation.ConstraintViolationException;
 import javax.xml.bind.ValidationException;
 import java.util.List;
+import java.util.Optional;
 
 @RestControllerAdvice
 public class ResponseAdvisor implements ResponseBodyAdvice<Object> {
@@ -64,7 +65,7 @@ public class ResponseAdvisor implements ResponseBodyAdvice<Object> {
             message.append(fieldError.getField()).append(":").append(fieldError.getDefaultMessage()).append(",");
         }
 
-        return R.fail(errorCode.getCode(),message.toString());
+        return R.fail(errorCode.getCode(), message.toString());
     }
 
     /**
@@ -73,7 +74,7 @@ public class ResponseAdvisor implements ResponseBodyAdvice<Object> {
     @ResponseBody
     @ExceptionHandler(ValidationException.class)
     public R handleValidationException(ValidationException e) {
-        return R.fail(ErrorCode.INVALID_PARAMETERS.getCode(),e.getMessage());
+        return R.fail(ErrorCode.INVALID_PARAMETERS.getCode(), e.getMessage());
     }
 
     /**
@@ -82,7 +83,7 @@ public class ResponseAdvisor implements ResponseBodyAdvice<Object> {
     @ResponseBody
     @ExceptionHandler(ConstraintViolationException.class)
     public R handleConstraintViolationException(ConstraintViolationException e) {
-        return R.fail(ErrorCode.INVALID_PARAMETERS.getCode(),e.getMessage());
+        return R.fail(ErrorCode.INVALID_PARAMETERS.getCode(), e.getMessage());
     }
 
     @ResponseBody
@@ -91,9 +92,9 @@ public class ResponseAdvisor implements ResponseBodyAdvice<Object> {
         BindingResult result = ex.getBindingResult();
         List<org.springframework.validation.FieldError> fieldErrors = result.getFieldErrors();
         for (FieldError fieldError : fieldErrors) {
-            return R.fail(ErrorCode.INVALID_PARAMETERS.getCode(),fieldError.getDefaultMessage());
+            return R.fail(ErrorCode.INVALID_PARAMETERS.getCode(), fieldError.getDefaultMessage());
         }
-        return R.fail(ErrorCode.INVALID_PARAMETERS.getCode(),ex.getMessage());
+        return R.fail(ErrorCode.INVALID_PARAMETERS.getCode(), ex.getMessage());
     }
 
 
@@ -101,36 +102,20 @@ public class ResponseAdvisor implements ResponseBodyAdvice<Object> {
     @ExceptionHandler(value = Throwable.class)
     public R exceptionHandler(Throwable e) {
         ExceptionEnums errorCode = ErrorCode.UNKNOWN_ERROR;
-        String errorMsg = null;
         TaierDefineException taierDefineException = null;
         if (e.getCause() instanceof DtCenterDefException) {
             taierDefineException = (TaierDefineException) e.getCause();
             if (taierDefineException.getErrorCode() != null) {
                 errorCode = taierDefineException.getErrorCode();
             }
-            errorMsg = taierDefineException.getErrorMsg();
-            if (e.getCause().getCause() != null) {
-                LOGGER.error("", e.getCause().getCause());
-            }
         } else if (e instanceof TaierDefineException) {
             taierDefineException = (TaierDefineException) e;
             if (taierDefineException.getErrorCode() != null) {
                 errorCode = taierDefineException.getErrorCode();
             }
-            errorMsg = taierDefineException.getErrorMsg();
-            if (e.getCause() != null) {
-                LOGGER.error("", e.getCause());
-            }
-        } else {
-            errorCode = ErrorCode.SERVER_EXCEPTION;
-            errorMsg = ExceptionUtil.getErrorMessage(e);
-            LOGGER.error("", e.getCause());
         }
-
-        if (errorCode.equals(ErrorCode.PERMISSION_LIMIT)) {
-            return R.fail(errorCode.getCode(), errorMsg);
-        }
-        return R.fail(errorCode.getCode(), errorMsg);
+        LOGGER.error("", e.getCause());
+        return R.fail(errorCode.getCode(), Optional.ofNullable(e.getMessage()).orElse(ExceptionUtils.getMessage(e.getCause())));
     }
 
     @Override
