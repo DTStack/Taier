45f7898b91b3bbddf7732cebe0f49f0bfccd25c9
Jixxx <75556346+bnyte@users.noreply.github.com>
2022-12-08 11:51:22 +0800

    fixed: 1.3.1 increment sql merged(#936) (#937)
    fixed: 1.3.1 repeat increment sql merged(#936)
    Former-commit-id: 72c233a0275083d0a80e40789c19548cf39df420

--- sql/1.3.1/1.3.1_increment.sql
+++
@@ -1,5 +0,0 @@
-BEGIN;
-
-# 增加MySQL任务
-INSERT INTO `dict` VALUES (363, '17', 'MySQL', '{"actions":["SAVE_TASK","RUN_TASK","STOP_TASK","SUBMIT_TASK","OPERATOR_TASK"],"barItem":["task","dependency","task_params","env_params"],"formField":["datasource"],"renderKind":"editor","dataTypeCodes":[1]}', '', 30, 0, 'STRING', '', 0, '2022-10-24 15:46:53', '2022-10-24 15:46:53', 0);
-COMMIT;
