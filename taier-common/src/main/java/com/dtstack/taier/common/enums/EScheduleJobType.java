eded54d1afe5be70775df301271af199ea8e7497
toutian <953372946@qq.com>
2023-02-09 21:09:03 +0800

    suppourt hadoop MR

--- taier-common/src/main/java/com/dtstack/taier/common/enums/EScheduleJobType.java
+++ taier-common/src/main/java/com/dtstack/taier/common/enums/EScheduleJobType.java
@@ -116,6 +116,8 @@ public enum EScheduleJobType {
     VERTICA(23, "Vertica", EJobType.SQL.getType(), 4, null, EComputeType.BATCH, EJobClientType.DATASOURCE_PLUGIN),
     MAXCOMPUTE(24, "MaxCompute", EJobType.SQL.getType(), 4, null, EComputeType.BATCH, EJobClientType.DATASOURCE_PLUGIN),
 
+    HADOOP_MR(25, "HadoopMR", EJobType.MR.getType(), 16, EComponentType.HDFS, EComputeType.BATCH, EJobClientType.WORKER_PLUGIN),
+
     ;
 
     private final Integer type;
