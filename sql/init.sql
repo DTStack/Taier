d693b5d249bea4a37fceb44d1ea298a75b46dc37
vainhope <845869847@qq.com>
2023-04-18 22:56:01 +0800

    [master][taier-sql] fix sql (#1054)
    Former-commit-id: 61fa90048d10e6d18607ba24f5cbebb5534d8741

--- sql/init.sql
+++ sql/init.sql
@@ -2340,9 +2340,6 @@ COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
 
-INSERT INTO `dict` (dict_code, dict_name, dict_value, dict_desc, type, sort, data_type, depend_name, is_default, gmt_create, gmt_modified, is_deleted) VALUES ('25', 'HadoopMR', '{"actions": ["SAVE_TASK", "SUBMIT_TASK", "OPERATOR_TASK"], "formField": ["resourceIdList", "mainClass", "exeArgs"],"barItem":["dependency","task_params","env_params"], "renderKind": "spark"}', '', 30, 5, 'STRING', '', 0, '2023-02-09 10:28:45', '2023-02-09 10:28:45', 0);
-
-
 -- script 支持 standalone
 
 -- 处理组件配置是否有多版本之类的
