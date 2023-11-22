3222c602ef383979577c580bd48756f0b5e6ea71
ddwolf715 <dufenghome@gmail.com>
2023-11-22 17:55:16 +0800

    整理数据库脚本，添加合并到初始化以及版本升级脚本中

--- sql/1.5/1.5_increment.sql
+++ sql/1.5/1.5_increment.sql
@@ -93,3 +93,8 @@ INSERT INTO task_param_template (task_type, task_name, task_version, params, gmt
 
 ## 开启spark推测行为，默认false
 # spark.speculation=false', now(), now(), 0);
+
+-- 2023-11-22 add by xjs1983boy OracleSQL to dict
+insert into dict(dict_code,dict_name,dict_value,dict_desc,type,sort,data_type,depend_name,is_default,gmt_create,gmt_modified,is_deleted)
+values('50','OracleSQL','{"actions":["SAVE_TASK","RUN_TASK","STOP_TASK","SUBMIT_TASK","OPERATOR_TASK"],"barItem":["task","dependency","task_params","env_params"],"formField":["datasource"],"renderKind":"editor","dataTypeCodes":[2]}','',30,0,'STRING','',0,'2023-11-06 13:35:00','2023-11-06 13:35:00',0);
+COMMIT;
