aa10a54d46e52d922e8941b2773d06923693ae7d
lalala <56628290+jin-sir@users.noreply.github.com>
2026-06-15 19:09:21 +0800

    WIP: [feat/sql-languages][taier-ui] add custom parameters (#1138)
    ### 简介
    
    控制台新增自定义参数
    
    ### 🔗 相关 Issue
    
    feat [#1137](https://github.com/DTStack/Taier/issues/1137)

--- taier-ui/src/pages/console/cluster/detail/detail.tsx
+++ taier-ui/src/pages/console/cluster/detail/detail.tsx
@@ -29,6 +29,7 @@ import {
 import type { RcFile } from 'antd/lib/upload';
 
 import api from '@/api';
+import CustomParameter, { ICustomValue } from '@/components/customParameter';
 import { COMPONENT_TYPE_VALUE } from '@/constant';
 import context from '@/context/cluster';
 import type { IComponentProps } from '.';
@@ -38,6 +39,7 @@ const { Sider, Content } = Layout;
 const { Panel } = Collapse;
 
 interface IDetailProps {
+    form: any;
     templateData: ILayoutData[];
     currentTreeNode?: IComponentProps;
     loading?: boolean;
@@ -80,11 +82,12 @@ export interface ITemplateData {
     /**
      * XML 表示该值作为 config 渲染
      */
-    type: 'INPUT' | 'RADIO_LINKAGE' | 'CHECKBOX' | 'XML' | 'GROUP';
+    type: 'INPUT' | 'RADIO_LINKAGE' | 'CHECKBOX' | 'XML' | 'GROUP' | 'CUSTOM';
     value: string;
 }
 
 export default function Detail({
+    form,
     loading,
     currentTreeNode,
     templateData = [],
@@ -371,6 +374,23 @@ export default function Detail({
                                             );
                                         }
                                     )}
+                                    <Form.Item
+                                        name={[template.groupName, 'customParameters'].join('$')}
+                                        noStyle
+                                        rules={[
+                                            {
+                                                validator(_, value: ICustomValue[]) {
+                                                    return value.every((item) => item.status)
+                                                        ? Promise.resolve()
+                                                        : Promise.reject('自定义字段名重复');
+                                                },
+                                            },
+                                        ]}
+                                    >
+                                        <CustomParameter
+                                            existingKeys={Object.keys(form.getFieldValue(template.groupName) || {})}
+                                        />
+                                    </Form.Item>
                                 </Panel>
                             </Collapse>
                         );
